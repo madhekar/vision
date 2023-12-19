14e105b0973e85a6ed2cfadf3ae49e4173929f08
Bhalchandra Madhekar <bmadhekar@gmail.com>
2023-12-19 11:22:40 -0800

    bug fix

--- research/code/instance_segmentation_with_object_tracking.py
+++ research/code/instance_segmentation_with_object_tracking.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 track_history = defaultdict(lambda: [])
 
-model = YOLO("yolov8n-seg.pt")
+model = YOLO("YOLOv8n-seg.pt") # yolov8n-seg.pt
 cap = cv2.VideoCapture("slow_traffic_small.mp4")
 
 out = cv2.VideoWriter('instance-segmentation-object-tracking.avi',
@@ -19,22 +19,22 @@ while True:
         print("Video frame is empty or video processing has been successfully completed.")
         break
 
-        results = model.track(im0, persist=True)
-        masks = results[0].masks.xy
-        track_ids = results[0].boxes.id.int().cpu().tolist()
+    results = model.track(im0, persist=True)
+    masks = results[0].masks.xy
+    track_ids = results[0].boxes.id.int().cpu().tolist()
 
-        annotator = Annotator(im0, line_width=2)
+    annotator = Annotator(im0, line_width=2)
 
-        for mask, track_id in zip(masks, track_ids):
-            annotator.seg_bbox(mask=mask,
+    for mask, track_id in zip(masks, track_ids):
+        annotator.seg_bbox(mask=mask,
                                mask_color=colors(track_id, True),
                                track_label=str(track_id))
 
-        out.write(im0)
-        cv2.imshow("instance-segmentation-object-tracking", im0)
+    out.write(im0)
+    cv2.imshow("instance-segmentation-object-tracking", im0)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 out.release()
 cap.release()
