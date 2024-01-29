715019d4cc1d2eb120f3255989f343bffac102c7
bhalchandra madhekar <bhal@bhalchandras-MacBook-Air-2.local>
2024-01-29 15:05:22 -0800

    webRTC signaling, ICE SDP etc.

--- research/code/webRTC/rtc/src/App.js
+++ research/code/webRTC/rtc/src/App.js
@@ -5,26 +5,83 @@ import {useRef, useEffect} from 'react';
 function App() {
   const localVideoRef = useRef()
   const remoteVideoRef = useRef()
+  const pc = useRef(new RTCPeerConnection(null))
+  const textRef = useRef()
 
-  const getUserMedia = async () => {
-    const constraints = {
-      audio : false,
-      video : true,
-    }
- /*    navigator.mediaDevices.getUserMedia(constraints)
-    .then(stream => {
-      // display stream
-      localVideoRef.current.srcObject = stream
-    })
-    .catch(e => {
-      console.log('getUserMedia error ...', e)
-    }) */
-    const stream = await navigator.mediaDevices.getUserMedia(constraints)
-    localVideoRef.current.srcObject = stream
+  useEffect(() =>{
+
+      const constraints = {
+        audio : false,
+        video : true,
+      }
+   navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        // display stream
+        localVideoRef.current.srcObject = stream
+
+        stream.getTracks().forEach(track => {
+          _pc.addTrack(track, stream)
+        })
+      })
+      .catch(e => {
+        console.log('getUserMedia error ...', e)
+      }) 
+      const _pc =  new RTCPeerConnection(null)
+      _pc.onicecandidate = (e) => {
+        if (e.candidate)
+          console.log(JSON.stringify(e.candidate))
+      }
+      // connected, dis-connected, failed, closed 
+      _pc.oniceconnectionstatechange = (e) => {
+        console.log(e)
+      }
+
+      _pc.ontrack = (e) =>{
+        // got remote stream
+        remoteVideoRef.current.srcObject = e.streams[0]
+      }
+      pc.current = _pc
+  }, [])
+
+  const createOffer = () => {
+    pc.current.createOffer({
+      offerToReceiveAudio: 1,
+      offerToReceiveVidio: 1,
+    }).then(sdp => {
+      console.log(JSON.stringify(sdp))
+      pc.current.setLocalDescription(sdp)
+    }).catch(e => console.log(e))
   }
+
+  const createAnswer = () => {
+    pc.current.createAnswer({
+      offerToReceiveAudio: 1,
+      offerToReceiveVidio: 1,
+    }).then(sdp => {
+      console.log(JSON.stringify(sdp))
+      pc.current.setLocalDescription(sdp)
+    }).catch(e => console.log(e))
+  }
+
+  const setRemoteDescription = () => {
+    // get SDP value from the text editor
+    const sdp = JSON.parse(textRef.current.value)
+    console.log(sdp)
+
+    pc.setRemoteDescription(new RTCSessionDescription(sdp))
+  }
+
+  const addCandidate = () => {
+    const candidate =  JSON.parse(textRef.current.value)
+    console.log('Additing Candidate...', candidate)
+
+    pc.current.addIceCandidate(new RTCIceCandidate(candidate))
+
+  }
+
+
   return (
     <div style={{margin:10}}>
-      <button onClick={() => getUserMedia()}>Get Media Access</button>
       <video style={{
         width: 240, height: 240,
         margin: 5, backgroundColor: 'black',
@@ -36,11 +93,13 @@ function App() {
       }}
       ref={remoteVideoRef} autoPlay></video>
       <br />
-      <button onClick={() => {}}>Offer</button>
-      <button onClick={() => {}}>Answer</button>
+      <button onClick={createOffer}>Offer</button>
+      <button onClick={createAnswer}>Answer</button>
       <br />
-      <button onClick={() => {}}>Remote Description</button>
-      <button onClick={() => {}}>Add Candidates</button>
+      <textarea ref={textRef}></textarea>
+      <br />
+      <button onClick={setRemoteDescription}>Remote Description</button>
+      <button onClick={addCandidate}>Add Candidates</button>
     </div>
   );
 }
