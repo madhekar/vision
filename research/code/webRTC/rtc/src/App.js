7e54599a17afdc199bea85338c249056af86a64d
bhalchandra madhekar <bhal@bhalchandras-MacBook-Air-2.local>
2024-01-29 14:15:20 -0800

    RTC async

--- research/code/webRTC/rtc/src/App.js
+++ research/code/webRTC/rtc/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import {useRef} from 'react';
+import {useRef, useEffect} from 'react';
 
 function App() {
   const localVideoRef = useRef()
+  const remoteVideoRef = useRef()
 
   const getUserMedia = async () => {
     const constraints = {
@@ -24,7 +25,22 @@ function App() {
   return (
     <div style={{margin:10}}>
       <button onClick={() => getUserMedia()}>Get Media Access</button>
-      <video ref={localVideoRef} autoPlay></video>
+      <video style={{
+        width: 240, height: 240,
+        margin: 5, backgroundColor: 'black',
+      }}
+      ref={localVideoRef} autoPlay></video>
+      <video style={{
+        width: 240, height: 240,
+        margin: 5, backgroundColor: 'black',
+      }}
+      ref={remoteVideoRef} autoPlay></video>
+      <br />
+      <button onClick={() => {}}>Offer</button>
+      <button onClick={() => {}}>Answer</button>
+      <br />
+      <button onClick={() => {}}>Remote Description</button>
+      <button onClick={() => {}}>Add Candidates</button>
     </div>
   );
 }
