c1cb27bb61a6087289be0a1aca5a77c9b4a1496c
bhalchandra madhekar <bmadhekar@gmail.com>
2024-01-30 20:28:23 -0800

    bugfix#120

--- research/code/webRTC/rtc/src/App.js
+++ research/code/webRTC/rtc/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import io from 'socket.io-client'
 
 const socket = io(
@@ -15,7 +15,10 @@ function App() {
   const remoteVideoRef = useRef()
   const pc = useRef(new RTCPeerConnection(null))
   const textRef = useRef()
-  const candidates = useRef([])
+
+  const [offerVisible, setOfferVisible] = useState(true)
+  const [answerVisible, setAnswerVisible] = useState(false)
+  const [status, setStatus] = useState('Make a call now')
 
   useEffect(() =>{
 
@@ -25,12 +28,21 @@ function App() {
 
     socket.on('sdp', data => {
       console.log(data)
+      pc.current.setRemoteDescription(new RTCSessionDescription(data.sdp))
       textRef.current.value = JSON.stringify(data.sdp)
+
+      if(data.sdp.type === 'offer'){
+        setOfferVisible(false)
+        setAnswerVisible(true)
+        setStatus('Incomming call...')
+      }else {
+        setStatus('Call established.')
+      }
     })
 
     socket.on('candidate', candidate => {
       console.log(candidate)
-      candidates.current = [...candidates.current, candidate]
+      pc.current.addIceCandidate(new RTCIceCandidate(candidate))
     })
 
       const constraints = {
@@ -52,10 +64,11 @@ function App() {
       }) 
 
       const _pc =  new RTCPeerConnection(null)
+
       _pc.onicecandidate = (e) => {
         if (e.candidate)
           console.log(JSON.stringify(e.candidate))
-          socket.emit('candidate', e.candidate)
+          sendToPeer('candidate', e.candidate)
       }
 
       // connected, disconnected, failed, closed 
@@ -63,84 +76,81 @@ function App() {
         console.log(e)
       }
 
-      _pc.ontrack = (e) =>{
+      _pc.ontrack = (e) => {
         // got remote stream
         remoteVideoRef.current.srcObject = e.streams[0]
       }
+
       pc.current = _pc
+
   }, [])
+
+  const sendToPeer = (eventType, payload) => {
+    socket.emit(eventType, payload)
+  }
+
+  const processSDP = (sdp) => {
+     console.log(JSON.stringify(sdp))
+     pc.current.setLocalDescription(sdp)
+     sendToPeer( 'sdp', { sdp })
+    }
 
   const createOffer = () => {
     pc.current.createOffer({
       offerToReceiveAudio: 1,
-      offerToReceiveVidio: 1,
+      offerToReceiveVideo: 1,
     }).then(sdp => {
-      console.log(JSON.stringify(sdp))
-      pc.current.setLocalDescription(sdp)
-
-      // send sdp to the server
-      socket.emit('sdp', {
-        sdp,
-      })
+      processSDP(sdp)
+      setOfferVisible(false)
+      setStatus('Calling...')
     }).catch(e => console.log(e))
   }
 
   const createAnswer = () => {
     pc.current.createAnswer({
       offerToReceiveAudio: 1,
-      offerToReceiveVidio: 1,
+      offerToReceiveVideo: 1,
     }).then(sdp => {
-      console.log(JSON.stringify(sdp))
-      pc.current.setLocalDescription(sdp)
-
       //send the answer sdp to the offering peer
-      socket.emit('sdp', {
-        sdp,
-      })
+      processSDP(sdp)
+      setAnswerVisible(false)
+      setStatus('Call established.')
     }).catch(e => console.log(e))
   }
 
-  const setRemoteDescription = () => {
-    // get SDP value from the text editor
-    const sdp = JSON.parse(textRef.current.value)
-    console.log(sdp)
-
-    pc.current.setRemoteDescription(new RTCSessionDescription(sdp))
+  const showHideButton = () => {
+    if (offerVisible){
+      return (
+        <div>
+          <button onClick={createOffer}>Call</button>
+        </div>
+      ) 
+    } else if (answerVisible){
+      return(
+        <div>
+          <button onClick={createAnswer}>Answer</button>
+        </div>
+      )
+    }
   }
-
-  const addCandidate = () => {
-    //const candidate =  JSON.parse(textRef.current.value)
-    //console.log('Additing Candidate...', candidate)
-
-    candidates.current.forEach(candidate => {
-      console.log(candidate)
-      pc.current.addIceCandidate(new RTCIceCandidate(candidate))
-    })
-  }
-
 
   return (
     <div style={{margin:10}}>
       <video style={{
-        width: 240, height: 240,
-        margin: 5, backgroundColor: 'black',
+        width: 240, height: 240, margin: 5, backgroundColor: 'black',
       }}
       ref={localVideoRef} autoPlay></video>
 
       <video style={{
-        width: 240, height: 240,
-        margin: 5, backgroundColor: 'black',
+        width: 240, height: 240, margin: 5, backgroundColor: 'black',
       }}
       ref={remoteVideoRef} autoPlay></video>
 
       <br />
-      <button onClick={createOffer}>Offer</button>
-      <button onClick={createAnswer}>Answer</button>
+        {showHideButton()}
+        <div>{status}</div>
       <br />
-      <textarea ref={textRef}></textarea>
-      <br />
-      <button onClick={setRemoteDescription}>Remote Description</button>
-      <button onClick={addCandidate}>Add Candidates</button>
+        <textarea ref={textRef}></textarea>
     </div>
   );
 }
