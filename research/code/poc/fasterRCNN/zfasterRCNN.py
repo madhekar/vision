28ba110992e89573cfba1abdec1b139ae94b7f2d
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-02-23 14:58:20 -0800

    code cleanup

--- research/code/poc/fasterRCNN/zfasterRCNN.py
+++ research/code/poc/fasterRCNN/zfasterRCNN.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt 
 from PIL import Image
-import random
 import torch
 import torchvision.transforms as T
 import torchvision
@@ -38,10 +37,10 @@ class zsehaDetector:
         
         pred_score = list(pred[0]['scores'].detach().cpu().numpy())
         pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
-        #masks = (pred[0]['masks'] > 0.5).squeeze().detach().cpu().numpy()
+
         pred_class = [self.COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].cpu().numpy())]
         pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().cpu().numpy())]
-        #masks = masks[:pred_t+1]
+
         pred_boxes = pred_boxes[:pred_t+1]
         pred_class = pred_class[:pred_t+1]
         return pred_boxes, pred_class
@@ -57,24 +56,20 @@ class zsehaDetector:
           - text_th - thichness of the text
         method:
           - prediction is obtained from get_prediction method
-          - for each prediction, bounding box is drawn and text is written 
-            with opencv
+          - for each prediction, bounding box is drawn and text is written with opencv
           - the final image is displayed
         """
         boxes, pred_cls = self.getPrediction(imgPath, threshold)
 
         img = cv2.imread(imgPath)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # print(len(boxes))
+
         for i in range(len(boxes)):
            pt1 = tuple(int(x) for x in boxes[i][0])
            pt2 = tuple(int(x) for x in boxes[i][1])
            cv2.rectangle(img, pt1, pt2, color=(0, 255, 0), thickness=rect_th)
            cv2.putText(img,pred_cls[i], pt1, cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
-        #plt.figure(figsize=(10,10))
         plt.imshow(img)
-        #plt.xticks([])
-        #plt.yticks([])
         plt.show()
     
 if __name__=='__main__':
