603b179c300cbac1d08604b74a1cf64ad9e9f777
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-02-23 18:34:19 -0800

    poc bug #98

--- research/code/poc/fasterRCNN/zfasterRCNN.py
+++ research/code/poc/fasterRCNN/zfasterRCNN.py
@@ -59,6 +59,7 @@ class zsehaDetector:
           - for each prediction, bounding box is drawn and text is written with opencv
           - the final image is displayed
         """
+
         boxes, pred_cls = self.getPrediction(imgPath, threshold)
 
         img = cv2.imread(imgPath)
@@ -67,8 +68,8 @@ class zsehaDetector:
         for i in range(len(boxes)):
            pt1 = tuple(int(x) for x in boxes[i][0])
            pt2 = tuple(int(x) for x in boxes[i][1])
-           cv2.rectangle(img, pt1, pt2, color=(0, 255, 0), thickness=rect_th)
-           cv2.putText(img,pred_cls[i], pt1, cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
+           cv2.rectangle(img, pt1, pt2, color=(250, 0, 0), thickness=rect_th)
+           cv2.putText(img,pred_cls[i], pt1, cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,0,0),thickness=text_th)
         plt.imshow(img)
         plt.show()
     
