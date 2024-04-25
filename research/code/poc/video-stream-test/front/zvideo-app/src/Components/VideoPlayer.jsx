39047b8a73b3ebb9111d668286dc8caebc683c11
bhalchandra madhekar <bmadhekar@gmail.com>
2024-04-25 12:51:03 -0700

    video stream app

---
+++ research/code/poc/video-stream-test/front/zvideo-app/src/Components/VideoPlayer.jsx
@@ -0,0 +1,22 @@
+import React, {useRef, useEffect} from 'react'
+
+const VideoPlayer = ({videoId}) => {
+    const videoRef = useRef(null)
+
+    useEffect(() => {
+        if (videoRef.current){
+            videoRef.current.pause()
+            videoRef.current.removeAttribute('src')
+            videoRef.current.load()
+        }
+
+    })
+  return (
+    <video ref={videoRef} width='480'  controls autoplay> 
+       <source src={`http://localhost:3000/videos/${videoId}`} type='video/mp4'></source>
+       Your browser does not support tag video.
+    </video>
+  )
+}
+
+export default VideoPlayer
