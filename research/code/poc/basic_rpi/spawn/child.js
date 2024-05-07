4e3643089cf1cb8385dab510273d98205b8baca0
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-05-07 12:55:19 -0700

    POC

--- research/code/poc/basic_rpi/spawn/child.js
+++ research/code/poc/basic_rpi/spawn/child.js
@@ -3,11 +3,29 @@ var cp = require('child_process');
 var progs = {
     lists: 'ls',
     copy: 'cp',
-    folder: 'mkdir'
+    folder: 'mkdir',
+    libcamera: 'libcamera-vid'
 }
 
-var child = cp.spawn(progs.lists, ['-r', '/Library']);
+var child = cp.spawn(progs.lists, ['-l'], {cwd: '..'} );
 
+var child = cp.spawn(progs.cp, ['test.txt', 'test1.txt'])
+
+var child = cp.spawn(progs.folder, ['dummy'])
+
+//var child = cp.spawn(progs.libcamera, ['--timeout', '0', '--framerate', '30', '--nopreview', '--output', '-']);
 child.stdout.on('data', (data) => {
    console.log(`data:\n ${data}`)
+})
+
+child.stderr.on('data', (err) => {
+    console.log(`error: ${err}`)
+})
+
+child.on('exit', () => {
+    console.log('child process is finished!')
+})
+
+child.on('close', () =>{
+    console.log('child process is closed!')
 })
