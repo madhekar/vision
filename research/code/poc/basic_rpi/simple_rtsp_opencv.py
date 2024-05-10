3247bbe988626d8b2e1fe3d2f3840c500051dfd2
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-05-10 08:02:05 -0700

    test for validation

--- research/code/poc/basic_rpi/simple_rtsp_opencv.py
+++ research/code/poc/basic_rpi/simple_rtsp_opencv.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 import os
+import time
 
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
-vcap = cv2.VideoCapture("rtsp://192.168.68.115:10554/stream_iot", cv2.CAP_FFMPEG)
-while(1):
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp;http"
+#vcap = cv2.VideoCapture("rtsp://192.168.68.115:10554/stream_iot", cv2.CAP_FFMPEG)
+vcap = cv2.VideoCapture("http://192.168.68.115:8000/stream.mjpg") #, cv2.CAP_V4L2) #http://192.168.68.115:8000/stream.mjpg
+time.sleep(1)
+
+while(True):
     ret, frame = vcap.read()
     if ret == False:
-        print("Frame is empty")
+        print("Frame is empty", frame)
         break;
     else:
         cv2.imshow('VIDEO', frame)
