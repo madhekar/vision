6a8ad3b699fb178b7bd0919d739c473060b7294d
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-05-10 13:08:04 -0700

    bug fix

--- research/code/poc/basic_rpi/simple_rtsp_opencv.py
+++ research/code/poc/basic_rpi/simple_rtsp_opencv.py
@@ -3,16 +3,34 @@ import numpy as np
 import os
 import time
 
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp;http"
-#vcap = cv2.VideoCapture("rtsp://192.168.68.115:10554/stream_iot", cv2.CAP_FFMPEG)
-vcap = cv2.VideoCapture("http://192.168.68.115:8000/stream.mjpg") #, cv2.CAP_V4L2) #http://192.168.68.115:8000/stream.mjpg
-time.sleep(1)
+source_url = 'rtsp://192.168.68.115:10554/stream_iot' # http://192.168.68.115:8000/stream.mjpg
 
-while(True):
-    ret, frame = vcap.read()
-    if ret == False:
-        print("Frame is empty", frame)
-        break;
-    else:
-        cv2.imshow('VIDEO', frame)
-        cv2.waitKey(1)
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp;http"
+cap = cv.VideoCapture(source_url) #cv2.CAP_V4L2  cv2.CAP_FFMPEG
+
+if not cap.isOpened():
+  print("Cannot open url/ camera!")
+  exit()
+
+while True:
+
+ # Capture frame-by-frame
+ ret, frame = cap.read()
+ 
+ # if frame is read correctly ret is True
+ if not ret:
+    print("Can't receive frame (stream end?). Exiting ...")
+    break
+
+ # Our operations on the frame come here
+ gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+ # Display the resulting frame
+ cv.imshow('Video-Frame', gray)
+ if cv.waitKey(1) == ord('q'):
+ break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
