2fb090b57a0759e2f5e1f69f41d91ef9c3a07971
bhalchandra madhekar <bmadhekar@gmail.com>
2024-05-15 14:23:28 -0700

    bug fix + enhancement

--- research/code/poc/zeshaVideo/web/src/App.js
+++ research/code/poc/zeshaVideo/web/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     }
 
     // DONT FORGET TO CHANGE TO YOUR URL
-    this.serviceIP = 'https://01e8-70-137-105-159.ngrok-free.app/webrtcPeer'
+    this.serviceIP = 'https://463a-70-137-105-159.ngrok-free.app/webrtcPeer'
 
     this.socket = null
     // this.candidates = []
