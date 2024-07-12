3a0549c25bdaa4eb1e652d11985f4bb0d232b67f
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-07-12 11:29:48 -0700

    geocoding call

--- research/code/multiModality/util.py
+++ research/code/multiModality/util.py
@@ -1,7 +1,8 @@
 import sys
 from PIL import Image
 from PIL.ExifTags import TAGS
-import time
+import pyexiv2
+import fractions
 from GPSPhoto import gpsphoto
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
@@ -51,3 +52,45 @@ def getMetadata(img):
     res = getDateTime(img)
     res.append(getLocationDetails(gpsInfo(img=img)))
     return res
+
+def to_deg(value, loc):
+        if value < 0:
+            loc_value = loc[0]
+        elif value > 0:
+            loc_value = loc[1]
+        else:
+            loc_value = ""
+        abs_value = abs(value)
+        deg =  int(abs_value)
+        t1 = (abs_value-deg)*60
+        min = int(t1)
+        sec = round((t1 - min)* 60, 5)
+        return (deg, min, sec, loc_value) 
+    
+def setGpsLocation(fname, lat, lng):
+    
+    lat_deg = to_deg(lat, ["S", "N"])
+    lng_deg = to_deg(lng, ["W", "E"])
+
+    print ('lat:', lat_deg, ' lng:', lng_deg)
+
+    # convert decimal coordinates into degrees, munutes and seconds
+    exiv_lat = (pyexiv2.Rational(lat_deg[0]*60+lat_deg[1],60),pyexiv2.Rational(lat_deg[2]*100,6000), pyexiv2.Rational(0, 1))
+    exiv_lng = (pyexiv2.Rational(lng_deg[0]*60+lng_deg[1],60),pyexiv2.Rational(lng_deg[2]*100,6000), pyexiv2.Rational(0, 1))
+
+    exiv_image = pyexiv2.Image(fname)
+    exiv_image.readMetadata()
+    exif_keys = exiv_image.exifKeys() 
+    print('exif keys: ', exif_keys)
+
+    exiv_image["Exif.GPSInfo.GPSLatitude"] = exiv_lat
+    exiv_image["Exif.GPSInfo.GPSLatitudeRef"] = lat_deg[3]
+    exiv_image["Exif.GPSInfo.GPSLongitude"] = exiv_lng
+    exiv_image["Exif.GPSInfo.GPSLongitudeRef"] = lng_deg[3]
+    exiv_image["Exif.Image.GPSTag"] = 654
+    exiv_image["Exif.GPSInfo.GPSMapDatum"] = "WGS-84"
+    exiv_image["Exif.GPSInfo.GPSVersionID"] = '2 0 0 0'
+
+    exiv_image.writeMetadata()
+
+set_gps_location(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]))
