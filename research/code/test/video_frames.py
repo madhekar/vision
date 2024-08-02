3635283bc6dd2cbce6781c700bcfb149062b8be1
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-08-02 13:04:33 -0700

    video quality frame extraction

--- research/code/test/video_frames.py
+++ research/code/test/video_frames.py
@@ -46,3 +46,10 @@ if __name__=='__main__':
 # ffmpeg -i "/home/madhekar/work/zsource/family/video/IMG_0005.MOV" -vf "select=eq(n\,34)" -vframes 1 out.png
 # ffmpeg -ss 4 -i /home/madhekar/work/zsource/family/video/IMG_0005.MOV -s 320x240 -frames:v 1 output.jpg
 # ffmpeg -i /home/madhekar/work/zsource/family/video/IMG_0005.MOV -r 0.25 output_%04d.png
+# ffmpeg -skip_frame nokey -i /home/madhekar/work/zsource/family/video/IMG_0005.MOV -vsync vfr thumb%04d.png -hide_banner
+# ffmpeg -skip_frame nokey -i /home/madhekar/work/zsource/family/video/IMG_0005.MOV -vf "select='gt(scene,0.4)'" -vsync vfr -q:v 1 -r 0.05 -s 224x224 frame%04d.jpg
+
+"""
+    -vsync vfr: discard the unused frames
+    -frame_pts true: use the frame index for image names, otherwise, the index starts from 1 and increments 1 each time
+"""
