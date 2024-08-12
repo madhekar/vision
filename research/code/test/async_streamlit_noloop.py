4c34c1deb2a0be23aeb5d7ad4e2dd8165a86c40a
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-08-12 13:41:58 -0700

    async calls refinement

---
+++ research/code/test/async_streamlit_noloop.py
@@ -0,0 +1,22 @@
+import asyncio
+import streamlit as st
+
+models = ["caption", "llm"]
+async def call_model(num):
+    st.write("Starting func {0}...".format(num))
+    await asyncio.sleep(1)
+    st.write("Ending func {0}...".format(num))
+
+async def create_tasks_func():
+    tasks = list()
+    for i in range(len(models)):
+        tasks.append(asyncio.create_task(call_model(models[i])))
+    await asyncio.wait(tasks)
+
+btn = st.button(label='call models')
+if btn:
+  loop = asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+  loop.run_until_complete(create_tasks_func())
+  st.rerun()
+  #loop.close()
