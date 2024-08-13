45d49dd03720da727740b77b3236400e2108c0f3
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-08-13 16:59:17 -0700

    async poc

---
+++ research/code/test/async_closed_events.py
@@ -0,0 +1,14 @@
+import asyncio
+import streamlit as st
+
+async def f():
+    st.write('starting...')
+    await asyncio.sleep(10)
+    st.write("ending...")
+
+loop = asyncio.new_event_loop()
+loop.run_until_complete(f())
+loop.close()
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
