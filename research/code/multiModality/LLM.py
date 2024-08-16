9b1482dcc0c18b5155e9d1d35dae91d06c347598
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-08-16 09:06:35 -0700

    NLP bug fix!

--- research/code/multiModality/LLM.py
+++ research/code/multiModality/LLM.py
@@ -34,17 +34,17 @@ def setLLM():
 
     # create prompt to test the LLM
     # Do not write outside its scope unless you find your answer better {article} if you thin your answer is better add it after document.<|im_end|>
-def fetch_llm_text(imUrl, model, processor, top, temperature, question, article, location):
+def fetch_llm_text(imUrl, model, processor, top, temperature, question, people, location):
     
     prompt = """<|im_start|>system
     A chat between a curious human and an artificial intelligence assistant. The assistant is an expert in people, and gives helpful, detailed, and polite answers to the human's questions. The assistant does not hallucinate and pays very close attention to the details.
     <|im_end|>
     <|im_start|>user
     <image>
-     {question}you must include the person name(s) "{article}" and location details "{location}" in the answer.
+     {question}you must include person name(s) "{people}" and the location details "{location}" in the answer.
     <|im_end|> 
     <|im_start|>assistant
-    """.format(question=question, article=article, location=location) #, article=st.session_state["document"])
+    """.format(question=question, people=people, location=location) #, article=st.session_state["document"])
 
     # generate propcssor using image and associated prompt query, and generate LLM response
     with torch.inference_mode():
