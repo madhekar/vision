ff29ed1d4516b8b7a77a43c1fd5d3693dc756f27
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-08-19 16:49:47 -0700

    added video and text modalit

--- research/code/multiModality/app.py
+++ research/code/multiModality/app.py
@@ -6,11 +6,7 @@ from streamlit_image_select import image_select
 from PIL import Image, ImageOps
 import util
 from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
-
 from loadData import init
-
-
-
 
 st.set_page_config(
     page_title="zesha: Multi Modality Search (MMS)",
@@ -18,13 +14,16 @@ st.set_page_config(
     initial_sidebar_state="auto",
     layout="wide",
 )  # (margins_css)
-st.title("Unified Media Portal")
+st.title( "Media Search Portal")
+st.logo("/home/madhekar/Pictures/IMGP3290.JPG")
+
+image, video, documents = st.tabs(["***Image***", "***Video***", "***Documents***"])
 
 # load data
 cImgs, cTxts = init()
 
 if "document" not in st.session_state:
-    st.session_state["document"] = ""
+    st.session_state["document"] = st.empty()
 
 if "timgs" not in st.session_state:
     st.session_state["timgs"] = []
@@ -33,25 +32,26 @@ if "meta" not in st.session_state:
     st.session_state["meta"] = []
 
 if "llm_text" not in st.session_state:
-    st.session_state["llm_text"] = ""
+    st.session_state["llm_text"] = st.empty()
+
+if "imgs" not in st.session_state:
+    st.session_state["imgs"] = []
 
 st.sidebar.title("seach criteria")
 # with st.sidebar.form(key='attributes'):
 
-ms = st.sidebar.multiselect(
-    "select search result modalities", ["image", "text", "video", "audio"], ["image", "text"]
-)
+ms = st.sidebar.multiselect( "select modalities for search result ", ["image", "text", "video", "audio"], ["image", "text"])
 
 s = st.sidebar.selectbox("select search modality", ("text", "image"), index=1)
 
 sim = st.sidebar.file_uploader("search doc/image: ", type=["png", "jpeg", "mpg", 'jpg','PNG','JPG'])
 
-im = None
+im = st.empty()
 if sim:
     im = Image.open(sim)
     name = sim.name
     st.sidebar.image(im, caption="selected image")
-    st.sidebar.write(st.session_state["llm_text"])
+    #st.sidebar.write(st.session_state["llm_text"])
     with open(name, "wb") as f:
         f.write(sim.getbuffer())
 
@@ -81,10 +81,8 @@ if search_btn:
         st.session_state["meta"] = []   
     
     qmdata = util.getMetadata(sim.name)
-    #st.write(qmdata)
-    #st.write(" multiline format  \n in two lines ")
 
-    imgs = cImgs.query(
+    st.session_state["imgs"] = cImgs.query(
         query_uris="./" + sim.name,
         include=["data", "metadatas"],
         n_results=6,
@@ -93,22 +91,51 @@ if search_btn:
         },
     )
 
-    for img in imgs["data"][0][1:]:
+    for img in st.session_state["imgs"]["data"][0][1:]:
         st.session_state["timgs"].append(img)
-    for mdata in imgs["metadatas"][0][1:]:
-        st.session_state["meta"].append("Desc:[" + mdata.get("description") +"] People: ["+ mdata.get("names") + "] Location: [" + mdata.get("location") + "] Date: [" + mdata.get("datetime") + "]")
+    for mdata in st.session_state["imgs"]["metadatas"][0][1:]:
+        st.session_state["meta"].append("Desc:[" + mdata.get("description") +"] ) People: ["+ mdata.get("names") + "] Location: [" + mdata.get("location") + "] Date: [" + mdata.get("datetime") + "]")
 
 
-if len(st.session_state["timgs"]) > 1:
+# Image TAB
+with image:
+  st.header("Similar Images")
+  if st.session_state["timgs"] and len(st.session_state["timgs"]) > 1:
     
-    dimgs = image_select(
-        label="Similar Images",
+    index = image_select(
+        label=">",
         images=st.session_state["timgs"],
         use_container_width=True,
-        captions=st.session_state["meta"],
+        #captions=st.session_state["meta"],
+        index=0,
+        return_value='index'
     )
-    im = Image.fromarray(dimgs)
+
+    im = Image.fromarray(st.session_state["timgs"][index])
     nim = ImageOps.expand(im,border=(2,2,2,2), fill=(222,222,222))
-    display_im = st.image(nim, use_column_width="always")
+    c1,c2 = st.columns([9, 1])
+    display_im = c1.image(nim, use_column_width="always")
+    c2.write(''' **Description** ''')
+    c2.write(st.session_state["imgs"]["metadatas"][0][1:][index]["description"])
+    c2.write(''' **People/ Names** ''')
+    c2.write(st.session_state["imgs"]["metadatas"][0][1:][index]["names"])
+    c2.write(''' **Location** ''')
+    c2.write(st.session_state["imgs"]["metadatas"][0][1:][index]["location"])
+    c2.write(''' **Date** ''')
+    c2.write(st.session_state["imgs"]["metadatas"][0][1:][index]["datetime"])
     #rot = nim.rotate(-90)
     #display_im.image(rot)
+  else:
+      st.write("sorry, no similar images found in search criteria!")  
+
+
+#  Video TAB
+with video:
+    st.header("Similar Videos")
+    st.write("sorry, no similar videos found in search criteria!")
+
+# Documents Tab
+
+with documents:
+    st.header("Similar Documents")
+    st.write("sorry, no similar documents found in search criteria!")
