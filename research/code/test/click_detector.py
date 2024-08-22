8423d4764b9c289e4df816f2840a397f1c14064a
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-08-21 17:28:06 -0700

    clickable images

--- research/code/test/click_detector.py
+++ research/code/test/click_detector.py
@@ -1,10 +1,20 @@
 import streamlit as st
 from st_click_detector import click_detector
+import base64
 
-content ="""
-    <p><a href='#' id='Link 1'>First link</a></p>
-    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
-    """
+def encode_img(img_fn):
+    with open(img_fn, "rb") as f:
+        data = f.read()
+        return base64.b64encode(data).decode()
+    
+content = f"<a href='#' id='Image 1'><img src='data:image/png;base64, {encode_img('/home/madhekar/work/zsource/zesha-high-resolution-logo.jpeg')}' alt='zesha image'></a>"
+
 clicked = click_detector(content)
 
-st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
+st.markdown(
+    f"**{clicked} clicked**" if clicked != "" else "**No click**",
+    unsafe_allow_html=True
+)
+
+#<p><a href='#' id='Link 1'>First link</a></p>
+# <img src='data:image/png;base64,{encoded_string}' alt='logo image' style='width:500px;height:500px;'>
--- research/code/test/streamlit_image_button.py
+++ research/code/test/streamlit_image_button.py
@@ -16,7 +16,7 @@ with open("/home/madhekar/Pictures/IMG_0070.JPG", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read()).decode()
 
 # encoded =  base64.b64encode(Image.open('/home/madhekar/Pictures/IMG_0070.JPG'))
-html= f"<a href='https://www.example.com'><img src='data:image/png;base64,{encoded_string}' alt='logo image' style='width:500px;height:500px;'></a>"
+html= f"<a href='https://www.example.com'><img src='data:image/png;base64,{encode_img('/home/madhekar/Pictures/IMG_0070.JPG')}' alt='logo image' style='width:500px;height:500px;'></a>"
 
 st.markdown(html, unsafe_allow_html=True)
 
