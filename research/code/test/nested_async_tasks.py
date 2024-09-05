2564d0b114613ecba1ca60cf5327d038029f3451
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-09-05 16:18:13 -0700

    semaphore does not used with gather

--- research/code/test/nested_async_tasks.py
+++ research/code/test/nested_async_tasks.py
@@ -1,33 +1,65 @@
 import asyncio
+import glob
 import os
+import uuid
+from random import randint
+
+sem = asyncio.Semaphore(2)
+
+def getRecursive(rootDir):
+    f_list=[]
+    for fn in glob.glob(rootDir + '/**/*', recursive=True):
+        if not os.path.isdir(os.path.abspath(fn)):
+            f_list.append(os.path.abspath(fn))
+    return f_list 
 
 class EmbeddingClass:
-    async def master_method(self, uri):
-      tasks = [self.sub_method(f) for f in os.listdir(uri)]
-      results = await asyncio.gather(*tasks)
-      print(results) 
+
+    async def safeMaster(self, iList):
+      async with sem:  # semaphore limits num of simultaneous downloads
+        return await self.master_method(iList)
+
+    async def master_method(self, imgList):
+      #async with sem:
+         tasks = [asyncio.ensure_future(self.sub_method(f)) for f in imgList]
+         return await asyncio.gather(*tasks)
 
     async def sub_method(self, uri):
-      subtasks = [self.generateId(), self.timestamp(uri), self.locationDetails(uri), self.namesOfPeople(uri), self.describeImage(uri) ]
-      results = await asyncio.gather(*subtasks)
-      print(results) 
+         subtasks = [asyncio.ensure_future(self.generateId()), 
+                  asyncio.ensure_future(self.timestamp(uri)), 
+                  asyncio.ensure_future(self.locationDetails(uri)), 
+                  asyncio.ensure_future(self.namesOfPeople(uri)), 
+                  asyncio.ensure_future(self.describeImage(uri)) ]
+         return await asyncio.gather(*subtasks)
+
 
     async def generateId(self):
-      return "generateId done"  
+      return { 'id': str(uuid.uuid4()) }  
     
     async def timestamp(self, uri):
-      return "timestamp done: "   
+      wait_time = randint(1, 3)
+      await asyncio.sleep(wait_time)
+      return f"timestamp done."   
     
     async def locationDetails(self, uri):
-      return "locationDetails done: "   
+      wait_time = randint(1, 3)
+      await asyncio.sleep(wait_time)
+      return f"locationDetails done."   
     
     async def namesOfPeople(self, uri):
-      return "namesOfPeople done: "
+      wait_time = randint(3, 7)
+      await asyncio.sleep(wait_time)
+      return f"namesOfPeople done."
 
     async def describeImage(self,uri):
-      return "describeImage done: "
+      wait_time = randint(5, 10)
+      await asyncio.sleep(wait_time)
+      return f"describeImage done."
     
 em = EmbeddingClass()
 
-asyncio.run(em.master_method('.'))
-print('do normal stuff..')
+
+
+iList = getRecursive('/Users/emadhekar/erase_me/images/')
+r = asyncio.run(em.safeMaster(iList))
+print('do normal stuff..', r)
