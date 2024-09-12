6ff17fc1305e0843a30965f37dc601b8ad40dd33
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-09-12 13:34:43 -0700

    code clenup

--- research/code/test/loadData.py
+++ research/code/test/loadData.py
@@ -6,10 +6,17 @@ with open("data.json", mode="r") as f:
     for line in f:
         data.append(json.loads(line))
 
-print(type(data[0]))
+#print(type(data[0]))
 
-print(data[0]["loc"])
+#print(data[0]["loc"])
 
 df =  pd.DataFrame(data)
 
-print(df["id"])
+#print(df.head())
+
+d_id = df["id"]
+d_metadata = df[["timestamp","lat","lon","loc","nam","txt"]].T.to_dict().values()
+d_url = df["url"]
+
+print(f'id: \n {d_id.head()} \n metadata: \n {d_metadata} \n url: \n {d_url.head()} ')
+
