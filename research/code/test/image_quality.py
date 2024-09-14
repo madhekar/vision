ff82fe97c0848af67b6bd4cab1ed323418afe6ae
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-09-14 12:36:58 -0700

    check image quality before processing

---
+++ research/code/test/image_quality.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+img = cv.imread("/Users/bmadhekar/Pictures/square.jpg")
+grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+blurScore = cv.Laplacian(grey, cv.CV_64F).var()
+score = cv.quality.QualityBRISQUE_compute(img, "brisque_model_live.yml", "brisque_range_live.yml")
+
+print(f' >> Blur Score: {blurScore}')
+print(f' >> BRISQUE Score: {score}')
+
+cv.namedWindow("Output", cv.WINDOW_NORMAL)
+cv.imshow("Output", img)
+k = cv.waitKey(0)
