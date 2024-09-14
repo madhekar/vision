21c12dcb8d1843fa092922e5d6d523e4d0925675
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-09-13 17:16:54 -0700

    code cleanup

--- research/code/test/loadData.py
+++ research/code/test/loadData.py
@@ -1,11 +1,11 @@
 import json
 import pandas as pd
+import yaml
+import os
 
-
-
-def load_metadata():
+def load_metadata(metadata_path, metadata_file):
   data = []
-  with open("data.json", mode="r") as f:
+  with open(os.path.join(metadata_path, metadata_file), mode="r") as f:
     for line in f:
         data.append(json.loads(line))
 
@@ -13,6 +13,7 @@ def load_metadata():
   return df
 
 def populate_vectordb(df):   
+  
   df_id = df["id"]
   df_metadata = df[["timestamp","lat","lon","loc","nam","txt"]].T.to_dict().values()
   df_url = df["url"]
@@ -20,7 +21,17 @@ def populate_vectordb(df):
   print(f'id: \n {df_id.head()} \n metadata: \n {df_metadata} \n url: \n {df_url.head()} ')
 
 if __name__=='__main__':
+    with open("metadata.yaml") as prop:
+        dict = yaml.safe_load(prop)
 
-   df_vector_database = load_metadata()
+        print("* * * * * * * * * * * Metadata Generator Properties * * * * * * * * * * * *")
+        print(dict)
+        print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
 
-   populate_vectordb()
+    metadata_path = dict["metadata"]["metadata_path"]
+    metadata_file = dict["metadata"]["metadata_file"]
+    image_dir_path = dict["vectordb"]["vectordb_path"]   
+
+    df_vector_database = load_metadata(metadata_path=metadata_path, metadata_file=metadata_file)
+
+    populate_vectordb(df_vector_database)
