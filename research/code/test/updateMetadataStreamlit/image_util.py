492499a093de5fdc6b3b10bbe9f20a45ad4bd553
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-01 16:10:53 -0700

    display image in markdown

---
+++ research/code/test/updateMetadataStreamlit/image_util.py
@@ -0,0 +1,14 @@
+import base64
+
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+
+def img_to_html(img_path):
+    img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+        img_to_bytes(img_path)
+    )
+    return img_html
