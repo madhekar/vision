5a4b389da4d374888ae9123a9ac7af1f325381c1
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-09 12:58:15 -0700

    refactor 3

---
+++ research/code/multiModality/generate-metadata-util/LLM.py
@@ -0,0 +1,79 @@
+
+import streamlit as st
+import torch
+from PIL import Image
+from transformers import AutoTokenizer, AutoProcessor, AutoModel
+from transformers import TextStreamer
+
+"""
+Automodel is a term used to describe a machine learning model that can be automatically generated or trained. 
+This can be done in a variety of ways, such as using AutoML tools or by using techniques such as transfer learning.
+Automodels are often used in machine learning because they can significantly reduce the time and effort required to develop and deploy models. 
+For example, an AutoML tool can be used to automatically generate a variety of different model architectures and then train and evaluate them on a given dataset. 
+This can help users to quickly identify the best model architecture for their specific needs.
+Automodels are also becoming increasingly popular in the field of natural language processing (NLP). 
+This is because AutoNLP tools can be used to automatically generate and train language models for a variety of tasks, such as text classification, question answering, and machine translation.
+"""
+#@st.cache_resource(ttl=36000, show_spinner=True)
+def setLLM():
+    """
+    model auto-tokenizer and processor components for LLM model MC-LLaVA-3b with trust flag
+    """
+
+    model = AutoModel.from_pretrained("visheratin/MC-LLaVA-3b", trust_remote_code=True)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        "visheratin/MC-LLaVA-3b", trust_remote_code=True, batched=True
+    )
+
+    processor = AutoProcessor.from_pretrained(
+        "visheratin/MC-LLaVA-3b", trust_remote_code=True
+    )
+
+    return model, tokenizer, processor
+
+    # create prompt to test the LLM
+    # Do not write outside its scope unless you find your answer better {article} if you thin your answer is better add it after document.<|im_end|>
+def fetch_llm_text(imUrl, model, processor, top, temperature, question, people, location):
+    
+    print("calling LLM")
+
+    prompt = """<|im_start|>system
+    A chat between a curious human and an artificial intelligence assistant. The assistant is an expert in people, and gives helpful, detailed, and polite answers to the human's questions. The assistant does not hallucinate and pays very close attention to the details.
+    <|im_end|>
+    <|im_start|>user
+    <image>
+     {question}you must include person name(s) "{people}" and the location details "{location}" in the answer.
+    <|im_end|> 
+    <|im_start|>assistant
+    """.format(question=question, people=people, location=location) #, article=st.session_state["document"])
+
+    # generate propcssor using image and associated prompt query, and generate LLM response
+    with torch.inference_mode():
+        inputs = processor(
+            prompt, [Image.open(imUrl)], model, max_crops=10, num_tokens=100        #100, 728
+            )
+        
+    # streamer 
+    streamer = TextStreamer(processor.tokenizer)
+
+    # model generator
+    with torch.inference_mode():
+        output =  model.generate(
+            **inputs,
+            max_new_tokens=70,  #200
+            do_sample=True,
+            use_cache=False,
+            top_p=top,
+            temperature=temperature,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            streamer=streamer,
+            #return_dict_in_generate=True,
+            #output_scores=True
+            
+        )
+
+    result = processor.tokenizer.decode(output[0])
+    result = result.replace(prompt, "").replace("<|im_end|>", "").replace("<|im_start|>", "")
+    return result
+    
---
+++ research/code/multiModality/generate-metadata-util/load_vectordb.py
@@ -0,0 +1,150 @@
+import json
+import pandas as pd
+import yaml
+import util
+import os
+import uuid
+import chromadb as cdb
+from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
+
+from chromadb.utils.data_loaders import ImageLoader
+from chromadb.config import Settings
+
+def load_metadata(metadata_path, metadata_file, image_final_path, image_final_folder):
+    data = []
+    with open(os.path.join(metadata_path, metadata_file), mode="r") as f:
+        for line in f:
+            data.append(json.loads(line))
+
+        df = pd.DataFrame(data)
+
+        df["url"] = df["url"].str.replace(
+            "input-data/img",
+            "final-data/img/" + image_final_folder,
+        )
+    return df
+
+
+def createVectorDB(df_data, vectordb_dir_path, image_collection_name, text_folder, text_collection_name):
+    
+    # vector database persistance
+    client = cdb.PersistentClient( path=vectordb_dir_path, settings=Settings(allow_reset=True))
+
+    # reset chromadb persistant store
+    # client.reset()
+
+    # list of collections
+    collections_list = [c.name for c in client.list_collections()]
+
+    # openclip embedding function!
+    embedding_function = OpenCLIPEmbeddingFunction()
+
+    # Image collection inside vector database 'chromadb'
+    image_loader = ImageLoader()
+
+    # collection images defined
+    collection_images = client.get_or_create_collection(
+        name=image_collection_name,
+        embedding_function=embedding_function,
+        data_loader=image_loader,
+    )
+
+    """
+    IMAGE embeddings in vector database
+    """
+    if image_collection_name not in collections_list:
+        # create list of image urls to embedded in vector db
+
+        df_urls = df_data["url"]
+
+        # create unique uuids for each image
+        df_ids = df_data["id"]
+
+        df_metadatas = df_data[["timestamp", "lat", "lon", "loc", "nam", "txt"]].T.to_dict().values()
+  
+        collection_images.add(ids=df_ids.tolist(), metadatas=list(df_metadatas), uris=df_urls.tolist())
+
+        print(f"id: \n {df_ids.head()} \n metadata: \n {df_metadata} \n url: \n {df_urls.head()} ")
+
+    """
+       Text collection inside vector database 'chromadb'
+    """
+    collection_text = client.get_or_create_collection(
+        name=text_collection_name,
+        embedding_function=embedding_function,
+    )
+
+    """
+      TEXT Embeddings on vector database
+    """
+    if text_collection_name not in collections_list:
+        text_pth = sorted(
+            [
+                os.path.join(text_folder, document_name)
+                for document_name in os.listdir(text_folder)
+                if document_name.endswith(".txt")
+            ]
+        )
+
+        print("=> text paths: \n", "\n".join(text_pth))
+
+        list_of_text = []
+
+        for text in text_pth:
+            with open(text, "r") as f:
+                text = f.read()
+                list_of_text.append(text)
+
+        ids_txt_list = [str(uuid.uuid4()) for _ in range(len(list_of_text))]
+
+        print("=> text generate ids:\n", ids_txt_list)
+
+        collection_text.add(documents=list_of_text, ids=ids_txt_list)
+
+    return collection_images, collection_text
+
+
+def archive_metadata(metadata_path, arc_folder_name, metadata_file):
+    new_archive_folder = os.path.join(
+        metadata_path,
+        arc_folder_name,
+    )
+    if not os.path.exists(new_archive_folder):
+        os.mkdir(new_archive_folder)
+        os.rename(os.path.join(metadata_path, metadata_file), os.path.join(new_archive_folder, metadata_file))
+
+
+     
+
+if __name__=='__main__':
+    with open("metadata_conf.yaml") as prop:
+        dict = yaml.safe_load(prop)
+
+        print("* * * * * * * * * * * * Metadata Load Properties * * * * * * * * * * * * * *")
+        print(dict)
+        print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+
+    metadata_path = dict["metadata"]["metadata_path"]
+    metadata_file = dict["metadata"]["metadata_file"]
+
+    vectordb_dir_path = dict["vectordb"]["vectordb_path"]   
+    image_collection_name = dict["vectordb"]["image_collection_name"]
+    text_collection_name = dict["vectordb"]["text_collection_name"]
+    text_folder_name = dict["vectordb"]["text_dir_path"]
+
+    image_initial_path = dict["metadata"]["image_dir_path"]
+    image_final_path = dict["prod"]["image_final_path"]
+
+    arc_folder_name = util.get_foldername_by_datetime()
+
+    #copy images in input-data to final-data/datetime
+    util.copy_folder_tree(image_initial_path, os.path.join(image_final_path, arc_folder_name) )
+
+    df_metadata = load_metadata(metadata_path=metadata_path, metadata_file=metadata_file, image_final_path=image_final_path, image_final_folder=arc_folder_name)
+
+    createVectorDB(df_metadata, vectordb_dir_path, image_collection_name, text_folder_name, text_collection_name)
+
+    archive_metadata(metadata_path, arc_folder_name, metadata_file)
+
+    util.remove_files_folders(image_initial_path)
+
