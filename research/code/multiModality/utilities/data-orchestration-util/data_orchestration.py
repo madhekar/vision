9f1838167c6b42bc5687d86a93df36072b8ba0d8
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-15 13:32:59 -0700

    data orchestration

--- research/code/multiModality/utilities/data-orchestration-util/data_orchestration.py
+++ research/code/multiModality/utilities/data-orchestration-util/data_orchestration.py
@@ -2,23 +2,25 @@
 import os
 import yaml
 import pprint
-
+import streamlit as st
 
 def build_orch_structure():
     """
-        idx, user-folder-name, state,
+    idx, user-folder-name, state,
     """
 
-def extract_user_raw_data_folders(path):
-   next(os.walk(path))[1]
+def extract_user_raw_data_folders(pth):
+   next(os.walk(pth))[1]
 
+@st.cache_resource
 def config_load():
     with open("data_orchestration_conf.yaml") as prop:
         dict = yaml.safe_load(prop)
 
-        pprint.pprint("* * * * * * * * * * * Data Orchestration Properties * * * * * * * * * * * *")
+        pprint.pprint("* * * * * * * * *  * Data Orchestration Properties * * * * * * * * * * *")
         pprint.pprint(dict)
-        pprint.pprint("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ")
+        pprint.pprint("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+
         raw_data_path = dict["raw-data"]["base_path"]
         duplicate_data_path = dict["duplicate"]["base_path"]
         quality_data_path = dict["quality"]["base_path"]
@@ -26,12 +28,19 @@ def config_load():
         metadata_file_path = dict["metadata"]["base_path"]
         static_metadata_file_path = dict["static-metadata"]["base_path"]
         vectordb_path = dict["vectordb"]["base_path"]
+
     return (
-        input_image_path,
-        missing_metadata_path,
-        missing_metadata_file,
+        raw_data_path,
+        duplicate_data_path,
+        quality_data_path,
+        missing_data_path,
+        metadata_file_path,
+        static_metadata_file_path,
+        vectordb_path
     )
 
 if __name__ == '__main__':
-    config_load()
+    rwp, ddp, qdp, mdp, sfp, vdbp = config_load()
+
+    raw_folders = extract_user_raw_data_folders(rwp)
   
