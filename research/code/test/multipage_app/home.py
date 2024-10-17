23548a37bd5f3255472afe896592d0f9590ccfc4
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-17 16:17:08 -0700

    poc

--- research/code/test/multipage_app/home.py
+++ research/code/test/multipage_app/home.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-
-st.title("At Home Media Portal")
-
 data_orchestration = st.Page(
     page='pages/data_orchestration.py',
     title="Data orchistration Workflow",
-    icon=":material/smart_toy:"
+    icon=":material/smart_toy:",
+    default=True
 )
 
 data_correction = st.Page(
@@ -14,3 +12,15 @@ data_correction = st.Page(
     title="Data ofix Workflow",
     icon=":material/smart_toy:"
 )
+
+pg = st.navigation(
+    {
+    "workflow": [data_orchestration],
+    "correction": [data_correction]
+    }
+    )
+
+st.logo("assets/zesha-high-resolution-logo.jpeg")
+st.sidebar.text("At Home Media Portal")
+
+pg.run()
