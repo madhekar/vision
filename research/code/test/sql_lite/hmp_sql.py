5380463175278fd89b8dbc384f05f817d760d56d
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-17 12:01:24 -0700

    sql poc

---
+++ research/code/test/sql_lite/hmp_sql.py
@@ -0,0 +1,11 @@
+import os
+import sqlite3 as sl3
+
+def create_or_connect_database(db_path, db_name):
+    return sl3.connect(os.path.join(db_path, db_name))
+
+def create_tables(con):
+    cur = con.cursor()
+    cur.execute("CREATE TABLE store(name string primary key, description string, serial_number string, timestamp string)")
+    cur.execute("CREATE TABLE store_attributes(store_name string foreign key, duplicate_files number(10), bad_quality_files number(10), missing_data_files number(10), metadata_generated_files number(10), vector_db_loaded_files number(10), memory_usage string )")
+    cur.execute("CREATE TABLE transactions(store_name string foreign key, folders_added number(10), files_added number(10), timestamp string )")
