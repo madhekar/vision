f401d3feba382240a9d43f0d36d5e61d2aa86d47
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-17 14:01:14 -0700

    design progress

--- research/code/test/sql_lite/hmp_sql.py
+++ research/code/test/sql_lite/hmp_sql.py
@@ -7,12 +7,15 @@ def create_or_connect_database(db_path, db_name):
 
 def create_tables(con):
     cur = con.cursor()
-    cur.execute("CREATE TABLE store(name string primary key, description string, serial_number string, timestamp string)")
-    cur.execute("CREATE TABLE store_attributes(store_name string foreign key, duplicate_files number(10), bad_quality_files number(10), missing_data_files number(10), metadata_generated_files number(10), vector_db_loaded_files number(10), memory_usage string )")
-    cur.execute("CREATE TABLE transactions(store_name string foreign key, folders_added number(10), files_added number(10), timestamp string )")
+    cur.execute("CREATE TABLE storage(name string primary key, description string, serial_number string, timestamp string)")
+    cur.execute("CREATE TABLE storage_audit(storage_name string foreign key, duplicate_files number(10), bad_quality_files number(10), missing_data_files number(10), metadata_generated_files number(10), vector_db_loaded_files number(10), memory_usage string, folders_added number(10), files_added number(10), timestamp string )")
     cur.commit()
 
-def initialize_store(con, store_name, description, serial_number):
+def initialize_storage(con, storage_name, description, serial_number):
     cur = con.cursor()
-    cur.execute("INSERT INTO store VALUES(?, ?, ?, ?))", (store_name, description, serial_number, datetime.now())) 
+    cur.execute("INSERT INTO storage VALUES(?, ?, ?, ?))", (storage_name, description, serial_number, datetime.now())) 
     cur.commit()
+
+def upsert_storage_audit(con, sn, df, bqf, mdf, mgf, vlf, mu, fod, fid ):
+    cur = con.cursor()
+    cur.execute("INSERT INTO storage_audit VALUES (?,?,?,?,?,?,?,?,?)", (sn, df, bqf, mdf, mgf, vlf, mu, fod, fid, datetime.now()))
