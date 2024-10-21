148847fd7f7eb2a869f3283933e7b2863a975f68
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-21 16:07:16 -0700

    poc

--- research/code/test/multipage_app/home.py
+++ research/code/test/multipage_app/home.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import streamlit_init as sti
-
+import os
 
 """
 https://docs.streamlit.io/get-started/tutorials/create-a-multipage-app
 """
 sti.initUI()
 
+def load_css(file_path):
+    with open(file=file_path) as f:
+      st.html(f'<style>{f.read()}</style>')
+
+css_path = os.path.join('assets', 'styles.css')      
 overview = st.Page(
     page="pages/overview.py",
     title=st.markdown("""# Overview"""),
@@ -22,7 +27,7 @@ storage_initialization = st.Page(
 
 data_orchestration = st.Page(
     page='pages/data_orchestration.py',
-    title="Data Orchistration",
+    title="Data Orchestration",
     icon=":material/group_work:",
 )
 
