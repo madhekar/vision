a8951f07da64bbfc31cd627ed813fdfdd5db344d
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-22 15:40:22 -0700

    poc

---
+++ research/code/test/streamlit_elements/button_color_change.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+def ChangeButtonColour(wgt_txt, wch_hex_colour = '12px'):
+    htmlstr = """<script>var elements = window.parent.document.querySelectorAll('*'), i;
+                for (i = 0; i < elements.length; ++i) 
+                    { if (elements[i].innerText == |wgt_txt|) 
+                        { elements[i].style.color ='""" + wch_hex_colour + """'; } }</script>  """
+
+    htmlstr = htmlstr.replace('|wgt_txt|', "'" + wgt_txt + "'")
+    components.html(f"{htmlstr}", height=0, width=0)
+
+cols = st.columns(4)
+cols[0].button('no colour', key='b1')
+cols[1].button('green', key='b2')
+cols[2].button('red', key='b3')
+cols[3].button('no colour', key='b4')
+
+ChangeButtonColour('green', '#4E9F3D') # button txt to find, colour to assign
+ChangeButtonColour('red', '#FF0000') # button txt to find, colour to assign
+
