8a9393109c856ac1dfeadf52406dcf309a7dfb87
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-23 11:46:08 -0700

    workflow POC

--- research/code/test/multipage_app/util.py
+++ research/code/test/multipage_app/util.py
@@ -1,18 +1,45 @@
+import os
 import streamlit as st
 import streamlit.components.v1 as components
 
+
+
+st.set_page_config(
+    page_title="zesha: Home Media Portal (HMP)",
+    page_icon="/home/madhekar/work/zsource/zesha-high-resolution-logo.jpeg",
+    initial_sidebar_state="auto",
+    layout="wide",
+)
+
+def load_css(css_path):
+    with open(file=css_path) as f:
+        s = f"<style>{f.read()}</style>"
+        st.html(s)
+
+
+css_path = os.path.join("assets", "styles.css")
+
+load_css(css_path)
 
 mystate = st.session_state
 if "btn_prsd_status" not in mystate:
     mystate.btn_prsd_status = [False] * 6
 
-btn_labels = ["load data check", "de-duplication check", "quality check", "missing metadata check", "fix Metadata check", "metadata loader check" ]
+btn_labels = ["load data check", "duplicate check", "quality check", "metadata check", "fix metadata", "data loader check" ]
 unpressed_colour = "#707070"
-pressed_colour = "#4CAF50"
+success_colour = "#4CAF50"
+failure_color = "#FF7F50"
+wip_color = "#FFD700"
 
 
 def ChangeButtonColour(widget_label, prsd_status):
-    btn_bg_colour = pressed_colour if prsd_status == True else unpressed_colour
+    btn_bg_colour = success_colour if prsd_status == True else unpressed_colour
+    if prsd_status == 1:
+        btn_bg_colour = success_colour
+    elif prsd_status == 2:
+        btn_bg_colour = failure_color
+    else:
+        btn_bg_colour = wip_color    
     htmlstr = f"""
         <script>
             var elements = window.parent.document.querySelectorAll('button');
@@ -31,17 +58,17 @@ def ChkBtnStatusAndAssignColour():
         ChangeButtonColour(btn_labels[i], mystate.btn_prsd_status[i])
 
 
-def btn_pressed_callback(i):
-    mystate.btn_prsd_status = [False] * 6
-    mystate.btn_prsd_status[i - 1] = True
+def btn_pressed_callback(i,j):
+    #mystate.btn_prsd_status = [False] * 6
+    mystate.btn_prsd_status[i - 1] = j
 
 
 with st.container():
-    c1, c2, c3, c4, c5,c6 = st.columns((1, 1, 1, 1, 1, 1))
-    c1.button("load data check", key="b1", on_click=btn_pressed_callback, args=(1,))
-    c2.button("de-duplication check", key="b2", on_click=btn_pressed_callback, args=(2,))
-    c3.button("quality check", key="b3", on_click=btn_pressed_callback, args=(1,))
-    c4.button("missing metadata check", key="b4", on_click=btn_pressed_callback, args=(2,))
-    c5.button("fix Metadata check", key="b5", on_click=btn_pressed_callback, args=(1,))
-    c6.button("metadata loader check", key="b6", on_click=btn_pressed_callback, args=(2,))
+    c1, c2, c3, c4, c5,c6 = st.columns ((1, 1, 1, 1, 1, 1), gap='small')
+    c1.button("load data check", key="g1", on_click=btn_pressed_callback, args=(1,1))
+    c2.button("duplicate check", key="g2", on_click=btn_pressed_callback, args=(2,3))
+    c3.button("quality check", key="g3", on_click=btn_pressed_callback, args=(3,2))
+    c4.button("metadata check", key="g4", on_click=btn_pressed_callback, args=(4,1))
+    c5.button("fix metadata", key="g5", on_click=btn_pressed_callback, args=(5,2))
+    c6.button("data loader check", key="g6", on_click=btn_pressed_callback, args=(6,2))
     ChkBtnStatusAndAssignColour() 
