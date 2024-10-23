1b24c0825163e699436a8db23dea17b651d10372
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-23 14:01:50 -0700

    POC workflow

--- research/code/test/multipage_app/util.py
+++ research/code/test/multipage_app/util.py
@@ -1,4 +1,6 @@
 import os
+import time
+import asyncio 
 import streamlit as st
 import streamlit.components.v1 as components
 
@@ -26,26 +28,29 @@ if "btn_prsd_status" not in mystate:
     mystate.btn_prsd_status = [False] * 6
 
 btn_labels = ["load data check", "duplicate check", "quality check", "metadata check", "fix metadata", "data loader check" ]
-unpressed_colour = "#707070"
-success_colour = "#4CAF50"
+unpressed_color = "#707070"
+success_color = "#4CAF50"
 failure_color = "#FF7F50"
 wip_color = "#FFD700"
 
 
-def ChangeButtonColour(widget_label, prsd_status):
-    btn_bg_colour = success_colour if prsd_status == True else unpressed_colour
-    if prsd_status == 1:
-        btn_bg_colour = success_colour
+def ChangeButtoncolor(widget_label, prsd_status):
+    btn_bg_color = success_color if prsd_status == True else unpressed_color
+    if prsd_status == 0:
+        btn_bg_color = unpressed_color
+    elif prsd_status == 1:
+        btn_bg_color = success_color
     elif prsd_status == 2:
-        btn_bg_colour = failure_color
+        btn_bg_color = failure_color    
     else:
-        btn_bg_colour = wip_color    
+        btn_bg_color = wip_color
     htmlstr = f"""
         <script>
             var elements = window.parent.document.querySelectorAll('button');
             for (var i = 0; i < elements.length; ++i) {{ 
                 if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].style.background = '{btn_bg_colour}'
+                    elements[i].style.background = '{btn_bg_color}'
+                    elements[i].style.animation ='pulse 3s'
                 }}
             }}
         </script>
@@ -53,22 +58,47 @@ def ChangeButtonColour(widget_label, prsd_status):
     components.html(f"{htmlstr}", height=0, width=0)
 
 
-def ChkBtnStatusAndAssignColour():
+def ChkBtnStatusAndAssigncolor():
     for i in range(len(btn_labels)):
-        ChangeButtonColour(btn_labels[i], mystate.btn_prsd_status[i])
+        ChangeButtoncolor(btn_labels[i], mystate.btn_prsd_status[i])
 
 
-def btn_pressed_callback(i,j):
+def btn_pressed_callback(i):
     #mystate.btn_prsd_status = [False] * 6
-    mystate.btn_prsd_status[i - 1] = j
+    r = exec_task(i)
+    mystate.btn_prsd_status[i - 1] = r
 
+
+def exec_task(iTask):
+    match iTask:
+        case 1:
+            print('start')
+            time.sleep(10)
+            print('done')
+            return 1
+        case 2:
+            time.sleep(1)
+            return 1
+        case 3:
+            time.sleep(1)
+            return 2
+        case 4:
+            return 0
+        case 5:
+            return 0
+        case 6: 
+            return 0
+        case _:
+            return -1
 
 with st.container():
+    st.title("workflow")
+    st.subheader('execute individual tasks: ')
     c1, c2, c3, c4, c5,c6 = st.columns ((1, 1, 1, 1, 1, 1), gap='small')
-    c1.button("load data check", key="g1", on_click=btn_pressed_callback, args=(1,1))
-    c2.button("duplicate check", key="g2", on_click=btn_pressed_callback, args=(2,3))
-    c3.button("quality check", key="g3", on_click=btn_pressed_callback, args=(3,2))
-    c4.button("metadata check", key="g4", on_click=btn_pressed_callback, args=(4,1))
-    c5.button("fix metadata", key="g5", on_click=btn_pressed_callback, args=(5,2))
-    c6.button("data loader check", key="g6", on_click=btn_pressed_callback, args=(6,2))
-    ChkBtnStatusAndAssignColour() 
+    c1.button("load data check", key="g1", on_click=btn_pressed_callback, args=(1,))
+    c2.button("duplicate check", key="g2", on_click=btn_pressed_callback, args=(2,))
+    c3.button("quality check", key="g3", on_click=btn_pressed_callback, args=(3,))
+    c4.button("metadata check", key="g4", on_click=btn_pressed_callback, args=(4,))
+    c5.button("fix metadata", key="g5", on_click=btn_pressed_callback, args=(5,))
+    c6.button("data loader check", key="g6", on_click=btn_pressed_callback, args=(6,))
+    ChkBtnStatusAndAssigncolor() 
