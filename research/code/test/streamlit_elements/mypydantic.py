ae8f9c0a38278d49c3ba650335df6260328b7269
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-29 14:30:36 -0700

    POC pydantic and st table

--- research/code/test/streamlit_elements/mypydantic.py
+++ research/code/test/streamlit_elements/mypydantic.py
@@ -1,16 +1,30 @@
+import os
 import dataclasses
-import json
+import numpy as np
+import pandas as pd
+#import json
 
 import streamlit as st
-from pydantic.json import pydantic_encoder
+#from pydantic.json import pydantic_encoder
 
 import streamlit_pydantic as sp
 
 
+static_metadata_path= '/home/madhekar/work/home-media-app/data/app-data/static-metadata/'
+static_metadata_file= 'static-metadata.csv'
+
+
+
+df1 = (pd.read_csv(os.path.join(static_metadata_path, static_metadata_file))) 
+print(df1.columns)
+#df1.columns(['name', 'desc', 'lat', 'lon'])
+my_table = st.table(df1)
+
+print(my_table)
 @dataclasses.dataclass
 class LocationModel:
     name: str = 'unique location name key'
-    desc: str = 'free form location description'
+    nesc: str = 'free form location description'
     lat: float = 0.0
     lon: float = 0.0
 
@@ -18,3 +32,5 @@ class LocationModel:
 data = sp.pydantic_form(key="zesha_loc_form", model=LocationModel)
 if data:
     st.write(dataclasses.asdict(data))
+    st.write(pd.DataFrame.from_dict(dataclasses.asdict(data), orient="index"))
+    my_table.add_rows(pd.DataFrame.from_dict(dataclasses.asdict(data), orient='index').transpose())
