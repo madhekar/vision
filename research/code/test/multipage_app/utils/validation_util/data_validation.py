ae919da5a066ad85e4957883823c92878366b1a7
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-10-31 15:05:13 -0700

    refactor validations

--- research/code/test/multipage_app/utils/validation_util/data_validation.py
+++ research/code/test/multipage_app/utils/validation_util/data_validation.py
@@ -60,45 +60,7 @@ def execute():
 
     orchestrator()
 
-    
-
-# @st.cache_resource
-# def config_load():
-#     with open("data_orchestration_conf.yaml") as prop:
-#         dict = yaml.safe_load(prop)
-
-#         pprint.pprint("* * * * * * * * *  * Data Orchestration Properties * * * * * * * * * * *")
-#         pprint.pprint(dict)
-#         pprint.pprint("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
-
-#         raw_data_path = dict["raw-data"]["base_path"]
-#         duplicate_data_path = dict["duplicate"]["base_path"]
-#         quality_data_path = dict["quality"]["base_path"]
-#         missing_data_path = dict["missing"]["base_path"]
-#         metadata_file_path = dict["metadata"]["base_path"]
-#         static_metadata_file_path = dict["static-metadata"]["base_path"]
-#         vectordb_path = dict["vectordb"]["base_path"]
-
-#     return (
-#         raw_data_path,
-#         duplicate_data_path,
-#         quality_data_path,
-#         missing_data_path,
-#         metadata_file_path,
-#         static_metadata_file_path,
-#         vectordb_path
-#     )
-
-
 if __name__ == '__main__':
-    # rwp, ddp, qdp, mdp,mfp, smfp, vdbp = config.data_validation_config_load()
-
-    # raw_folders = extract_user_raw_data_folders(rwp)
-
-    # dfo = build_orch_structure(raw_folders)
-
-    # orchestrator()
-
     execute()
    
   
