b89327816713a4ccfc93d00ace7bf466c80ffc7d
bhalchandra madhekar <bmadhekar@gmail.com>
2024-11-05 15:20:19 -0800

    notes and bug fix

--- research/code/test/folder_structure_lineage/count_filetypes.py
+++ research/code/test/folder_structure_lineage/count_filetypes.py
@@ -31,7 +31,7 @@ def count_file_types(directory):
     file_sizes = collections.defaultdict(int)
     for  root, _, files in os.walk(directory, topdown=True):
         for file in files:
-            _, ext = os.path.splitext(file) #os.stat(file).st_size
+            _, ext = os.path.splitext(file) 
             file_counts[ext] += 1
             file_sizes[ext] += os.stat(os.path.join(root, file)).st_size      
     return file_counts, file_sizes
