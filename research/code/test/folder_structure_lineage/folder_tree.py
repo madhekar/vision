80d150cee6094ffc35f8339b67a32e0725d2f1e2
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-12 09:34:21 -0800

    display ui/ux poc for folder structure

---
+++ research/code/test/folder_structure_lineage/folder_tree.py
@@ -0,0 +1,16 @@
+import os
+
+def display_tree(path, indent=''):
+    """Displays the directory tree structure."""
+
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        if os.path.isdir(full_path):
+            print(indent + '├── ' + entry)
+            display_tree(full_path, indent + '|   ')
+        else:
+            print(indent + '└── ' + entry)
+
+if __name__ == "__main__":
+    path = '.'  # Replace with the desired directory path
+    display_tree(path)
