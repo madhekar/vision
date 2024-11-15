b6e4047dbe1512a0f5a92b507d6f3425d63215dc
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-15 14:34:27 -0800

    poc

---
+++ research/code/test/multipage_app/pages/data_add.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from utils.dataload_util import dataload as dlu
+
+st.subheader("METADATA: EDITOR/ CORRECTION")
+dlu.execute()
--- research/code/test/multipage_app/utils/dataload_util/dataload.py
+++ research/code/test/multipage_app/utils/dataload_util/dataload.py
@@ -1,7 +1,10 @@
 import os
 import getpass
-# from utils.config_util import config
+from utils.config_util import config
 import streamlit as st   
+import util
+from utils.util import adddata_util as adu
+from streamlit_tree_select import tree_select
 
 
 def get_user():
@@ -10,37 +13,44 @@ def get_user():
 def get_external_devices(user):
     return os.listdir(f'/media/{user}')
 
-def display_tree(path, indent=""):
-    """Displays the directory tree structure."""
-    sout = ""
+def get_path_as_dict(path):
+    nodes = []
+    nodes.append(adu.path_dict(path))
+    return nodes
 
-    for entry in os.listdir(path):
-      if os.access(os.path.join(path, entry), os.R_OK):  
-        full_path = os.path.join(path, entry)
-        if os.path.isdir(full_path):
-            sout += indent + "├── " + entry + '\n'
-            display_tree(full_path, indent + "|   ")
-        else:
-            sout += indent + "└── " + entry + '\n'
-    return sout        
+def display_folder_tree(nodes):
+    con = st.sidebar.container(height=500)
+    with con:
+        return_select = tree_select(nodes, no_cascade=True)
+
+    con1 = st.sidebar.expander(label="checked folders")  # container(height=500)
+    with con1:
+        selected = []
+        for e in return_select["checked"]:
+            e0 = e.split("@@")[0]
+            selected.append(e0)
+            st.write(e0)
+
+    
 
 
 def execute():
-    # (
-    #     raw_data_path,
-    #     input_image_path,
-    #     input_video_path,
-    #     input_txt_path,
-    # ) = config.dataload_config_load()
+    (
+        raw_data_path,
+        input_image_path,
+        input_video_path,
+        input_txt_path,
+    ) = config.dataload_config_load()
 
-    source_list = []
-    source_list = get_external_devices(get_user())
-    if len(source_list) > 0:
-       ext = st.sidebar.selectbox(label="Select Source", options=source_list)
+    display_folder_tree(get_path_as_dict(raw_data_path))
+    st.sidebar.button(label="Trim(Checked-Folders)")  
     
+    # source_list = []
+    # source_list = get_external_devices(get_user())
+    # if len(source_list) > 0:
+    #    ext = st.sidebar.selectbox(label="Select Source", options=source_list)
 
-    c1, c2 = st.columns([1,1])
-    c1.text_area(label="External Source Structure", value= display_tree(os.path.join('/media/madhekar/' , ext)))
+    # c1.text_area(label="External Source Structure", value= display_tree(os.path.join('/media/madhekar/' , ext)))
 
 if __name__ == "__main__":
     execute()
