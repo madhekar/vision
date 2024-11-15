bf5d06266b64c52f399423ce4a76e127e99bbe2a
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-15 15:21:07 -0800

    fix trim

--- research/code/test/multipage_app/utils/dataload_util/dataload.py
+++ research/code/test/multipage_app/utils/dataload_util/dataload.py
@@ -19,12 +19,12 @@ def get_path_as_dict(path):
     return nodes
 
 def display_folder_tree(nodes):
-    con = st.sidebar.container(height=500)
+    con = st.sidebar.container(height=500, border=False)
     with con:
         return_select = tree_select(nodes, no_cascade=True)
 
-    con1 = st.sidebar.expander(label="checked folders")  # container(height=500)
-    with con1:
+    exp = st.sidebar.expander(label="CHECKED FOLDERS TO TRIM")  # container(height=500)
+    with exp:
         selected = []
         for e in return_select["checked"]:
             e0 = e.split("@@")[0]
@@ -42,13 +42,14 @@ def execute():
         input_txt_path,
     ) = config.dataload_config_load()
 
+    st.sidebar.caption("CHECK FOLDERS TO TRIM",unsafe_allow_html=True)
     display_folder_tree(get_path_as_dict(raw_data_path))
-    st.sidebar.button(label="Trim(Checked-Folders)")  
-    
-    # source_list = []
-    # source_list = get_external_devices(get_user())
-    # if len(source_list) > 0:
-    #    ext = st.sidebar.selectbox(label="Select Source", options=source_list)
+    st.sidebar.button(label="TRIM",use_container_width=True)  
+
+    source_list = []
+    source_list = get_external_devices(get_user())
+    if len(source_list) > 0:
+       ext = st.sidebar.selectbox(label="Select Source", options=source_list)
 
     # c1.text_area(label="External Source Structure", value= display_tree(os.path.join('/media/madhekar/' , ext)))
 
