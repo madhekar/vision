17d18335f6e87a4a164558e783b3c28c778a6053
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-15 11:10:23 -0800

    finalize

--- research/code/test/folder_structure_lineage/dicttojson.py
+++ research/code/test/folder_structure_lineage/dicttojson.py
@@ -5,79 +5,43 @@ import streamlit as st
 from streamlit_tree_select import tree_select
 import streamlit_scrollable_textbox as stx
 
+st.set_page_config(
+        page_title="zesha: Home Media Portal (HMP)",
+        page_icon="/home/madhekar/work/zsource/zesha-high-resolution-logo.jpeg",
+        initial_sidebar_state="auto",
+        layout="wide",
+    )
+
 st.title("🐙 Streamlit-tree-select")
 st.subheader("A simple and elegant checkbox tree for Streamlit.")
 
-
 @st.cache_resource
 def path_dict(path):
-    d = {"label": os.path.basename(path), "value": str(path) + '@@' + str(uuid.uuid4())} #, 'id': str(uuid.uuid4())}
+    d = {"label": os.path.basename(path), "value": str(path) + '@@' + str(uuid.uuid4())}
     if os.path.isdir(path):
         d["label"] = os.path.basename(path)  #'dir'
-        d["children"] = [
-            path_dict(os.path.join(path, x))
-            for x in os.listdir(path)
-            if os.path.isdir(os.path.join(path, x))
-        ]
-    # else:
-    #     d["label"] = os.path.basename(path)  #"file"
-    #print(d)
+        d["children"] = [ path_dict(os.path.join(path, x)) for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))]
+    else:
+         d["label"] = os.path.basename(path)  #"file"
     return d      
 
 @st.cache_resource
 def path_dict_file(path):
-    d1 = {"label": os.path.basename(path), "value": str(path) + '@@' + str(uuid.uuid4())} #, 'id': str(uuid.uuid4())}
+    d1 = {"label": os.path.basename(path), "value": str(path) + '@@' + str(uuid.uuid4())}
     print(path)
     if os.path.isdir(path):
         d1["label"] = os.path.basename(path)  #'dir'
-        d1["children"] = [
-            path_dict_file(os.path.join(path, x))
-            for x in os.listdir(path)
-        ]
+        d1["children"] = [ path_dict_file(os.path.join(path, x)) for x in os.listdir(path)]
     else:
          d1["label"] = os.path.basename(path)  #"file"
-    print(d1)
     return d1
 
-
-def dict_to_json(path):
-   return json.dumps(path_dict(path),0)
-
-# Create nodes to display
-# nodes = [
-#     {"label": "Folder A", "value": "folder_a"},
-#     {
-#         "label": "Folder B",
-#         "value": "folder_b",
-#         "children": [
-#             {"label": "Sub-folder A", "value": "sub_a"},
-#             {"label": "Sub-folder B", "value": "sub_b"},
-#             {"label": "Sub-folder C", "value": "sub_c"},
-#         ],
-#     },
-#     {
-#         "label": "Folder C",
-#         "value": "folder_c",
-#         "children": [
-#             {"label": "Sub-folder D", "value": "sub_d"},
-#             {
-#                 "label": "Sub-folder E",
-#                 "value": "sub_e",
-#                 "children": [
-#                     {"label": "Sub-sub-folder A", "value": "sub_sub_a"},
-#                     {"label": "Sub-sub-folder B", "value": "sub_sub_b"},
-#                 ],
-#             },
-#             {"label": "Sub-folder F", "value": "sub_f"},
-#         ],
-#     },
-# ]
-#dict_to_json('/home/madhekar/work/home-media-path/data/raw-data')
 nodes = []
 nodes.append(path_dict("/home/madhekar/work/home-media-app/data/raw-data"))
 
+con = st.container(border=True, height=1000)
 #print(nodes)
-c1,c2 = st.columns([.5,1], gap='small', vertical_alignment='top')
+c1,c2 = con.columns([.5,1], gap='small', vertical_alignment='top')
 
 with c1:
     if nodes:
