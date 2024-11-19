688997b84e84e7435bdc2da7ed611916e2266b68
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-19 11:59:45 -0800

    cleanup amd LLM prompt correction

--- research/code/test/multipage_app/utils/preprocess_util/LLM.py
+++ research/code/test/multipage_app/utils/preprocess_util/LLM.py
@@ -39,7 +39,7 @@ def fetch_llm_text(imUrl, model, processor, top, temperature, question, people, 
     print("calling LLM")
 
     prompt = """<|im_start|>system
-    A chat between a curious human and an artificial intelligence assistant. The assistant is an expert in people, and gives helpful, detailed, and polite answers to the human's questions. The assistant does not hallucinate and pays very close attention to the details.
+    A chat between a curious human and an artificial intelligence assistant. The assistant is an expert in people's and location's, and gives helpful, detailed, and polite answers to the human's questions. The assistant does not hallucinate and pays very close attention to the details.
     <|im_end|>
     <|im_start|>user
     <image>
