12baf34150b04c8c8d52984f98168879947863ef
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-20 18:16:55 -0800

    refactor

---
+++ research/code/test/multipage_app/utils/addextdata_util/addextdata.py
@@ -0,0 +1,54 @@
+
+import util
+import os
+import streamlit as st
+from utils.util import adddata_util as adu
+from streamlit_tree_select import tree_select
+from utils.config_util import config
+
+nodes=[]
+nodes.append(adu.path_dict("/home/madhekar/work/home-media-app/data/raw-data"))
+
+con = st.sidebar.container(height=500)
+with con:
+    return_select = tree_select(nodes, no_cascade=True)
+
+con1 = st.sidebar.expander(label="checked folders")  # container(height=500)
+with con1:
+    selected = []
+    for e in return_select["checked"]:
+        e0 = e.split("@@")[0]
+        selected.append(e0)
+        st.write(e0)
+
+st.sidebar.button(label="Trim(Checked-Folders)")
+
+
+def execute():
+        (
+        raw_data_path,
+        input_image_path,
+        input_video_path,
+        input_txt_path,
+    ) = config.dataload_config_load()
+
+    # select load data from external data source such as USB device
+
+    # create new path string by appending raw-data path with source-name from external device
+
+    # check new path raw-data/source-name exists on hard drive
+    
+    # check folders and files exists in raw-data/source-name folder   
+
+    # remove all folders and files on raw-data/source-name  --- todo think about UNDO requirments
+
+    # copy all folders and files to raw-data/source-name folder
+        
+    # arc_folder_name = util.get_foldername_by_datetime()
+    # archive_dup_path = os.path.join(archive_dup_path, arc_folder_name)
+    # dr = DuplicateRemover(dirname=input_image_path, archivedir=archive_dup_path)
+    # dr.find_duplicates()
+
+
+if __name__ == "__main__":
+    execute()
---
+++ research/code/test/multipage_app/utils/datatrim_util/datatrim.py
@@ -0,0 +1,81 @@
+import os
+import getpass
+from utils.config_util import config
+import streamlit as st   
+import util
+from utils.util import adddata_util as adu
+from streamlit_tree_select import tree_select
+from utils.util import model_util as mu
+from utils.util import storage_stat as ss
+
+
+def get_user():
+    return getpass.getuser()
+
+def get_external_devices(user):
+    return os.listdir(f'/media/{user}')
+
+def get_path_as_dict(path):
+    nodes = []
+    nodes.append(adu.path_dict(path))
+    return nodes
+
+#@st.fragment
+def display_folder_tree(nodes):
+    con = st.sidebar.container(height=500, border=False)
+    with con:
+        return_select = tree_select(nodes, no_cascade=True) 
+    display_folder_stats(return_select["checked"])
+
+    exp = st.sidebar.expander(label="CHECKED FOLDERS TO TRIM")  # container(height=500)
+    with exp:
+
+        for e in return_select["checked"]:
+            e0 = e.split("@@")[0]
+            st.write(e0)
+
+
+def display_folder_stats(flist):
+    row_size = 4
+    page =1 
+    batch_size = 12
+    batch = flist[(page - 1) * batch_size : page * batch_size]
+    grid = st.columns(row_size, gap="small", vertical_alignment="top")
+    col = 0
+    st.cache_resource
+    for df in batch:
+        with grid[col]:
+            folder = df.split("@@")[0]
+            st.subheader(folder, divider='gray')
+            st.bar_chart(ss.extract_folder_stats(folder))
+
+        col = (col + 1) % row_size
+    
+def execute():
+    (
+        raw_data_path,
+        input_image_path,
+        input_video_path,
+        input_txt_path,
+    ) = config.dataload_config_load()
+
+
+    '''
+    select data source to trim data
+    ''' 
+    source_list = []
+    #source_list = get_external_devices(get_user())
+    source_list = mu.extract_user_raw_data_folders(raw_data_path)
+    if len(source_list) > 0:
+       ext = st.sidebar.selectbox(label="Select Source", options=source_list)
+
+
+    st.sidebar.caption("CHECK FOLDERS TO TRIM",unsafe_allow_html=True)
+    display_folder_tree(get_path_as_dict(os.path.join(raw_data_path, ext)))
+    st.sidebar.button(label="TRIM CHECKED FOLDERS",use_container_width=True) 
+    # c1.text_area(label="External Source Structure", value= display_tree(os.path.join('/media/madhekar/' , ext)))
+
+
+
+if __name__ == "__main__":
+    execute()
