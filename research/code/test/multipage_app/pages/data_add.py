c6d77545dbdb764924b5f7bb197b97af0fd784f2
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-20 18:15:08 -0800

    refactor

--- research/code/test/multipage_app/pages/data_add.py
+++ research/code/test/multipage_app/pages/data_add.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.dataload_util import dataload as dlu
+from utils.datatrim_util import datatrim as dlu
 
 st.subheader("DATA: TRIM (VERIFY)", divider='gray')
 dlu.execute()
