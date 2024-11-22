f93767c88ce2d238d01387d2f892f2f4429b6257
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-22 15:28:27 -0800

    add more data

--- research/code/test/multipage_app/utils/addextdata_util/addextdata.py
+++ research/code/test/multipage_app/utils/addextdata_util/addextdata.py
@@ -3,35 +3,44 @@ import os
 import getpass
 import streamlit as st
 from utils.util import adddata_util as adu
+from utils.util import model_util as mu
 from streamlit_tree_select import tree_select
 from utils.config_util import config
 
-nodes=[]
-nodes.append(adu.path_dict("/home/madhekar/work/home-media-app/data/raw-data"))
+# nodes=[]
+# nodes.append(adu.path_dict("/home/madhekar/work/home-media-app/data/raw-data"))
 
-con = st.sidebar.container(height=500)
-with con:
-    return_select = tree_select(nodes, no_cascade=True)
+# con = st.sidebar.container(height=500)
+# with con:
+#     return_select = tree_select(nodes, no_cascade=True)
 
-con1 = st.sidebar.expander(label="checked folders")  # container(height=500)
-with con1:
-    selected = []
-    for e in return_select["checked"]:
-        e0 = e.split("@@")[0]
-        selected.append(e0)
-        st.write(e0)
+# con1 = st.sidebar.expander(label="checked folders")  # container(height=500)
+# with con1:
+#     selected = []
+#     for e in return_select["checked"]:
+#         e0 = e.split("@@")[0]
+#         selected.append(e0)
+#         st.write(e0)
 
-st.sidebar.button(label="Trim(Checked-Folders)")
+# st.sidebar.button(label="Trim(Checked-Folders)")
 
 def get_user():
     return getpass.getuser()
 
-
 def get_external_devices(user):
     return os.listdir(f"/media/{user}")
 
+def get_existing_data_sources(dpath):
+     pass
+     
+# def overrite(data_source):
+#     st.write(f'Are Sure, Overrite Existing {data_source} Data Source')
+#     if st.button('Submit'):
+#         st.session_state.overrite = 
+
+
 def execute():
-        (
+    (
         raw_data_path,
         input_image_path,
         input_video_path,
@@ -39,8 +48,29 @@ def execute():
     ) = config.dataload_config_load()
 
     # select load data from external data source such as USB device
+    user = get_user()
+    devices_list = get_external_devices(user=user)
+
+    #show external devices in dropdown box
+    ext_source = st.sidebar.selectbox(label="EXTERNAL DATA SOURCES", options=devices_list)
+    
+    #show data already imported data sources
+    source_list = mu.extract_user_raw_data_folders(raw_data_path)
+    s=""
+    for str in source_list:
+        s += str + '\n'
+
+    if len(source_list) > 0:
+       #st.sidebar.selectbox(label="Existing Imported Data Sources", options=source_list)
+       st.sidebar.text_area(label="EXISTING DATA SOURCES", value=s)
 
     # create new path string by appending raw-data path with source-name from external device
+    if ext_source in source_list:
+        ans = st.sidebar.toggle(f'DO YOU REALLY LIKE TO OVERRITE [**{ext_source}**] DATA SOURCE?')
+        if ans:   
+            st.sidebar.button(label="IMPORT & OVERRIDE DATA", use_container_width=True)
+    else:
+        st.sidebar.button(label="IMPORT DATA", use_container_width=True)
 
     # check new path raw-data/source-name exists on hard drive
     
