a6170761e8708159a1e49bc7115660783b587525
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-11-26 09:57:26 -0800

    POC

--- research/code/test/folder_structure_lineage/folder_path_short.py
+++ research/code/test/folder_structure_lineage/folder_path_short.py
@@ -1,21 +1,17 @@
 import uuid
-
-
 import os
-
-
 import hashlib
 
-#def create_uuid_from_string(val: str):
-#    hex_string = hashlib.md5(val.encode("UTF-8")).hexdigest()
-#    return uuid.UUID(hex=hex_string)
+def create_uuid_from_string(val: str):
+   hex_string = hashlib.md5(val.encode("UTF-8")).hexdigest()
+   return uuid.UUID(hex=hex_string)
 
 def path_encode(spath):
     return str(uuid.uuid5(uuid.NAMESPACE_DNS, spath))
 
 
-def path_encode_1(spath):
-  return str(uuid.uuid5('zesha llc',spath))
+# def path_encode_1(spath):
+#   return str(uuid.uuid5(str.encode('zesha llc'),spath))
 
 plist = [
     "/home/madhekar/work/home-media-portal/data/raw-data/1",
@@ -28,9 +24,9 @@ plist = [
     "/"
 ]
 
-print({x: path_encode(x) for x in plist})
+# print({x: path_encode(x) for x in plist})
 
-print({x: path_encode_1(x) for x in plist})
+# print({x: path_encode_1(x) for x in plist})
 
 
-#print({x: create_uuid_from_string(x) for x in plist})
+print({x: str(create_uuid_from_string(x)) for x in plist})
