66edbe556c9941fffecbea3619b6bc98992599f9
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-12-03 13:42:21 -0800

    deaft POC

--- research/code/test/folder_structure_lineage/folder_path_short.py
+++ research/code/test/folder_structure_lineage/folder_path_short.py
@@ -5,7 +5,7 @@ import shutil
 
 
 media_extensions = ['.mp3', '.mp4', '.avi', '.mov', '.jpg', '.jpeg', '.png', '.gif']  # Add more as needed
-document_extensions = ['.txt', '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx']  # Add more as needed
+document_extensions = ['.txt', '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.log']  # Add more as needed
 
 def create_uuid_from_string(val: str):
    hex_string = hashlib.md5(val.encode("UTF-8")).hexdigest()
@@ -24,28 +24,30 @@ def copy_files_only(src_dir, dest_dir):
     for root, dirnames, items in os.walk(src_dir):
         if not dirnames:
             if len(items) > 0:
-                # print (root, "has 0 subdirectories and", len(items), "files")
                 print(root + " - " + str(dirnames) + " - " + str(items))
-                uuid_path = path_encode(root)
-                f_dest = os.path.join(dest_dir, uuid_path)
-                print(src_dir + " - " + f_dest +" - " + str(len(items)))
-                os.makedirs(f_dest)
-                for item in items:
-                    item_path = os.path.join(root, item)
-                    print(item_path+ ' -> '+ dest_dir)
-                    if os.path.isfile(item_path):
-                        print('**' + item_path + " - "+ dest_dir)
-                        try:
-                            shutil.copy(item_path, f_dest)
-                        except FileNotFoundError:
-                            print("Source file not found.")
-                        except PermissionError:
-                            print("Permission denied.")
-                        except FileExistsError:
-                            print("Destination file already exists.")
-                        except Exception as e:
-                            print(f"An error occurred: {e}")
-                
+                items = [ f for f in items if os.path.splitext(f)[1].lower() in media_extensions or os.path.splitext(f)[1].lower() in document_extensions]
+                if len(items) > 0:
+                    print(root + " - " + str(dirnames) + " - " + str(items))
+                    uuid_path = path_encode(root)
+                    f_dest = os.path.join(dest_dir, uuid_path)
+                    print(src_dir + " - " + f_dest +" - " + str(len(items)))
+                    os.makedirs(f_dest)
+                    for item in items:
+                        item_path = os.path.join(root, item)
+                        print(item_path+ ' -> '+ dest_dir)
+                        if os.path.isfile(item_path):
+                            print('**' + item_path + " - "+ dest_dir)
+                            try:
+                                shutil.copy(item_path, f_dest)
+                            except FileNotFoundError:
+                                print("Source file not found.")
+                            except PermissionError:
+                                print("Permission denied.")
+                            except FileExistsError:
+                                print("Destination file already exists.")
+                            except Exception as e:
+                                print(f"An error occurred: {e}")
+                    
 
 # def create_dirtree_without_files(src, dst):
    
