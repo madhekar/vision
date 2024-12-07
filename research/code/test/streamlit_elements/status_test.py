d03d41501514d7927de2cc367bae62786e50365c
Bhalchandra Madhekar <bmadhekar@gmail.com>
2024-12-06 22:41:00 -0800

    poc

--- research/code/test/streamlit_elements/status_test.py
+++ research/code/test/streamlit_elements/status_test.py
@@ -2,22 +2,22 @@ import random
 import streamlit as st
 
 st.write(
-    """
+"""
 # Biopsias classification demo
 """
 )
 
 print(st.session_state)
 
-text_area = st.empty()
+ta = st.empty()
 
-text = text_area.text_area("Text to analyze", "Write a number")
+text = ta.text_area("Text to analyze", "Write a number")
 
 button1 = st.button("Random")
 button2 = st.button("Run")
 
 if button1:
-    text = text_area.text_area("Text to analyze", str(random.random()))
+    text = ta.text_area("Text to analyze", str(random.random()))
 
 if button2:
     st.write(text)
