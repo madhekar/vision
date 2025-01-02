9dba5178b3d7824b51ed8f951ae2dfa5a39ccf96
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-02 13:17:45 -0800

    status

--- research/code/test/multipage_app/utils/util/statusmsg_util.py
+++ research/code/test/multipage_app/utils/util/statusmsg_util.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from collections import defaultdict
 
 def init():
     if "btn_prsd_status" not in st.session_state:
@@ -12,5 +13,9 @@ def add_messages(msg_type, message):
     st.session_state.msgs[msg_type].append(message)
 
 def get_message_by_type(tmsg):
-    return {ele.split("|")[0]: ele.split("|")[1] for ele in st.session_state.msgs[tmsg]}
+    d = defaultdict(set)
+    for ele in st.session_state.msgs[tmsg]:
+        k, v =  ele.split("|")
+        d[k].add(v)
+    return d
 
