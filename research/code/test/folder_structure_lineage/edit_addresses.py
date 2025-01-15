2ab758e2cc1e4e889cf5ae1670c3832c03a17203
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-15 14:35:32 -0800

    allow editing csv

---
+++ research/code/test/folder_structure_lineage/edit_addresses.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+import streamlit as st
+
+
+def update(edit_df):
+   edit_df.to_csv('my.csv', index=False)
+
+
+def read_addresses_file(sap, saf):
+    df = pd.read_csv(os.path.join(sap, saf),delimiter=",")
+
+    if "df_val" not in st.session_state:
+        st.session_state.df_val = df
+    return df
+
+def show_current_addresses(df):
+    show_df = st.table(df)
+    return show_df
+
+def show_edit_addresses(df):
+    edit_df = st.data_editor(df, key="editor", num_rows="dynamic")
+    if edit_df is not None and not edit_df.equals(st.session_state["df_val"]):
+        update(edit_df)
+        st.session_state["df_val"] = edit_df
+    return edit_df
+
+if __name__=='__main__':
+    zapp_static_locaton_prep_data_path = "/home/madhekar/work/home-media-app/data/input-data/prep/"
+    zapp_static_locaton_prep_file = "static_addresses.csv"
+
+    st.set_page_config(layout="wide")
+
+    df = read_addresses_file(zapp_static_locaton_prep_data_path, zapp_static_locaton_prep_file)
+    c1, c2 =st.columns([1,2], gap="small")
+    with c1:
+        show_edit_addresses(df)
+
+    with c2:
+       show_current_addresses(df)
