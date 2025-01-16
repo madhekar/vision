813ec42d8c41091cd2ed2d44be413ed5e28d6811
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-16 12:52:58 -0800

    other POC

---
+++ research/code/test/folder_structure_lineage/data_edit.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+import streamlit as st
+
+sap = "/home/madhekar/work/home-media-app/data/input-data/prep/"
+saf = "static_addresses.csv"
+
+
+def update(edit_df):
+    print(sap + ":" + saf)
+    edit_df.to_csv(os.path.join(sap, saf), index=False)
+
+
+def read_addresses_file():
+    df = pd.read_csv(os.path.join(sap, saf), delimiter=",")
+
+    if "df_val" not in st.session_state:
+        st.session_state.df_val = df
+    return df
+
+
+def show_current_addresses(df):
+    show_df = st.table(df)
+    return show_df
+
+
+def show_edit_addresses(df):
+    edit_df = st.data_editor(df, key="editor", num_rows="dynamic")
+    if edit_df is not None and not edit_df.equals(st.session_state["df_val"]):
+        update(edit_df)
+        st.session_state["df_val"] = edit_df
+        return edit_df
+
+
+if __name__ == "__main__":
+    st.set_page_config(layout="wide")
+
+    df = read_addresses_file()
+    c1, c2 = st.columns([3, 1], gap="small")
+    with c1:
+        show_edit_addresses(df)
+
+    with c2:
+        show_current_addresses(df)
+        with st.form ('address_edit'):
+            name_value = st.text_input(label='enter location name or description')
+            addess_value = st.text_input(label='enter address of the location')
+
+            submit = st.form_submit_button(label='submit')
+
+        if submit:
+            d = {}
+            d['name'] = name_value
+            d['address'] = addess_value
+
