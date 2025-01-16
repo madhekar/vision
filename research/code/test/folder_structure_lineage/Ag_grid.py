ec6091ffbbcb11591e872055f01c947a56d40c75
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-16 15:17:39 -0800

    wip

--- research/code/test/folder_structure_lineage/Ag_grid.py
+++ research/code/test/folder_structure_lineage/Ag_grid.py
@@ -1,16 +1,19 @@
 
+import os
 import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
 
-# Sample data
-df = pd.DataFrame(
-    {
-        "Name": ["Alice", "Bob", "Charlie"],
-        "Age": [25, 30, 35],
-        "City": ["New York", "London", "Tokyo"],
-    }
-)
+
+sap = "."
+saf = "my.csv"
+
+@st.cache_data
+def read_addresses_file():
+    df = pd.read_csv(os.path.join(sap, saf), delimiter=",")
+    return df
+
+df = read_addresses_file()
 
 # Create grid options
 gb = GridOptionsBuilder.from_dataframe(df)
@@ -19,4 +22,4 @@ gb.configure_side_bar()
 gridOptions = gb.build()
 
 # Display the grid
-AgGrid(df, gridOptions=gridOptions, height=400)
+g_response = AgGrid(df, gridOptions=gridOptions, update_mode='SELECTION_CHANGED', allow_unsafe_jscode=True)
