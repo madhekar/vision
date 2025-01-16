e5ecfe474d290fda007e0ce0b3cbab6f55c22e83
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-16 12:04:21 -0800

    data editor issues

--- research/code/test/folder_structure_lineage/edit_addresses.py
+++ research/code/test/folder_structure_lineage/edit_addresses.py
@@ -2,13 +2,15 @@ import os
 import pandas as pd
 import streamlit as st
 
+sap = "/home/madhekar/work/home-media-app/data/input-data/prep/"
+saf = "static_addresses.csv"
 
-def update(sap, saf, edit_df):
+def update(edit_df):
    print(sap + ':' + saf)
    edit_df.to_csv(os.path.join(sap, saf), index=False)
 
 
-def read_addresses_file(sap, saf):
+def read_addresses_file():
     df = pd.read_csv(os.path.join(sap, saf),delimiter=",")
 
     if "df_val" not in st.session_state:
@@ -19,23 +21,21 @@ def show_current_addresses(df):
     show_df = st.table(df)
     return show_df
 
-def show_edit_addresses(sap, saf, df):
+def show_edit_addresses(df):
     edit_df = st.data_editor(df, key="editor", num_rows="dynamic")
     if edit_df is not None and not edit_df.equals(st.session_state["df_val"]):
-        update(sap, saf, edit_df)
+        update(edit_df)
         st.session_state["df_val"] = edit_df
-    return edit_df
+        return edit_df
 
 if __name__=='__main__':
-    zapp_static_locaton_prep_data_path = "/home/madhekar/work/home-media-app/data/input-data/prep/"
-    zapp_static_locaton_prep_file = "static_addresses.csv"
 
     st.set_page_config(layout="wide")
 
-    df = read_addresses_file(zapp_static_locaton_prep_data_path, zapp_static_locaton_prep_file)
-    c1, c2 =st.columns([1,2], gap="small")
+    df = read_addresses_file()
+    c1, c2 =st.columns([3,1], gap="small")
     with c1:
-        show_edit_addresses(zapp_static_locaton_prep_data_path, zapp_static_locaton_prep_file, df)
+        show_edit_addresses(df)
 
     with c2:
        show_current_addresses(df)
