3af6c551e4243f7b9a282c44e29cc0faf1d8f703
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-16 12:52:07 -0800

    bug fix 12

--- research/code/test/folder_structure_lineage/data_editor_poc.py
+++ research/code/test/folder_structure_lineage/data_editor_poc.py
@@ -7,9 +7,9 @@ if "df_value" not in st.session_state:
     st.session_state.df_value = df
 
 
-def update(edited_df):
-    print(edited_df)
-    edited_df.to_csv('my.csv', index=False)
+def update(d_df):
+    print(d_df)
+    d_df.to_csv('my.csv', index=False)
     # some more code
 
 
@@ -17,12 +17,14 @@ edited_df = st.data_editor(
     df,
     key="editor",
     num_rows="dynamic",
+    on_change=update,
+    args=(df,)
 )
 
-if edited_df is not None and not edited_df.equals(st.session_state["df_value"]):
-    # This will only run if
-    # 1. Some widget has been changed (including the dataframe editor), triggering a
-    # script rerun, and
-    # 2. The new dataframe value is different from the old value
-    update(edited_df)
-    st.session_state["df_value"] = edited_df
+# if edited_df is not None and not edited_df.equals(st.session_state["df_value"]):
+#     # This will only run if
+#     # 1. Some widget has been changed (including the dataframe editor), triggering a
+#     # script rerun, and
+#     # 2. The new dataframe value is different from the old value
+#     update(edited_df)
+#     st.session_state["df_value"] = edited_df
