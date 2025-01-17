864329df4a4b11f4e6b6372db58fa84aa927e710
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-17 11:22:44 -0800

    new simple app

---
+++ research/code/test/folder_structure_lineage/data_editor_poc1.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+mycsv = 'my.csv'
+
+def update(editdf):
+    editdf.to_csv(mycsv, index=False)
+    
+
+@st.cache_data(ttl='1d')
+def load_df():
+    return pd.read_csv(mycsv)
+
+df = load_df()
+edf = st.data_editor(df)  
+st.button('save', on_click=update, args=(edf, ))
