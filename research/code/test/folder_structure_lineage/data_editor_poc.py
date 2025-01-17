2ce07ec782cfb0fdfe6bbd4ce1cedf20b267daba
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-17 11:04:35 -0800

    bug

--- research/code/test/folder_structure_lineage/data_editor_poc.py
+++ research/code/test/folder_structure_lineage/data_editor_poc.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-if "df_value" not in st.session_state:
-    st.session_state.df_value = None
+
 
 
 def load_csv():
@@ -26,22 +25,27 @@ def clean_dataframe(d):
 
     print(df_r)
 
-load_csv()
+def edit_data():
+    edited_df = st.data_editor(
+        st.session_state.df_value,
+        key="editor",
+        num_rows="dynamic",
+        #on_change=update,
+        #args=(df,)
+    )
 
-edited_df = st.data_editor(
-    st.session_state.df_value,
-    key="editor",
-    num_rows="dynamic",
-    #on_change=update,
-    #args=(df,)
-)
+    if edited_df is not None and not edited_df.equals(st.session_state["df_value"]):
+        st.write('-----> here')
+        # This will only run if
+        # 1. Some widget has been changed (including the dataframe editor), triggering a
+        # script rerun, and
+        # 2. The new dataframe value is different from the old value
+        update(edited_df)
+        st.session_state["df_value"] = edited_df
 
-if edited_df is not None and not edited_df.equals(st.session_state["df_value"]):
-    st.write('-----> here')
-    # This will only run if
-    # 1. Some widget has been changed (including the dataframe editor), triggering a
-    # script rerun, and
-    # 2. The new dataframe value is different from the old value
-    update(edited_df)
-    st.session_state["df_value"] = edited_df
+if __name__=='__main__':
+    
+    if "df_value" not in st.session_state:
+       st.session_state.df_value = None
 
+    load_csv()
