5cbc6ffedcb7a8f908c8e3e8222081db32bc0023
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-21 13:51:44 -0800

    using pyap module

--- research/code/test/folder_structure_lineage/dialog.py
+++ research/code/test/folder_structure_lineage/dialog.py
@@ -4,6 +4,7 @@ from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 import time
 import pandas as pd
+import pyap as ap
 from pprint import pprint
 
 if "parse" not in st.session_state:
@@ -51,12 +52,13 @@ def location_details(name, query = {}):
     return dfr
 
 def parse_address(full_add):
-    tokens = full_add.split(',')
-    return tokens
+    parsed_address = ap.parse(full_add)[0]
+    return parsed_address
 
 @st.dialog("Enter your location")
 def add_location():
-    st.subheader("location name/ desc")
+
+    st.subheader("location name or desc")
     name = st.text_input('name', placeholder='Enter name or description of location', label_visibility="collapsed")
 
 
@@ -64,32 +66,34 @@ def add_location():
     full_address = st.text_input('full_address', placeholder='Enter Address of location', label_visibility="collapsed")
 
     if st.button('parse'):
-        s, c, st, cn, po = parse_address(full_address)
+        apa = parse_address(full_address)
         d={}
         street = st.text_input("Enter street address of location")
-        d['street'] = street
+        d['street'] = apa.street_number + ' ' + apa.street_name
         city = st.text_input("Enter city/ town/ village of location")
-        d['city'] = city
+        d['city'] = apa.city
         state = st.text_input("Enter state/ province name of location")
-        d['state'] = state
+        d['state'] = apa.region
         country = st.text_input("Enter country name of location")
-        d['country'] = country
+        d['country'] = apa.country
         pincode = st.text_input("Enter pin/ zip code of location")
-        d['postalcode'] = pincode
-    
+        d['postalcode'] = apa.zipcode
+        st.session_state['parse'] = not st.session_state['parse']
 
-    if st.button('latlon'):
-        fs = d
-        st.text_area(label="query", value=fs)
-        ld= location_details(name=name, query=d)
-        st.text_area(label='location details', value=ld.to_string())
-        st.session_state['latlon'] = not st.session_state['latlon']
+    if st.session_state['parse']:
+        if st.button('latlon'):
+            fs = d
+            st.text_area(label="query", value=fs)
+            ld= location_details(name=name, query=d)
+            st.text_area(label='location details', value=ld.to_string())
+            st.session_state['latlon'] = not st.session_state['latlon']
 
-    if st.session_state['latlon']:
-        if st.button("Submit"):
-            st.session_state.location = {"nam": name, "street": street, 'city': city, 'state': state, 'country': country, 'zip code': pincode}
-            st.session_state["submit"] = not st.session_state["submit"]
-            #st.rerun()
+    if st.session_state['parse']:
+        if st.session_state['latlon']:
+            if st.button("Submit"):
+                st.session_state.location = {"nam": name, "street": street, 'city': city, 'state': state, 'country': country, 'zip code': pincode}
+                st.session_state["submit"] = not st.session_state["submit"]
+                #st.rerun()
 
 
 if "location" not in st.session_state:
