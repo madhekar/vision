3153c7aadebe8026add27686b900e1a080e640c3
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-23 12:49:11 -0800

    streamlit bug

--- research/code/test/folder_structure_lineage/rev_dialog.py
+++ research/code/test/folder_structure_lineage/rev_dialog.py
@@ -15,27 +15,55 @@ def set_state(i):
 
 def parse_address(full_add):
     par_address = ap.parse(full_add, country="US")
+    print(par_address)
     return par_address[0].as_dict()
 
-def state_changes():
+def display_parsed_address(apa):
+    c1, c2, c3 , c4, c5 = st.columns([.7,.5,.3,.3,.5], gap="small")
+    d={}  
+    with c1:
+        d['street'] =  apa['full_street'] 
+        street = st.text_input("street name", value=d['street'], placeholder=d['street'],label_visibility="collapsed")
 
-    if st.session_state.stage == 0:
-        name = st.text_input('Name')
-        address = st.text_input('Address', on_change=set_state, args=[1])
+    with c2:
+        d['city'] = apa['city']
+        city = st.text_input("city/ town/ village", value=d['city'], placeholder=d['city'],label_visibility="collapsed")
 
-    if st.session_state.stage >= 1:
-        st.write(f'Hello {name}!')
-        color = st.selectbox(
-            'Pick a Color',
-            [None, 'red', 'orange', 'green', 'blue', 'violet'],
-            on_change=set_state, args=[3]
-        )
-        if color is None:
-            set_state(2)
+    with c3:    
+        d['state'] = apa['region1']
+        state = st.text_input("state/ province", value=d['state'], placeholder=d['state'],label_visibility="collapsed")
+    
+    with c4:
+        d['country'] = apa['country_id']
+        country = st.text_input("country code/ name", value=d['country'], placeholder=d['country'], label_visibility="collapsed")
+    with c5:    
+        d['postalcode'] = apa['postal_code']
+        pincode = st.text_input("pin/ zip code", value=d['postalcode'], placeholder=d['postalcode'], label_visibility="collapsed")
+    
+    st.session_state['p_location'] = d
 
-    if st.session_state.stage >= 2:
-        st.write(f':{color}[Thank you!]')
-        st.button('Start Over', on_click=set_state, args=[0])
+
+
+if st.session_state.stage == 0:
+    name = st.text_input('Name')
+    address = st.text_input('Address', on_change=set_state, args=[1])
+
+if st.session_state.stage >= 1:
+    st.write(f' hello{name}')
+    # pa = parse_address(address)
+    # d = display_parsed_address(pa)
+
+    # st.write(f'Hello {name}!')
+    # color = st.selectbox(
+    #     'Pick a Color',
+    #     [None, 'red', 'orange', 'green', 'blue', 'violet'],
+    #     on_change=set_state, args=[3]
+    # )
+    # if color is None:
+    #     set_state(2)
+
+if st.session_state.stage >= 2:
+    st.write(f'Thank you!')
+    st.button('Start Over', on_click=set_state, args=[0])
         
-
-state_changes()        
+      
--- research/code/test/streamlit_elements/button_state.py
+++ research/code/test/streamlit_elements/button_state.py
@@ -1,7 +1,45 @@
 import streamlit as st
+import pandas as pd
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+import time
+import pandas as pd
+import pyap as ap
+from pprint import pprint
 
 if 'stage' not in st.session_state:
     st.session_state.stage = 0
+
+geolocator = Nominatim(user_agent="name_ofgent")
+
+
+def location_details(name, query={}):
+    locs = []
+    d = {}
+    try:
+        loc = geolocator.geocode(
+            query=query, exactly_one=True, timeout=60, addressdetails=True
+        )
+        data = loc.raw
+        data = data["address"]
+        state = country = ""
+        state = str(data["state"]).lower()
+        country = str(data["country_code"]).lower()
+        d["id"] = 1
+        d["desc"] = name
+        d["state"] = state
+        d["country"] = country
+        d["lat"] = loc.latitude
+        d["lon"] = loc.longitude
+        locs.append(d)
+    except Exception as e:
+        pprint(f"error: geocode failed with {query} with exception {e}")
+    return d
+
+def parse_address(full_add):
+    par_address = ap.parse(full_add, country="US")
+    print(par_address)
+    return par_address[0].as_dict()
 
 def set_state(i):
     st.session_state.stage = i
@@ -14,15 +52,22 @@ if st.session_state.stage >= 1:
     address = st.text_input('Address', on_change=set_state, args=[2])
 
 if st.session_state.stage >= 2:
-    st.write(f'Hello {name}!')
-    color = st.selectbox(
-        'Pick a Color',
-        [None, 'red', 'orange', 'green', 'blue', 'violet'],
-        on_change=set_state, args=[3]
-    )
-    if color is None:
+    st.write(f'Hello {name} {address}!')
+    pa = parse_address(address)
+    st.write(pa)
+    bchecked = st.checkbox(label='is correct?')
+    if bchecked:
+        set_state(3)
+    # color = st.selectbox(
+    #     'Pick a Color',
+    #     [None, 'red', 'orange', 'green', 'blue', 'violet'],
+    #     on_change=set_state, args=[3]
+    # )
+    if pa is None:
         set_state(2)
 
 if st.session_state.stage >= 3:
-    st.write(f':{color}[Thank you!]')
+    ld = location_details(name=name, query=pa)
+    st.write(ld)
+    st.write(f'Thank you!')
     st.button('Start Over', on_click=set_state, args=[0])
