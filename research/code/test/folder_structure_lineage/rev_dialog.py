029473ac20d894af3cc77926f056f8b7f7be3125
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-24 14:53:37 -0800

    location bug

--- research/code/test/folder_structure_lineage/rev_dialog.py
+++ research/code/test/folder_structure_lineage/rev_dialog.py
@@ -53,34 +53,56 @@ def display_parsed_address(apa):
 @st.dialog("Enter location details: ")
 def add_location():
 
-    if st.session_state.stage >= 0:
-        name = st.text_input('Name')
-        address = st.text_input('Address', on_change=set_state, args=[1])
+    loc_type = st.radio("location data", ['Basic', 'Addesss'], captions=None, horizontal=True, label_visibility="collapsed")
 
-    if st.session_state.stage >= 1:
-        try:
-            pa = parse_address(address)
-            if pa is None:
-                set_state(1)
-            else:
-                d = display_parsed_address(pa)
-                st.info(f'Search Latitude and Longitude for : {name}', icon=":material/search:")
-                set_state(2)
-        except Exception as e:
-                st.error(f'Failed {e} to validate address for location: {name}', icon=":material/error:")  
+    if loc_type =='Basic':
+        if st.session_state.stage >= 0:
+            name = st.text_input(key='b1', label="Name", placeholder='location name or description')
+            cb1, cb2 = st.columns([1,1])
+            with cb1:
+                country = st.text_input(key="b3", label="Country", placeholder='US')
+            with cb2:    
+                state = st.text_input(key="b2", label="State", placeholder='CA')
+            cb3, cb4 = st.columns([1,1])    
+            with cb3:
+                latitude = st.text_input(key="b4", label="Latitude", placeholder='0.0')
+            with cb4:    
+                longitude = st.text_input(key='b5', label="Latitude", on_change=set_state, args=[1], placeholder='0.0')
+        if st.session_state.stage >=1:
+                st.info(f'{name}: {country}, {state}, {latitude}, {longitude}', icon=':material/check:')
 
-    if st.session_state.stage >= 2:
-        try:
-            ld= location_details(name=name, query=d)
+                cb5, cb6 = st.columns([1,1])
+                with cb5: 
+                    st.button('submit',use_container_width=True)
+                with cb6: 
+                    st.button('Cancel',use_container_width=True,  on_click=set_state, args=[0])
 
-            st.info(f'Latitude: {ld["lat"]:.3f} and Longitude: {ld["lon"]:.3f} found for : {name}', icon=":material/done_all:")
-        except Exception as e:
-            st.error(f'Failed {e} to search latitude and longitude for location: {name}', icon=":material/error:") 
+    else:    
+        if st.session_state.stage >= 0:
+            name = st.text_input('Name')
+            address = st.text_input('Address', on_change=set_state, args=[1])
 
-        c1, c2 = st.columns([1,1],gap="small")
-        with c1:
-            st.button('submit')
-        with c2:
-            st.button('Cancel', on_click=set_state, args=[0])
+        if st.session_state.stage >= 1:
+            try:
+                pa = parse_address(address)
+                if pa is None:
+                    set_state(1)
+                else:
+                    d = display_parsed_address(pa)
+                    st.info(f'Search Latitude and Longitude for : {name}', icon=":material/search:")
+                    set_state(2)
+            except Exception as e:
+                    st.error(f'Failed {e} to validate address for location: {name}', icon=":material/error:")  
+
+        if st.session_state.stage >= 2:
+            try:
+                ld= location_details(name=name, query=d)
+
+                st.info(f'Latitude: {ld["lat"]:.3f} and Longitude: {ld["lon"]:.3f} found for : {name}', icon=":material/done_all:")
+            except Exception as e:
+                st.error(f'Failed {e} to search latitude and longitude for location: {name}', icon=":material/error:") 
+
+                st.button('submit')
+                st.button('Cancel', on_click=set_state, args=[0])
 
 add_location()              
