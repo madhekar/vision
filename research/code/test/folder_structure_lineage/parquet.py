d1fa4b2ae6972bbc0a0f6e6066c718807ae842f4
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-28 15:31:10 -0800

    exception handling

--- research/code/test/folder_structure_lineage/parquet.py
+++ research/code/test/folder_structure_lineage/parquet.py
@@ -15,32 +15,43 @@ def define_schema():
     return static_location_schema
 
 def create_default_location(schema):
-
-    df = pd.read_csv("default-locations.csv", delimiter=',')
-    #create default table
-    table = pa.Table.from_pandas(df, schema=schema) 
     try:
-      pq.write_table(table, 'pqt/static_locations.parquet')#, partition_cols=['country','state'])
+        df = pd.read_csv("default-locations.csv", delimiter=',')
+        #create default table
+        table = pa.Table.from_pandas(df, schema=schema) 
+        pq.write_table(table, 'pqt/static_locations.parquet')#, partition_cols=['country','state'])
     except Exception as e:
-       print(f'failed with exception: {e}')
+        print(f'create default locations parquet failed with exception: {e}')
 
    
 def read_parquet_file():
+   
    df = pd.read_parquet('pqt/')
    return df
 
-def append_locations(path='pqt', file_name='default-locations.parquet', df):
-   df.to_parquet(os.path.join(path, file_name), engine="pyarrow", mode="append")
-
-   
+def append_locations(df,path='pqt', file_name='default-locations.parquet'):
+    try:
+        df.to_parquet(os.path.join(path, file_name), engine="pyarrow", mode="append")
+    except Exception as e:
+        print(f"append locations failed with exception: {e}")
    
 def get_all_loc_by_country(country):
-   df = pd.read_parquet('pqt/')   
-   print(df[df['country'] == country])
+    rdf = None
+    try:
+        df = pd.read_parquet("pqt/")
+        rdf = df[(df["country"] == country)]
+    except Exception as e:
+        print(f"get all locations by contry: {country} failed with exception: {e}")
+    return rdf
 
 def get_all_loc_by_country_and_state(country, state):
-    df = pd.read_parquet("pqt/")
-    print(df[(df["country"] == country) & (df["state"] == state)])
+    rdf = None
+    try:
+      df = pd.read_parquet("pqt/")
+      rdf = df[(df["country"] == country) & (df["state"] == state)]
+    except Exception as e:
+       print(f'get all locations by contry: {country} and state: {state} failed with exception: {e}') 
+    return rdf    
 
 create_default_location(define_schema())   
 
