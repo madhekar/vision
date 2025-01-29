c484a0e779a0e2977d9d82b2759e4e1969d04c45
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-29 13:39:41 -0800

    standardize

--- research/code/test/folder_structure_lineage/loc_formatting.py
+++ research/code/test/folder_structure_lineage/loc_formatting.py
@@ -1,23 +1,28 @@
+import glob
 import pandas as pd
 import country_converter as coco
 import us_states as ust
 
 cc = coco.CountryConverter()
 
-with open("locations-US-buildings.csv", 'r') as temp_f:
-    f_arr = []
-    for line in temp_f.readlines():
-        a_ = line.split(',')
-        a_ = [s.strip() for s in a_]
-        if len(a_) > 5:
-            a_[0:len(a_) - 4] = ['-'.join(a_[0:len(a_)-4])]
-        f_arr.append(a_)    
-    # create data frame    
-    df = pd.DataFrame(f_arr, columns=['name', 'state', 'country', 'latitude', 'longitude'])    
-    
-    # format country codes
-    df["country"] = cc.pandas_convert(series=df["country"], to="ISO2")
-    # standardize us state codes
-    df['state'] = df['state'].apply(lambda x: ust.multiple_replace(ust.statename_to_abbr, x))
-    print(df)
+def transform_raw_locations(fpath):
+    with open(fpath, "r") as temp_f:
+        f_arr = []
+        for line in temp_f.readlines():
+            a_ = line.split(',')
+            a_ = [s.strip() for s in a_]
+            if len(a_) > 5:
+                a_[0:len(a_) - 4] = ['-'.join(a_[0:len(a_)-4])]
+            f_arr.append(a_)    
+        # create data frame    
+        df = pd.DataFrame(f_arr, columns=['name', 'state', 'country', 'latitude', 'longitude'])    
+        
+        # format country codes
+        df["country"] = cc.pandas_convert(series=df["country"], to="ISO2")
+        # standardize us state codes
+        df['state'] = df['state'].apply(lambda x: ust.multiple_replace(ust.statename_to_abbr, x))
+        print(df)
 
+if __name__=='__main__':
+    cvs_files = glob.glob('locations/*.csv')
+    print(cvs_files)
