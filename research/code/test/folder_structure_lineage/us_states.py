84fc6566653a37338c1f85b225e7080a1424fdd5
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-30 09:28:00 -0800

    fix issues

--- research/code/test/folder_structure_lineage/us_states.py
+++ research/code/test/folder_structure_lineage/us_states.py
@@ -1,6 +1,35 @@
 import re
 
 statename_to_abbr = {
+    #india
+    "Andhra Pradesh": "AP", 
+    "Arunachal Pradesh": "AR", 
+    "Assam": "AS", 
+    "Bihar": "BR", 
+    "Chhattisgarh": "CG", 
+    "Goa": "GA", 
+    "Gujarat": "GJ", 
+    "Haryana": "HR", 
+    "Himachal Pradesh": "HP", 
+    "Jharkhand": "JH", 
+    "Karnataka": "KA", 
+    "Kerala": "KL", 
+    "Madhya Pradesh": "MP", 
+    "Maharashtra": "MH", 
+    "Manipur": "MN", 
+    "Meghalaya": "ML", 
+    Mizoram: MZ 
+    Nagaland: NL 
+    Odisha: OD 
+    Punjab: PB 
+    Rajasthan: RJ 
+    Sikkim: SK 
+    Tamil Nadu: TN 
+    Telangana: TG 
+    Tripura: TR 
+    Uttarakhand: UK 
+    Uttar Pradesh: UP 
+    "West Bengal": "WB", 
     # Other
     "District Of Columbia": "DC",
     "Washington Dc": "DC",
