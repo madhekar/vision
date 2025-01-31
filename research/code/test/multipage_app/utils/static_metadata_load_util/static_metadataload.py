c1dd5e670f45b0f0963ef50782e30636941a86d4
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-31 14:54:16 -0800

    refactor

--- research/code/test/multipage_app/utils/static_metadata_load_util/static_metadataload.py
+++ research/code/test/multipage_app/utils/static_metadata_load_util/static_metadataload.py
@@ -1,2 +1,23 @@
+import streamlit as st
+
+import os
+from utils.config_util import config
+import streamlit as st
 
 
+
+
+def execute(source_name):
+    (
+        static_metadata_data_path,
+        static_metadata_file,
+    ) = config.dataload_config_load()
+
+    """
+      paths to import files
+    """
+    mdatapath = os.path.join(static_metadata_data_path, static_metadata_file)
+
+
+if __name__ == "__main__":
+    execute()
