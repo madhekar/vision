ff249204d73d74a8d929f234aaa9b9361b1efcdb
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-01-31 15:23:38 -0800

    refactor

--- research/code/test/multipage_app/utils/static_metadata_load_util/static_metadataload.py
+++ research/code/test/multipage_app/utils/static_metadata_load_util/static_metadataload.py
@@ -1,23 +1,21 @@
-import streamlit as st
-
 import os
 from utils.config_util import config
+from utils.util import fast_parquet_util as fpu
 import streamlit as st
 
-
+def transform_and_add_static_metadata(location_metadata_path, final_storage):
+    pass
 
 
 def execute(source_name):
-    (
-        static_metadata_data_path,
-        static_metadata_file,
-    ) = config.dataload_config_load()
+
+    (location_metadata_path, address_metadata_path, static_metadata_path, static_metadata_file)  = config.static_metadata_config_load()
 
     """
       paths to import files
     """
-    mdatapath = os.path.join(static_metadata_data_path, static_metadata_file)
-
+    metadata_storage_path = os.path.join(static_metadata_path, static_metadata_file)
+    transform_and_add_static_metadata(location_metadata_path=location_metadata_path, final_storage=metadata_storage_path)
 
 if __name__ == "__main__":
     execute()
