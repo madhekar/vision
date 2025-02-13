a320f2f07df0356d94130bbad085fcd7a216b21e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-02-12 17:36:17 -0800

    refactor image annotate

---
+++ research/code/test/image_annotation/annotation_poc.py
@@ -0,0 +1,74 @@
+import streamlit as st
+from glob import glob
+import pandas as pd
+import json
+from streamlit_image_annotation import detection, classification, pointdet
+import torchvision.transforms.functional as F
+
+mode = st.tabs(["Detection", "Classification", "Point"])
+label_list = ["Esha","Anjali","Bhalchandra","Asha","Kumar","Shibangi","Sham","Advait","Chandrakant","Puja","Sagar","Shoma","Bhiman","friend","relative","kid","man","woman" ]
+image_path_list = glob("/Users/emadhekar/Desktop/images/*.*")
+# label_list = ['deer', 'human', 'dog', 'penguin', 'framingo', 'teddy bear']
+# image_path_list = glob('image/*.jpg')
+
+
+
+with mode[0]:
+    if 'result_dict_det' not in st.session_state:
+        result_dict = {}
+        for img in image_path_list:
+            result_dict[img] = {'bboxes': [],'labels':[]}
+        st.session_state['result_dict_det'] = result_dict.copy()
+
+    num_page = st.slider('page', 0, len(image_path_list)-1, 0, key='slider_det')
+    target_image_path = image_path_list[num_page]
+
+    new_labels = detection(image_path=target_image_path, 
+                        bboxes=st.session_state['result_dict_det'][target_image_path]['bboxes'], 
+                        labels=st.session_state['result_dict_det'][target_image_path]['labels'], 
+                        label_list=label_list, use_space=True, key=target_image_path+'_det')
+    if new_labels is not None:
+        st.session_state['result_dict_det'][target_image_path]['bboxes'] = [v['bbox'] for v in new_labels]
+        st.session_state['result_dict_det'][target_image_path]['labels'] = [v['label_id'] for v in new_labels]
+    st.json(st.session_state['result_dict_det'])
+
+    btn = st.button('Submit')
+    if btn:
+        with open("data.json", "w") as file:
+           json.dump(st.session_state.result_dict_det, file, indent=4)
+
+with mode[1]:
+
+    if 'result_df_cls' not in st.session_state:
+        st.session_state['result_df_cls'] = pd.DataFrame.from_dict({'image': image_path_list, 'label': [0]*len(image_path_list)}).copy()
+
+    num_page = st.slider('page', 0, len(image_path_list)-1, 0, key='slider_cls')
+
+    label = classification(image_path_list[num_page], 
+                            label_list=label_list, 
+                            default_label_index=int(st.session_state['result_df_cls'].loc[num_page, 'label']))
+
+    if label is not None and label['label'] != st.session_state['result_df_cls'].loc[num_page, 'label']:
+        st.session_state['result_df_cls'].loc[num_page, 'label'] = label_list.index(label['label'])
+    st.table(st.session_state['result_df_cls'])
+
+with mode[2]:
+
+    if 'result_dict_point' not in st.session_state:
+        result_dict = {}
+        for img in image_path_list:
+            result_dict[img] = {'points': [[0,0],[50,150], [200,200]],'labels':[0,3,4]}
+        st.session_state['result_dict_point'] = result_dict.copy()
+
+    num_page = st.slider('page', 0, len(image_path_list)-1, 0, key='slider_point')
+    target_image_path = image_path_list[num_page]
+
+    new_labels = pointdet(image_path=target_image_path, 
+                            label_list=label_list, 
+                            points=st.session_state['result_dict_point'][target_image_path]['points'],
+                            labels=st.session_state['result_dict_point'][target_image_path]['labels'],
+                            use_space=True, key=target_image_path+'_point')
+    if new_labels is not None:
+        st.session_state['result_dict_point'][target_image_path]['points'] = [v['point'] for v in new_labels]
+        st.session_state['result_dict_point'][target_image_path]['labels'] = [v['label_id'] for v in new_labels]
+    st.json(st.session_state['result_dict_point'])
---
+++ research/code/test/image_annotation/detection/BoxUtils.py
@@ -0,0 +1,73 @@
+class BoxUtils(object):
+    @staticmethod
+    def box_cxcywh_to_xyxy(x):
+        x_c, y_c, w, h = x.unbind(-1)
+        b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+             (x_c + 0.5 * w), (y_c + 0.5 * h)]
+        return torch.stack(b, dim=-1)
+
+    @staticmethod
+    def box_xyxy_to_cxcywh(x):
+        x0, y0, x1, y1 = x.unbind(-1)
+        b = [(x0 + x1) / 2, (y0 + y1) / 2,
+             (x1 - x0), (y1 - y0)]
+        return torch.stack(b, dim=-1)
+
+    @staticmethod
+    def rescale_bboxes(out_bbox, size):
+        img_h, img_w = size
+        b = BoxUtils.box_cxcywh_to_xyxy(out_bbox)
+        b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+        return b
+
+    @staticmethod
+    def box_area(boxes):
+        """
+        Computes the area of a set of bounding boxes, which are specified by its
+        (x1, y1, x2, y2) coordinates.
+        Arguments:
+            boxes (Tensor[N, 4]): boxes for which the area will be computed. They
+                are expected to be in (x1, y1, x2, y2) format
+        Returns:
+            area (Tensor[N]): area for each box
+        """
+        return (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+        
+    @staticmethod
+    # modified from torchvision to also return the union
+    def box_iou(boxes1, boxes2):
+        area1 = BoxUtils.box_area(boxes1)
+        area2 = BoxUtils.box_area(boxes2)
+
+        lt = torch.max(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
+        rb = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
+
+        wh = (rb - lt).clamp(min=0)  # [N,M,2]
+        inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+
+        union = area1[:, None] + area2 - inter
+
+        iou = inter / union
+        return iou, union
+
+    @staticmethod
+    def generalized_box_iou(boxes1, boxes2):
+        """
+        Generalized IoU from https://giou.stanford.edu/
+        The boxes should be in [x0, y0, x1, y1] format
+        Returns a [N, M] pairwise matrix, where N = len(boxes1)
+        and M = len(boxes2)
+        """
+        # degenerate boxes gives inf / nan results
+        # so do an early check
+        assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
+        assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+        iou, union = BoxUtils.box_iou(boxes1, boxes2)
+
+        lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
+        rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
+
+        wh = (rb - lt).clamp(min=0)  # [N,M,2]
+        area = wh[:, :, 0] * wh[:, :, 1]
+
+        return iou - (area - union) / area
