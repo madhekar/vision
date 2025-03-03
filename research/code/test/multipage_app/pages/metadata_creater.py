a5caa6424c20e3cf45d9bcab00147b8b8b47f8bc
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-03 14:47:40 -0800

    code cleanup

--- research/code/test/multipage_app/pages/metadata_creater.py
+++ research/code/test/multipage_app/pages/metadata_creater.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from utils.preprocess_util import pp1 #preprocess as pp
+from utils.preprocess_util import preprocess as pp
 
 st.subheader("METADATA: GENERATE", divider="gray")
 
 bcreate_metadata = st.button('create image metadata')
 if bcreate_metadata:
-   pp1.execute()
+   pp.execute()
