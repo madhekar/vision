7ddbc95ea0b348eb02b1f9828b363367565dacfa
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-07 14:03:13 -0800

    fix various issues image quality

--- research/code/test/multipage_app/utils/missing_util/missing_metadata.py
+++ research/code/test/multipage_app/utils/missing_util/missing_metadata.py
@@ -1,6 +1,7 @@
 import subprocess
 import shlex
 import os
+from utils.util import model_util as mu
 from utils.config_util import config
 from utils.util import statusmsg_util as sm
 """
@@ -10,20 +11,25 @@ missing-metadata:
   missing_metadata_file: missing-metadata-wip.csv
 
 """
-def execute():
+def execute(source_name):
     sm.add_messages("metadata", "s| starting to analyze missing metadata files...")
 
     imp, mmp, mmf = config.missing_metadata_config_load()
 
+    input_image_path = os.path.join(imp, source_name)
+
     args = shlex.split(
-        f"exiftool -GPSLongitude -GPSLatitude -DateTimeOriginal -csv -T -r -n {imp}"
+        f"exiftool -GPSLongitude -GPSLatitude -DateTimeOriginal -csv -T -r -n {input_image_path}"
     )
     proc = subprocess.run(args, capture_output=True)
 
-    output_file_path = os.path.join(mmp, mmf)
-    with open(output_file_path, "wb") as output:
+    arc_folder_name_dt = mu.get_foldername_by_datetime()
+    output_file_path = os.path.join(mmp, source_name, arc_folder_name_dt)
+    if not os.path.exists(output_file_path):
+        os.makedirs(output_file_path)
+    with open(os.path.join(output_file_path, mmf), "wb") as output:
         output.write(proc.stdout)
 
     sm.add_messages("metadata",f"w| finized to analyze missing metadata files created {output_file_path}.",)    
 if __name__=='__main__':
-    execute()
+    execute(source_name="")
