38bf809735665423dd4098dbca2d3017d0b5b22e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-18 13:20:04 -0700

    add example

---
+++ research/code/test/multiprocessing/vectordb/simple_q.py
@@ -0,0 +1,63 @@
+import multiprocessing
+from multiprocessing import Queue
+import chromadb
+from chromadb.utils import embedding_functions
+import time
+
+def process_data(data_queue, collection_name):
+    client = chromadb.Client()
+    default_ef = embedding_functions.SentenceTransformerEmbeddingFunction()
+    collection = client.get_or_create_collection(name=collection_name, embedding_function=default_ef)
+    while True:
+        item = data_queue.get()
+        if item is None:
+            break
+        collection.add(documents=[item['document']], ids=[item['id']])
+        data_queue.task_done()
+    print(f"Worker finished processing for {collection_name}")
+
+def main():
+    collection_name = "my_collection"
+    data_queue = Queue()
+    
+    # Sample data
+    data = [
+        {"id": "1", "document": "This is document 1."},
+        {"id": "2", "document": "This is document 2."},
+        {"id": "3", "document": "This is document 3."},
+        {"id": "4", "document": "This is document 4."},
+        {"id": "5", "document": "This is document 5."},
+    ]
+
+    # Initialize and start worker processes
+    num_processes = multiprocessing.cpu_count()
+    processes = []
+    for _ in range(num_processes):
+        process = multiprocessing.Process(target=process_data, args=(data_queue, collection_name))
+        processes.append(process)
+        process.start()
+
+    # Populate queue with data
+    for item in data:
+        data_queue.put(item)
+
+    # Add sentinel values to signal the end of processing
+    for _ in range(num_processes):
+        data_queue.put(None)
+
+    # Wait for all tasks to be processed
+    data_queue.join()
+
+    # Wait for all worker processes to finish
+    for process in processes:
+        process.join()
+    print("All workers finished")
+
+    # Perform a query to verify data is in ChromaDB
+    client = chromadb.Client()
+    collection = client.get_collection(name=collection_name)
+    results = collection.query(query_texts=["something about document"], n_results=2)
+    print(results)
+
+if __name__ == "__main__":
+    main()
