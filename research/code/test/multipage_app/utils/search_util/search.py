230c5374fe7f0754918d8c12628862ad6849df58
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-21 14:56:54 -0700

    code cleanup

--- research/code/test/multipage_app/utils/search_util/search.py
+++ research/code/test/multipage_app/utils/search_util/search.py
@@ -116,38 +116,6 @@ def search_fn():
 
 
 def execute():
-
     search_fn()
-    # sm.add_messages("metadata", "s| starting to analyze missing metadata files...")
 
-    # imp, mmp, mmf = config.missing_metadata_config_load()
-
-    # input_image_path = os.path.join(imp, source_name)
-    # try:
-    #     args = shlex.split(
-    #         f"exiftool -gps:GPSLongitude -gps:GPSLatitude -DateTimeOriginal -csv -T -r -n {input_image_path}"
-    #     )
-    #     proc = subprocess.run(args, capture_output=True)
-    # except Exception as e:
-    #     print(f"error {e}")
-
-    # # print(proc.stderr)
-    # # print(proc.stdout)
-
-    # # arc_folder_name_dt = mu.get_foldername_by_datetime()
-
-    # output_file_path = os.path.join(mmp, source_name)  # , arc_folder_name_dt)
-
-    # if not os.path.exists(output_file_path):
-    #     os.makedirs(output_file_path)
-
-    # with open(os.path.join(output_file_path, mmf), "wb") as output:
-    #     output.write(proc.stdout)
-
-    # create_missing_report(os.path.join(output_file_path, mmf))
-
-    # sm.add_messages(
-    #     "metadata",
-    #     f"w| finized to analyze missing metadata files created {output_file_path}.",
-    # )
     
