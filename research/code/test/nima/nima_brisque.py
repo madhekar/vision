2da58addd1073b38412d797b17a022700e9dc0d5
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-26 08:07:34 -0700

    benchmark

---
+++ research/code/test/nima/nima_brisque.py
@@ -0,0 +1,34 @@
+import torch
+import pyiqa
+from PIL import Image
+from torchvision import transforms
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+nima_metric = pyiqa.create_metric("nima", device=device)
+brisque_metric = pyiqa.create_metric("brisque", device=device)
+
+
+
+# image preprocessing
+preprocess = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
+sample_img = (
+    "/home/madhekar/work/home-media-app/data/train-data/img/AnjaliBackup/images.jpeg"
+)
+
+image = Image.open(sample_img)
+image_tensor = preprocess(image).unsqueeze(0)
+
+normalized_tensor = (image_tensor - image_tensor.min()) / (
+    image_tensor.max() - image_tensor.min()
+)
+with torch.no_grad():
+    score = nima_metric(normalized_tensor)
+
+print(f"nima score: {score.item():.4f}")
