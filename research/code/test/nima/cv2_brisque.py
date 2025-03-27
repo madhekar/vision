8e48cffa614f55122eb3cff5f7eb3b02711d7afa
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-26 22:08:16 -0700

    cv2 bug

--- research/code/test/nima/cv2_brisque.py
+++ research/code/test/nima/cv2_brisque.py
@@ -3,7 +3,7 @@ import cv2 as cv
 img = cv.imread("/home/madhekar/work/home-media-app/data/train-data/img/AnjaliBackup/images.jpeg")
 grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blurScore = cv.Laplacian(grey, cv.CV_64F).var()
-score = cv.quality.QualityBRISQUE_compute(img, "brisque_model_live.yml", "brisque_range_live.yml")
+#score = cv.quality.QualityBRISQUE_compute(img, "brisque_model_live.yml", "brisque_range_live.yml")
 
 print(f' >> Blur Score: {blurScore}')
-print(f' >> BRISQUE Score: {score}')
+#print(f' >> BRISQUE Score: {score}')
