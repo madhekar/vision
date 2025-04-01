ab0e2eb9980827589598c109e04e9a64a41c13f0
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-04-01 15:43:17 -0700

    test location prediction model

--- research/code/test/geoclip/predict_latlon.py
+++ research/code/test/geoclip/predict_latlon.py
@@ -2,12 +2,11 @@ import os
 from geoclip import GeoCLIP
 
 model = GeoCLIP()
-sample_path = "/Users/emadhekar/tmp/images"
-
+sample_path = "/home/madhekar/work/home-media-app/data/final-data/img/AnjaliBackup"
+print("Location,Latitude,Longitude")
 for rt, _, files in os.walk(sample_path, topdown=True):
-    print("Location,Latitude,Longitude")
     for file in files:
         top_pred_gps, top_pred_prob = model.predict(os.path.join(rt, file), top_k=1)
         if top_pred_prob[0] > 0.099:
            lat, lon = top_pred_gps[0]
-           print(f"{file}-{top_pred_prob[0]:.6f},{lat:.6f},{lon:.6f}")
+           print(f"{rt}/{file}-{top_pred_prob[0]:.6f},{lat:.6f},{lon:.6f}")
