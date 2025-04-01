ebdbb64d4c8c80eeff37e14146db1cc8a4255fd6
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-03-31 20:36:07 -0700

    geolocation estimation

--- research/code/test/geoclip/test.py
+++ research/code/test/geoclip/test.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from geoclip import GeoCLIP
 
@@ -5,12 +6,19 @@ model = GeoCLIP()
 
 image_path = "/Users/emadhekar/Downloads/gettyimages-2147692990-170667a.jpg"
 
-top_pred_gps, top_pred_prob = model.predict(image_path, top_k=5)
 
-print("Top 5 GPS Predictions")
-print("=====================")
-for i in range(5):
-    lat, lon = top_pred_gps[i]
-    print(f"Prediction {i+1}: ({lat:.6f}, {lon:.6f})")
-    print(f"Probability: {top_pred_prob[i]:.6f}")
-    print("")
+sample_path = "/Users/emadhekar/tmp/images"
+
+for rt, _, files in os.walk(sample_path, topdown=True):
+    print("Location,Latitude,Longitude")
+    for file in files:
+        top_pred_gps, top_pred_prob = model.predict(os.path.join(rt, file), top_k=1)
+
+        #print("Top 5 GPS Predictions")
+        #print("=====================")
+        for i in range(1):
+            lat, lon = top_pred_gps[i]
+            #print(f"{file}, Prediction {i+1}: ({lat:.6f}, {lon:.6f}): Prob: {top_pred_prob[i]:.6f}")
+            # print(f"Probability: {top_pred_prob[i]:.6f}")
+            print(f"{file},{lat:.6f},{lon:.6f}")
+            #print("* * *")
