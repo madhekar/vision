b452f752c7143e9b95bfd8341b97be3e30648461
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-04-09 11:38:49 -0700

    geo detection

--- research/code/test/latlong/gpsphoto_test.py
+++ research/code/test/latlong/gpsphoto_test.py
@@ -1,10 +1,12 @@
 from GPSPhoto import gpsphoto
 # Get the data from image file and return a dictionary
 data = gpsphoto.getGPSData(
-    "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/bf98198d-fcc6-51fe-a36a-275c06005669/IMAG0191.jpg"
+    #"/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/bf98198d-fcc6-51fe-a36a-275c06005669/IMAG0191.jpg"
+    "/home/madhekar/work/home-media-app/data/train-data/img/AnjaliBackup/IMAG2477.jpg"
 )
 rawData = gpsphoto.getRawData(
-    "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/bf98198d-fcc6-51fe-a36a-275c06005669/IMAG0191.jpg"
+    #"/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/bf98198d-fcc6-51fe-a36a-275c06005669/IMAG0191.jpg"
+    "/home/madhekar/work/home-media-app/data/train-data/img/AnjaliBackup/IMAG2477.jpg"
 )
 
 # Print out just GPS Data of interest
