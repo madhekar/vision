51b324df32e691dfa0200488ef41d074989e3294
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-04-25 08:32:29 -0700

    coco utils

---
+++ research/code/test/annotations/util/merge_coco_annotations.py
@@ -0,0 +1,57 @@
+# pip install pycocotools
+from pycocotools.coco import COCO
+import json
+
+def merge_coco_json(json_files, output_file):
+    merged_annotations = {
+        "info": {},
+        "licenses": [],
+        "images": [],
+        "annotations": [],
+        "categories": []
+    }
+
+    image_id_offset = 0
+    annotation_id_offset = 0
+    category_id_offset = 0
+    existing_category_ids = set()
+
+    for idx, file in enumerate(json_files):
+        coco = COCO(file)
+
+        # Update image IDs to avoid conflicts
+        for image in coco.dataset['images']:
+            image['id'] += image_id_offset
+            merged_annotations['images'].append(image)
+
+        # Update annotation IDs to avoid conflicts
+        for annotation in coco.dataset['annotations']:
+            annotation['id'] += annotation_id_offset
+            annotation['image_id'] += image_id_offset
+            merged_annotations['annotations'].append(annotation)
+
+        # Update categories and their IDs to avoid conflicts
+        for category in coco.dataset['categories']:
+            if category['id'] not in existing_category_ids:
+                category['id'] += category_id_offset
+                merged_annotations['categories'].append(category)
+                existing_category_ids.add(category['id'])
+
+        image_id_offset = len(merged_annotations['images'])
+        annotation_id_offset = len(merged_annotations['annotations'])
+        category_id_offset = len(merged_annotations['categories'])
+
+    # Save merged annotations to output file
+    with open(output_file, 'w') as f:
+        json.dump(merged_annotations, f)
+
+# List of paths to COCO JSON files to merge
+json_files = ["part1.json", "part2.json"]
+
+# Output file path for merged annotations
+output_file = "merged_coco.json"
+
+# Merge COCO JSON files
+merge_coco_json(json_files, output_file)
+
+print("Merged COCO JSON files saved to", output_file)
