8c5bce578824ca22cccf62617a80ba3db407a51b
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-05-01 11:51:20 -0700

    new modules for face detection

---
+++ research/code/test/face/base_face_infer.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import joblib
+import os
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from mtcnn.mtcnn import MTCNN
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+import base_facenet
+
+class inferance_faces:
+    def __init__(self, _mpath, _ppath, _lpath):
+        self.detector = MTCNN()
+        self.t_size = (160, 160)
+        self.x, self.y = np.load(_ppath)
+        self.model = joblib.load(_mpath)
+        self.label_encoder = joblib(_lpath)
+        self.facenet_i = base_facenet()
+
+    def extract_faces(self, img):
+        tin = cv.imread(img)
+        tin = cv.cvtColor(tin, cv.COLOR_BGR2RGB)
+        res = self.detector.detect_faces(tin)
+
+        if res and len(res) > 0:
+            dict = {}
+            cnt = 1
+            for d in res:
+                x, y, w, h = d["box"]
+                face = tin[y : y + h, x : x + w]
+                face_arr = cv.resize(face, (160, 160))
+                key = f"face_{cnt}"
+                dict[key] = face_arr
+                cnt += 1
+        return dict
+
+    def predict_names(self, dict):
+        if dict and dict.keys() > 0:
+            names = []
+            for e in dict.items():
+                test_im = self.facenet_i.get_embeddings(e.value())
+                test_im = [test_im]
+
+                ypred = self.model.predict(test_im)
+                names.append(self.label_encoder.inverse_transform(ypred))
