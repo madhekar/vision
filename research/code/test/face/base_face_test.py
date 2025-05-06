91cbe6c3e48e033f9d59102f85c74e096879b452
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-05-06 15:27:15 -0700

    revised strategy

--- research/code/test/face/base_face_test.py
+++ research/code/test/face/base_face_test.py
@@ -1,6 +1,8 @@
 import os
 import base_face_infer as bft
 import multiprocessing as mp
+import concurrent.futures 
+
 
 class base_face_res:
     def __init__(self):
@@ -24,24 +26,53 @@ class base_face_res:
         names =  self.faces_infer_obj.predict_names(img)
         return (names)
     
-bfs = base_face_res()
-bfs.init()    
+   
 
 def worker(img):
-   names = bfs.pred_names_of_peope(img)
+   names = bfs.pred_names_of_people(img)
    return names
+
+"""
+import concurrent.futures
+import multiprocessing
+import time
+
+    cores = multiprocessing.cpu_count()
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=cores) as executor:
+        res = executor.map(task_1, nums)
+
+        for r in res:
+            print(r)
+
+
+"""
+# pool iniitializer
+def pool_init(BFS):
+    global bfs
+    bfs = BFS
 
 def exec():
     img_path = '/home/madhekar/work/home-media-app/data/input-data/img'
-    processes = []
-    for img in os.listdir(img_path):
-      p = mp.Process(target=worker, args=(img,))
-      processes.append(p)
-      #names =  bfs.pred_names_of_people( os.path.join(img_path, img))
-      p.start()
-      p.join()
-      names = p.name
-      print(f' file: {img} identified faces: {names}')
+    cores =  mp.cpu_count()
+    BFS = base_face_res()
+    BFS.init() 
+    pool_init(BFS)
+
+
+    imgs = [os.path.join (img_path, ifile) for ifile in os.listdir(img_path)]
+    with concurrent.futures.ProcessPoolExecutor(max_workers=cores, initializer=pool_init, initargs=(BFS,)) as executor:
+       res = executor.map(worker, imgs)
+       for r in res:
+           print(r)
+    # for img in os.listdir(img_path):
+    #   p = mp.Process(target=worker, args=(img,))
+    #   processes.append(p)
+    #   #names =  bfs.pred_names_of_people( os.path.join(img_path, img))
+    #   p.start()
+    #   p.join()
+    #   names = p.name
+    #   print(f' file: {img} identified faces: {names}')
    
 if __name__=="__main__":
   exec()
