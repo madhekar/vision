15a1ce0939b374f1d196648a8542168f3e545916
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-05-09 17:13:59 -0700

    process sequ because of keras issue with multiprocessing

--- research/code/test/multipage_app/utils/face_util/base_face_test.py
+++ research/code/test/multipage_app/utils/face_util/base_face_test.py
@@ -1,4 +1,6 @@
 import os
+import time
+import pandas as pd
 import streamlit as st
 from utils.face_util import base_face_infer as bftf
 from utils.config_util import config
@@ -44,27 +46,49 @@ class base_face_res:
 #     names = ibtf.predict_names(img)
 #     return names
 
-# @st.cache_resource
-# def init():
-#     (faces_dir,  class_embeddings_folder, class_embeddings, label_encoder_path, label_encoder, faces_svc_path, faces_svc) = config.faces_config_load()
+@st.cache_resource
+def init():
+    (faces_dir,  class_embeddings_folder, class_embeddings, label_encoder_path, label_encoder, faces_svc_path, faces_svc) = config.faces_config_load()
 
-#     faces_embeddings, faces_label_enc, faces_model_svc = (
-#         os.path.join(class_embeddings_folder, class_embeddings),
-#         os.path.join(label_encoder_path,label_encoder),
-#         os.path.join(faces_svc_path,faces_svc)
-#         )
-#     ibtf = bftf.infer_faces(faces_embeddings, faces_label_enc, faces_model_svc)    
-#     print(f'-->init face test{label_encoder_path}{faces_svc_path}')
-#     return ibtf
+    faces_embeddings, faces_label_enc, faces_model_svc = (
+        os.path.join(class_embeddings_folder, class_embeddings),
+        os.path.join(label_encoder_path,label_encoder),
+        os.path.join(faces_svc_path,faces_svc)
+        )
+    ibtf = bftf.infer_faces(faces_embeddings, faces_label_enc, faces_model_svc)    
+    ibtf.init()
+    print(f'-->init face test{label_encoder_path}{faces_svc_path}')
+    return ibtf
 
-# def exec():
-#     img_path = '/home/madhekar/work/home-media-app/data/input-data/img'
-#     ibtf = init()
-#     for img in os.listdir(img_path):
-#       nfaces, names = pred_names(ibtf, os.path.join(img_path, img))
-#       print(f' file: {img} # of faces: {nfaces} identified faces: {names}')
+def process_images_in_batch(ibtf, parquet_file, img_dir, batch_size=10):
+    img_paths= [os.path.join(img_dir, img) for img in os.listdir(img_dir)]
+    num_imgs = len(img_paths)
 
-# # kick-off face training generation
-# if __name__ == "__main__":
-#     exec()
+    for i in range(0, num_imgs, batch_size):
+        batch_paths = img_paths[i: i+ batch_size]
+        result = {(file_path, ibtf.pred_names_of_people(file_path)) for file_path in batch_paths }
+        df = pd.DataFrame(result, columns=["image", "people"])
+        df.to_parquet(parquet_file, mode="append", engine="fastparquet")
+        time.sleep(2)
+    return num_imgs, 'Done!'
+
+def exec():
+    ibtf =  init()
+    parquet_file = 'people_in_image.parquet'
+    img_path = '/home/madhekar/work/home-media-app/data/input-data/img'
+    num, ret = process_images_in_batch(ibtf, parquet_file, img_path,batch_size=100)
+    st.info(f'processed {num} images to predict people with status: {ret}')
+
+    #r = {(os.path.join(img_path,img_file), ibtf.pred_names_of_people(os.path.join(img_path, img_file))) for img_file in os.listdir(img_path)[0:2]}
+    # df = pd.DataFrame(r, columns=['image', 'people'])
+    # print(df)
+    # df.to_parquet('./image_people.parquet')
+
+    # for img in os.listdir(img_path):
+    #   nfaces, names = pred_names(ibtf, os.path.join(img_path, img))
+    #   print(f' file: {img} # of faces: {nfaces} identified faces: {names}')
+
+# kick-off face training generation
+if __name__ == "__main__":
+    exec()
     
