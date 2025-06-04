39df143a74cb66d23e5e9decfc298cdc8fc4fa85
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-06-04 10:31:50 -0700

    poc

---
+++ research/code/test/process_structures/process.py
@@ -0,0 +1,125 @@
+ll = [
+    [
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/5442fe69-599e-5cde-a409-bc46145701bc/vcm_s_kf_repr_886x587.jpg",
+            "2a470c5a-cbb6-4269-82a4-71f3426e7acb",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/319dbc8e-2c74-58cd-8f7a-43c1e7447c1d/vcm_s_kf_repr_886x587.jpg",
+            "a26f6faf-195e-4a09-9d29-b6f4f3b57840",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/49ba0a90-d4f1-5420-9bcc-1eaa231656f2/vcm_s_kf_repr_832x624.jpg",
+            "d99054f3-010f-4e95-a788-fb1c44f02c9a",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/916cc21d-6dc2-5369-b994-ae97e411b377/vcm_s_kf_repr_832x624.jpg",
+            "def12a35-4f15-45b2-b305-cb1f07c1fc46",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/56a82a18-dd40-5ff5-90e8-9122a46a3f94/vcm_s_kf_repr_832x624.jpg",
+            "16a7abbd-04a9-4b91-bb01-8fb48e8d2619",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/07ef46d5-4941-513a-9b4c-7dbc78200519/vcm_s_kf_repr_832x624.jpg",
+            "5babfef3-ae5a-4e01-b166-e7fba4c0d735",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/daa1d99c-9ef5-5758-bb69-875e8b530eb7/vcm_s_kf_repr_886x587.jpg",
+            "d455f10c-80ff-4342-b6f8-74c322c073c3",
+        ),
+        (
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/19951e8d-2921-566b-b23c-dd08ddfb25de/vcm_s_kf_repr_960x540.jpg",
+            "e5b33360-b44a-4815-8f81-17b3f3760d5b",
+        ),
+    ],
+    [
+        "1365323374.0",
+        "1365222901.0",
+        "1315180357.0",
+        "1291580101.0",
+        "1312911544.0",
+        "1314075205.0",
+        "1365223146.0",
+        "1345132415.0",
+    ],
+    [
+        (
+            "(32.9687, -117.184196)",
+            "madhekar residence at carmel vally san diego, california",
+        ),
+        (
+            "(32.9687, -117.184196)",
+            "madhekar residence at carmel vally san diego, california",
+        ),
+        (
+            "(47.610378000000004, -122.200676)",
+            "FedEx, Main Street, Bellevue, King County, Washington, 98004, United States",
+        ),
+        (
+            "(32.941483, -117.233592)",
+            "12327, Caminito Mira del Mar, San Diego, San Diego County, California, 92130, United States",
+        ),
+        ("(36.887856, -118.555145)", "Fresno County, California, United States"),
+        (
+            "(32.941483, -117.233592)",
+            "12327, Caminito Mira del Mar, San Diego, San Diego County, California, 92130, United States",
+        ),
+        (
+            "(32.9687, -117.184196)",
+            "madhekar residence at carmel vally san diego, california",
+        ),
+        (
+            "(32.9687, -117.184196)",
+            "madhekar residence at carmel vally san diego, california",
+        ),
+    ],
+    [
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/5442fe69-599e-5cde-a409-bc46145701bc/vcm_s_kf_repr_886x587.jpg",
+            "",
+            "sad",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/319dbc8e-2c74-58cd-8f7a-43c1e7447c1d/vcm_s_kf_repr_886x587.jpg",
+            "",
+            "sad",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/49ba0a90-d4f1-5420-9bcc-1eaa231656f2/vcm_s_kf_repr_832x624.jpg",
+            "esha",
+            "happy",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/916cc21d-6dc2-5369-b994-ae97e411b377/vcm_s_kf_repr_832x624.jpg",
+            "esha, anjali, and a person",
+            "angry",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/56a82a18-dd40-5ff5-90e8-9122a46a3f94/vcm_s_kf_repr_832x624.jpg",
+            "esha, and a person",
+            "sad",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/07ef46d5-4941-513a-9b4c-7dbc78200519/vcm_s_kf_repr_832x624.jpg",
+            "esha",
+            "happy",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/daa1d99c-9ef5-5758-bb69-875e8b530eb7/vcm_s_kf_repr_886x587.jpg",
+            "",
+            "sad",
+        ],
+        [
+            "/home/madhekar/work/home-media-app/data/input-data-1/img/AnjaliBackup/19951e8d-2921-566b-b23c-dd08ddfb25de/vcm_s_kf_repr_960x540.jpg",
+            "",
+            "angry",
+        ],
+    ],
+]
+
+print(ll)
+
+res = [list(x) for x in zip(*ll)]
+
+print(res)
