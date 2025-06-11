d3b03886beaf530522c28e56cf24495d1834d4f5
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-06-11 12:21:51 -0700

    tool

---
+++ research/code/test/geoclip/get_lat_log.py
@@ -0,0 +1,12 @@
+from geopy.geocoders import Nominatim
+import sys
+
+def getll(address):
+   loc = Nominatim(user_agent='zlatlon')
+   getLoc = loc.geocode(address)
+   return getLoc.latitude,getLoc.longitude
+
+if __name__=='__main__':
+   if len(sys.argv) > 1:
+      address = sys.argv[1:]
+      getll(address)
