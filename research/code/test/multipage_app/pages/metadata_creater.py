0f19d04e577797937cebef3537007ed03a9f9784
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-06-16 14:24:23 -0700

    metadata code cleanup

--- research/code/test/multipage_app/pages/metadata_creater.py
+++ research/code/test/multipage_app/pages/metadata_creater.py
@@ -17,22 +17,24 @@ user_source_selected = st.sidebar.selectbox(
     )
 st.subheader("METADATA: GENERATE", divider="gray")
 
-c1,c2 = st.columns([.1,.9], gap="medium", vertical_alignment="top")
-with c1:
-   c = st.container(border=True)
-   btn_face = c.button(label='Refresh: Face Model')
-   c1_status = c.status('refresh people detection model', state='running', expanded=True)
-   with c1_status:
-      if btn_face:
-          c1_status.info("starting to create face model.")
-          st.info('step: - 1: train know faces for search...')
-          bft_train.exec()
-          c1_status.update(label="face detection model complete!", state="complete", expanded=False) 
-with c2: 
-   c_ = st.container(border=True)
-   btn_metatdata = c_.button(label='Metadata Generate')
+# c1,c2 = st.columns([.1,.9], gap="medium", vertical_alignment="top")
+# with c1:
+#    c = st.container(border=True)
+#    btn_face = c.button(label='Refresh: Face Model')
+#    c1_status = c.status('refresh people detection model', state='running', expanded=True)
+#    with c1_status:
+#       if btn_face:
+#           c1_status.info("starting to create face model.")
+#           st.info('step: - 1: train know faces for search...')
+#           bft_train.exec()
+#           c1_status.update(label="face detection model complete!", state="complete", expanded=False) 
+# with c2: 
+#   c_ = st.container(border=True)
+status = st.status('refresh people detection model', state='running', expanded=True)
+btn_metatdata = st.button(label='Metadata Generate')
+with status:
    if btn_metatdata:
-          st.info('step 2: create metadata for search such as annotations location, text, person names etc...')
-          pp.execute(user_source_selected)
+        st.info('step 2: create metadata for search such as annotations location, text, person names etc...')
+        pp.execute(user_source_selected)
 
    
