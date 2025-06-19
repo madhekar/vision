222a2ccd681748f18d8cb9e7040f57031cb9e5e9
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-06-19 14:37:08 -0700

    issues

--- research/code/test/ollama/llava.py
+++ research/code/test/ollama/llava.py
@@ -1,6 +1,10 @@
+'''
+https://github.com/InternLM/xtuner/tree/main/examples/huggingface_trainer
+'''
+
 from llama_index.llms.ollama import Ollama
 
-llm = Ollama(model='llava-llama3', request_timeout=300.0)
+llm = Ollama(model='llava-phi3', request_timeout=300.0)
 
 res = llm.chat(  messages = [{'role':'user', 'content': 'describe the image','images':['/Users/emadhekar/Downloads/chandrakant9.png']}],)
 
