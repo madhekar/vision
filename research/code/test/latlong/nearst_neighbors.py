76c76b7cfb31119efe49e3e3d1ebc82055a55a8e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-06-30 15:27:24 -0700

    poc lat lon centroids

--- research/code/test/latlong/nearst_neighbors.py
+++ research/code/test/latlong/nearst_neighbors.py
@@ -1,22 +1,45 @@
 from sklearn.neighbors import NearestNeighbors
+from math import cos, asin, sqrt
 import numpy as np
+import pandas as pd
 
 # Your custom distance metric (e.g., Manhattan distance)
 def manhattan_distance(x, y):
   return np.sum(np.abs(x - y))
 
-# Example data
-X = np.array([[1, 2], [3, 4], [5, 6]])
+"""
+def distance(lat1, lon1, lat2, lon2):
+    p = 0.017453292519943295
+    hav = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
+    return 12742 * asin(sqrt(hav))
+"""
 
-# Initialize NearestNeighbors with your custom metric
-nbrs = NearestNeighbors(n_neighbors=2, metric=manhattan_distance)
+def haversine_distance(lat_lon1, lat_lon2):
+    p = 0.017453292519943295
+    hav = (
+        0.5
+        - cos((lat_lon2[0] - lat_lon1[0]) * p) / 2
+        + cos(lat_lon1[0] * p) * cos(lat_lon2[0] * p) * (1 - cos((lat_lon2[1] - lat_lon1[1]) * p)) / 2
+    )
+    return 12742 * asin(sqrt(hav))
 
-# Fit the model
-nbrs.fit(X)
 
-# Find nearest neighbors for a new point
-new_point = np.array([[2, 3]])
-distances, indices = nbrs.kneighbors(new_point)
+def walk_centroids(X, cen):
 
-print("Distances:", distances)
-print("Indices:", indices)
+  # Initialize NearestNeighbors with your custom metric
+  nbrs = NearestNeighbors(n_neighbors=20, metric=haversine_distance, radius=5.0, leaf_size=30, p=2)
+
+  # Fit the model
+  nbrs.fit(X)
+
+  # Find nearest neighbors for a new point
+  #new_point = np.array([[117.28210047 , 33.010097  ]])
+
+  for c in cen:
+     distances, indices = nbrs.kneighbors(c)
+     print("Distances:", distances)
+     print("Indices:", indices)  
+
+
+df = pd.read_csv("lat_lon_nodup.csv")
+X = df.values.tolist()
--- research/code/test/latlong/user_lat_long_agg.py
+++ research/code/test/latlong/user_lat_long_agg.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
+from math import cos, asin, sqrt
 import numpy as np
 
 
@@ -13,7 +14,7 @@ def get_num_of_clusters(X):
 
    for n_clusters in range_n_clusters:
         # Initialize KMeans with the current number of clusters
-        kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
+        kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init='auto')
         cluster_labels = kmeans.fit_predict(X)
 
         # Calculate the silhouette score
@@ -26,7 +27,7 @@ def get_cluster_centers(X, num_clusters):
 
     # Apply K-Means clustering
     # Initialize KMeans with the desired number of clusters (k=3 in this case)
-    kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=10) # n_init for robustness
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init='auto') # n_init for robustness
     kmeans.fit(X)
 
     # Get the cluster labels for each data point
@@ -37,11 +38,46 @@ def get_cluster_centers(X, num_clusters):
 
     return labels, centroids
 
+def haversine_distance(lat_lon1, lat_lon2):
+    p = 0.017453292519943295
+    hav = (
+        0.5
+        - cos((lat_lon2[0] - lat_lon1[0]) * p) / 2
+        + cos(lat_lon1[0] * p)
+        * cos(lat_lon2[0] * p)
+        * (1 - cos((lat_lon2[1] - lat_lon1[1]) * p))
+        / 2
+    )
+    return 12742 * asin(sqrt(hav))
+
+
+def walk_centroids(X, cen):
+    ret = {}
+
+    # Initialize NearestNeighbors with your custom metric
+    nbrs = NearestNeighbors( n_neighbors=20, metric=haversine_distance, radius=5.0, leaf_size=30, p=2)
+
+    # Fit the model
+    nbrs.fit(X)
+
+    # Find nearest neighbors for a new point
+    # new_point = np.array([[117.28210047 , 33.010097  ]])
+
+    for c in cen:
+        print(c.tolist())
+        distances, indices = nbrs.kneighbors([c.tolist()])
+        ret[tuple(c.tolist())] = (indices)
+    return ret
+
+
 if __name__=='__main__':
     df = pd.read_csv('lat_lon_nodup.csv')
     X = df.values.tolist()
     n_clusters = get_num_of_clusters(X)
 
     labels, centroids = get_cluster_centers(X, n_clusters + 2)
-
     print(centroids)
+    
+    ret = walk_centroids(X, centroids)
+
+    print(ret)
