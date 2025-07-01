172191a5e505da6ad88c56a5754c95fd4e2a0eea
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-01 10:37:33 -0700

    haversine dist

--- research/code/test/latlong/haversine.py
+++ research/code/test/latlong/haversine.py
@@ -1,9 +1,25 @@
-from math import cos, asin, sqrt
+from math import cos, asin, sqrt, radians, sin
 
 def distance(lat1, lon1, lat2, lon2):
     p = 0.017453292519943295
     hav = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
     return 12742 * asin(sqrt(hav))
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 3956  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 def closest(data, v):
     return min(data, key=lambda p: distance(v['lat'],v['lon'],p['lat'],p['lon']))
@@ -14,3 +30,7 @@ tempDataList = [{'lat': 39.7612992, 'lon': -86.1519681},
 
 v = {'lat': 39.7622290, 'lon': -86.1519750}
 print(closest(tempDataList, v))
+
+print(distance( 39.7622292, -86.1578917, 39.7612992, -86.1519681))
+
+print(haversine(39.7622292, -86.1578917, 39.7612992, -86.1519681))
--- research/code/test/latlong/user_lat_long_agg.py
+++ research/code/test/latlong/user_lat_long_agg.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.neighbors import NearestNeighbors
-from math import cos, asin, sqrt
+from math import cos, asin, sqrt, radians, sin
 import numpy as np
 
 def get_num_of_clusters(X):
@@ -37,6 +37,25 @@ def get_cluster_centers(X, num_clusters):
 
     return labels, centroids
 
+"""
+The "radius" parameter in NearestNeighbors algorithms, such as those found in scikit-learn, represents a
+distance threshold. The units of this radius are dependent on the distance metric used. 
+Essentially, the radius defines a "neighborhood" around each data point, and the algorithm identifies all other data points within that neighborhood. 
+Here's how the radius unit is determined:
+
+Inherited from the Distance Metric: The units of the radius are the same as the units of the chosen distance metric. For example, if you're using Euclidean distance and your data represents spatial coordinates in meters, then the radius will be in meters.
+Euclidean Distance: This is the most common distance metric, measuring the straight-line distance between two points in a multi-dimensional space. If your data features are in units of meters, the radius for Euclidean distance will also be in meters.
+Other Metrics: Different distance metrics exist, each with its own interpretation of distance. For instance:
+    
+    Manhattan Distance: Measures distance as the sum of absolute differences between coordinates.
+    
+    Cosine Similarity: Measures the angle between vectors, often used for text analysis.
+    
+    Haversine Distance: Used for calculating distances on a sphere, like the Earth. If you use this, the radius unit would be related to spherical units, 
+    but you'd often convert to familiar units like miles or kilometers by multiplying by the Earth's radius. 
+
+In summary, when using the "radius" parameter in NearestNeighbors, remember that its units are tied to the chosen distance metric and your data's feature units
+"""
 def haversine_distance(lat_lon1, lat_lon2):
     p = 0.017453292519943295
     hav = (
@@ -48,6 +67,23 @@ def haversine_distance(lat_lon1, lat_lon2):
         / 2
     )
     return 12742 * asin(sqrt(hav))
+
+# ---
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 
 def walk_centroids(X, cen, data_vs_clusters):
