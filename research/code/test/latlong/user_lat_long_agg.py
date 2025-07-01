b2be13d33abdab6b90e661d4ba365aef835cf00d
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-01 11:59:04 -0700

    code cleanup

--- research/code/test/latlong/user_lat_long_agg.py
+++ research/code/test/latlong/user_lat_long_agg.py
@@ -69,9 +69,9 @@ def haversine_distance(lat_lon1, lat_lon2):
     return 12742 * asin(sqrt(hav))
 
 # ---
-def haversine(lon1, lat1, lon2, lat2):
+def haversine_dist(lon1, lat1, lon2, lat2):
     """
-    Calculate the great circle distance in kilometers between two points
+    Calculate the great circle distance in miles between two points
     on the earth (specified in decimal degrees)
     """
     # convert decimal degrees to radians
@@ -82,7 +82,7 @@ def haversine(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    r = 3956  # Radius of earth in kilometers. Use 6371 for kilometers. Determines return value units.
     return c * r
 
 
@@ -92,7 +92,7 @@ def walk_centroids(X, cen, data_vs_clusters):
     print(data_vs_clusters)
 
     # Initialize NearestNeighbors with your custom metric
-    nbrs = NearestNeighbors( n_neighbors=data_vs_clusters, metric=haversine_distance, radius=5.0, leaf_size=30, p=2)
+    nbrs = NearestNeighbors( n_neighbors=data_vs_clusters, metric=haversine_distance, radius=10.0, leaf_size=30, p=2)
 
     # Fit the model
     nbrs.fit(X)
@@ -114,6 +114,6 @@ if __name__=='__main__':
     labels, centroids = get_cluster_centers(X, n_clusters + 2)
     print(centroids)
     
-    ret = walk_centroids(X, centroids, len_X// n_clusters)
+    ret = walk_centroids(X, centroids,  10)#len_X// n_clusters)
 
     print(ret)
