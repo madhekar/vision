30e78b700b3218dfaf41fdeb5ed29cc65bb8b6e8
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-02 15:55:12 -0700

    refresh design

--- research/code/test/latlong/color_loop2.py
+++ research/code/test/latlong/color_loop2.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from cycler import cycler
 
 num_colors = 20
-viridis_colors = [plt.cm.viridis(i/num_colors) for i in range(num_colors)]
+#viridis_colors = [plt.cm.viridis(i/num_colors) for i in range(num_colors)]
+colors = [plt.cm.plasma(i/num_colors) for i in range(num_colors)]
 
 x = np.linspace(0, 10, 200)
 fig, ax = plt.subplots()
-ax.set_prop_cycle(cycler('color', viridis_colors))
+ax.set_prop_cycle(cycler('color', colors))
 
 for i in range(num_colors):
     y = np.sin(x + i * np.pi / num_colors)
