e5ce4525790960c0277eb75af941a53f805c3254
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-18 14:36:49 -0700

    parallel processing

--- research/code/test/multipage_app/utils/quality_util/iq.py
+++ research/code/test/multipage_app/utils/quality_util/iq.py
@@ -34,19 +34,7 @@ def getRecursive(rootDir, chunk_size=10):
     for i in range(0, len(f_list), chunk_size):
         yield f_list[i : i + chunk_size]
 
-# def getRecursive(rootDir):
-#     f_list = []
-#     for fn in glob.iglob(rootDir + "/**/*", recursive=True):
-#         if not os.path.isdir(os.path.abspath(fn)):
-#             f_list.append(( str(os.path.abspath(fn)).replace(str(os.path.basename(fn)), ""), os.path.basename(fn)))
-#     return f_list
-
-# class Quality():
-#     def __init__(self, image_path, archive_dir):
-#         self.image_path = image_path
-#         self.archive_dir = archive_dir
-
-async def is_valid_size_and_score(self, img, metric, threshold=6.0):
+async def is_valid_size_and_score(img, metric, threshold=6.0):
       try:  
         if img is not None and os.path.getsize(img) > 512:
             im = Image.open(img).convert("RGB")
@@ -57,63 +45,64 @@ async def is_valid_size_and_score(self, img, metric, threshold=6.0):
             
             im_tensor = transform(im).unsqueeze(0).to(device)
             score = metric(im_tensor)
-            fscore = score.item()
+            f_score = score.item()
 
-            print(f'{img} :: {h}:{w} :: {fscore}')
+            print(f'{img} :: {h}:{w} :: {f_score}')
 
-            return fscore < threshold
+            return f_score < threshold
         else:
             sm.add_messages('quality', 'e| unable to load - NULL / Invalid image')
       except Exception as e:
                 sm.add_messages("quality", f"e| error: {e} occurred while opening the image: {os.path.join(img[0], img[1])}")
-                #os.remove(os.path.join(img[0], img[1]))
-                #continue
       return False    
-            
-async def iq_work_flow()            
+
 """
 Find and Archive quality images
 """
-def find_images_quality(self, iqa_metric, image_quality_threshold):
-
-        file_tuple_list = getRecursive(self.image_path)
-        total_images = len(file_tuple_list)
-        bad_quality_tuple_list = []
-        
+async def find_images_quality(iqa_metric, image_quality_threshold, image):
         sm.add_messages("quality", "s| Identifying Good Quality Images...")
+        if not is_valid_size_and_score(image, iqa_metric, image_quality_threshold):
+                return image
 
-        for im in file_tuple_list:
-            img = os.path.join(im[0], im[1])
-            if not self.is_valid_size_and_score(img, iqa_metric, image_quality_threshold):
-                bad_quality_tuple_list.append(im)
+def archive_images(image_path, archive_path, bad_quality_tuple_list):                
+    if len(bad_quality_tuple_list) != 0:
+        space_saved = 0
+        image_cnt =0 
+        for quality in bad_quality_tuple_list:
+                space_saved += os.path.getsize(os.path.join(quality[0], quality[1]))
+                image_cnt += 1
+                #uuid_path = mu.create_uuid_from_string(quality[0]) 
+                uuid_path = mu.extract_subpath(image_path, quality[0])
+                if not os.path.exists(os.path.join(archive_path, uuid_path)):
+                    os.makedirs(os.path.join(archive_path, uuid_path))
+                os.rename( os.path.join(quality[0], quality[1]), os.path.join(archive_path, uuid_path, quality[1]))
+                print(f"{quality} Moved Successfully!")
+                #sm.add_messages("quality", f"s| file {quality} moved successfully.")
 
-        images_with_quality_issues_count = len(bad_quality_tuple_list)
+        print(f"\n\n saved {round(space_saved / 1000000)} mb of Space, {image_cnt} images archived.")
+        sm.add_messages("quality",f"w| saved {round(space_saved / 1000000)} mb of Space, {image_cnt} images archived.")
+    else:
+        print("No bad quality images Found :)")
+        sm.add_messages("quality", "w| no bad quality images Found.")
 
-        sm.add_messages("quality",f"w| {images_with_quality_issues_count} bad quality images found out off: {total_images}, percentage: {(images_with_quality_issues_count / total_images) * 100}%")
-                
-        if len(bad_quality_tuple_list) != 0:
-            a = input(f"Do you want to move/ archive these {len(bad_quality_tuple_list)} Images? Press Y or N:  ")
-            space_saved = 0
 
-            if a.strip().lower() == "y":
-                for quality in bad_quality_tuple_list:
-                    space_saved += os.path.getsize(os.path.join(quality[0], quality[1]))
-                    #uuid_path = mu.create_uuid_from_string(quality[0]) 
-                    uuid_path = mu.extract_subpath(self.image_path, quality[0])
-                    if not os.path.exists(os.path.join(self.archive_dir, uuid_path)):
-                        os.makedirs(os.path.join(self.archive_dir, uuid_path))
-                    os.rename( os.path.join(quality[0], quality[1]), os.path.join(self.archive_dir, uuid_path, quality[1]))
-                    print(f"{quality} Moved Successfully!")
-                    #sm.add_messages("quality", f"s| file {quality} moved successfully.")
+async def iq_work_flow(image_dir_path, chunk_size):
 
-                print(f"\n\nYou saved {round(space_saved / 1000000)} mb of Space!")
-                sm.add_messages("quality",f"w| saved {round(space_saved / 1000000)} mb of Space.")
-            else:
-                print("Using quality Remover")
-                sm.add_messages("quality", "s| No images are archived")
-        else:
-            print("No bad quality images Found :)")
-            sm.add_messages("quality", "w| no bad quality images Found.")
+    lock = asyncio.Lock()
+    img_iterator = mu.getRecursive(image_dir_path, chunk_size=chunk_size)
+    iqa_metric = create_metric()
+
+    with st.status("Generating LLM responses...", expanded=True) as status:
+        async with Pool(processes=chunk_size,  maxtasksperchild=1) as pool:  #initializer=pool_init, initargs=(bfs,),
+            count = 0
+            res = [] 
+            for ilist in img_iterator:
+                if len(ilist) > 0:
+                     
+            res = await asyncio.gather(
+                        pool.map(generateId, ilist)
+ 
+            )         
 """
     input_image_path,
     archive_quality_path,
@@ -121,23 +110,18 @@ def find_images_quality(self, iqa_metric, image_quality_threshold):
 
 """
 def execute(source_name):
-    (
-        input_image_path,
-        archive_quality_path,
-        image_quality_threshold
-    ) = config.image_quality_config_load()
+
+    (input_image_path, archive_quality_path,image_quality_threshold) = config.image_quality_config_load()
 
     input_image_path_updated = os.path.join(input_image_path,source_name)
     
     arc_folder_name = mu.get_foldername_by_datetime()  
     
     archive_quality_path = os.path.join(archive_quality_path, source_name, arc_folder_name)
-  
-    dr = Quality(input_image_path_updated, archive_quality_path )
 
     iqa_metric = create_metric()
 
-    dr.find_images_quality( iqa_metric, image_quality_threshold)
+    find_images_quality( input_image_path_updated, arc_folder_name, iqa_metric, image_quality_threshold)
 
     ss.remove_empty_files_and_folders(input_image_path_updated)
     
