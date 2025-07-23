8f5231ed104174db2f5ef75b7116f4d8318fefdb
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-23 09:45:43 -0700

    perf tune

--- research/code/test/multipage_app/utils/preprocess_util/preprocess.py
+++ research/code/test/multipage_app/utils/preprocess_util/preprocess.py
@@ -156,6 +156,7 @@ async def run_workflow(
     df,
     image_dir_path,
     chunk_size,
+    queue_size,
     metadata_path,
     metadata_file,
     number_of_instances,
@@ -179,7 +180,7 @@ async def run_workflow(
     img_iterator = mu.getRecursive(image_dir_path, chunk_size=chunk_size)
 
     with st.status("Generating LLM responses...", expanded=True) as status:
-        async with Pool(processes=chunk_size,  maxtasksperchild=1) as pool:  #initializer=pool_init, initargs=(bfs,),
+        async with Pool(processes=chunk_size,  queuecount=queue_size) as pool: #maxtasksperchild=1) as pool:  #initializer=pool_init, initargs=(bfs,),
             count = 0
             res = []
             for ilist in img_iterator:
@@ -259,7 +260,8 @@ def execute(user_source_selected):
     else:
         df_loc = st.session_state.df_loc   
 
-    chunk_size = int(mp.cpu_count() // 4)
+    chunk_size = int(aiomp.cpu_count())
+    queue_size = chunk_size // 4
     st.sidebar.subheader("Metadata Generation")
     st.sidebar.divider()
 
@@ -287,6 +289,7 @@ def execute(user_source_selected):
         df,
         image_dir_path,
         chunk_size,
+        queue_size,
         metadata_path,
         metadata_file,
         number_of_instances,
