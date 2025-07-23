981cfec24e4ae173229025e971b7f8ad15af239b
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-23 15:03:45 -0700

    added BallTree impldded

---
+++ research/code/test/multipage_app/utils/util/ball_tree.py
@@ -0,0 +1,26 @@
+from sklearn.neighbors import BallTree
+import numpy as np
+
+class GeoBallTree():
+    def __init__(self, df):
+        self.df = df
+        self.lpt = None
+        self.lrad = None
+        self.BT = None
+
+    def create_data_structure(self):
+        self.lpt = self.df[['latitude','longitude']].to_numpy()    
+        self.lrad = np.radians(self.lpt)
+        self.BT = BallTree(self.lrad, metric='haversine')
+
+    def query_find_nearest(self, lat, lon):
+       
+       query_pt_radians = np.radians([lat, lon])
+
+       dist, index = self.BT.query(query_pt_radians, k=1)
+
+       nearest_pt_rad = self.lrad(index[0])
+
+       nearest_pt = np.degrees(nearest_pt_rad)
+
+       return nearest_pt, dist
