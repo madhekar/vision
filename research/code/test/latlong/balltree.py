7e5f935829c6f42dbbcceb7cbe1d26792b019dd3
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-24 13:48:20 -0700

    data issues

--- research/code/test/latlong/balltree.py
+++ research/code/test/latlong/balltree.py
@@ -1,15 +1,75 @@
+import os
 from sklearn.neighbors import BallTree
 import numpy as np
-import geopandas as gpd
+import pandas as pd
+import fastparquet as fp
+#import geopandas as gpd
 
-# Sample data (replace with your actual data loading)
-np.random.seed(0)
-num_points = 1000000
-# Example: random points in a 10x10 area
-points = np.random.rand(num_points, 2) * 10
 
-# Convert to radians for BallTree (if using lat/lon)
-points_radians = np.radians(points)
+def read_parquet(p,f):
+    df = None
+    try:
+        pf = fp.ParquetFile(os.path.join(p,f))
+        df = pf.to_pandas()
+        df['name'].str.strip()
+    except Exception as e:
+       print(f"error loading/ reading file: {e}")  
+    return df
 
-# Build the BallTree (using haversine distance for lat/lon)
-tree = BallTree(points_radians, metric='haversine')
+def create_balltree(df):
+    df.dropna()
+    print(df.describe())
+    df.drop(columns=["name", "state", "country"], inplace=True)
+
+    print(df.head(20)) 
+
+    df = df[
+            (df.longitude != "-")
+            & (df.latitude != "-")
+            & (~df.longitude.isnull())
+            & (~df.latitude.isnull())
+        ]  
+
+    print(df.head(20))
+
+    # df_lat = pd.to_numeric(df['latitude'], errors='coerce')
+    # df_lon = pd.to_numeric(df['longitude'], errors='coerce')
+
+    # print(df_lat)
+    # print(df_lon)
+   
+    #df[["latitude", "longitude"]] = df[["latitude", "longitude"]].astype(float)
+
+    #df = df.astype(float)
+
+    cols = ['latitude','longitude']
+    for col in cols:
+        df[col] = pd.to_numeric(df[col] , errors='coerce')
+
+    print(df.dtypes)
+    print(df.head(20))
+
+def sample_ball_tree():
+    # Sample data (replace with your actual data loading)
+    np.random.seed(0)
+    num_points = 1000000
+    # Example: random points in a 10x10 area
+    points = np.random.rand(num_points, 2) * 10
+
+    print(points)
+
+    # Convert to radians for BallTree (if using lat/lon)
+    points_radians = np.radians(points)
+
+    print(points_radians)
+
+    # Build the BallTree (using haversine distance for lat/lon)
+    tree = BallTree(points_radians, metric='haversine')
+
+if __name__=='__main__':
+    parquet_path = '/home/madhekar/work/home-media-app/data/app-data/static-metadata/locations/user-specific/Madhekar'
+    parquet_file = 'static_locations.parquet'
+
+    dff = read_parquet(parquet_path, parquet_file)
+
+    create_balltree(dff)
