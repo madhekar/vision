b112f7922f941292bbdd7d302f2c8d8ddc5333dc
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-31 14:37:06 -0700

    bug in os.path.join

--- research/code/test/folder_structure_lineage/app_structure_paths/create_app_paths.py
+++ research/code/test/folder_structure_lineage/app_structure_paths/create_app_paths.py
@@ -1,6 +1,6 @@
 """
 /home-media-app/app/main
-/home-media-app/app-data/metadata/
+/home-media-app/app-data/metadata
 /home-media-app/app-data/static-metadata/faces
 /home-media-app/app-data/static-metadata/locations/default
 /home-media-app/app-data/static-metadata/locations/user-specific
@@ -16,13 +16,28 @@
 /home-media-app/data/input-data/error/img/duplicate
 /home-media-app/data/input-data/error/img/missing-data
 /home-media-app/data/input-data/error/img/quality
-/home-media-app/data/raw-data/
-/home-media-app/data/train-data/img/
-/home-media-app/data/train-data/txt/
-/home-media-app/data/train-data/video/
-/home-media-app/data/train-data/audio/
-/home-media-app/models/faces_label_enc/
-/home-media-app/models/faces_svc/
-/home-media-app/models/faces_embeddings/
+/home-media-app/data/raw-data
+/home-media-app/data/train-data/img
+/home-media-app/data/train-data/txt
+/home-media-app/data/train-data/video
+/home-media-app/data/train-data/audio
+/home-media-app/models/faces_label_enc
+/home-media-app/models/faces_svc
+/home-media-app/models/faces_embeddings
 
 """
+import os
+import pathlib
+import yaml
+
+with open('app_paths.yaml') as ifile:
+    loded_data = yaml.safe_load(ifile)
+
+    root_folder = loded_data['app-root']
+    app_path_list = loded_data['app-paths']
+
+    for p in app_path_list:
+        ap = os.path.join(root_folder, *p.split('/'))
+        print(ap)
+        pathlib.Path(ap).mkdir(parents= True, exist_ok= True)
+
