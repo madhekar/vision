2cf4b4b4b4faeac3bfd4e18a172f6d2aa4da6436
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-07-30 17:51:00 -0700

    cleanup

--- research/code/test/multipage_app/utils/search_util/search.py
+++ research/code/test/multipage_app/utils/search_util/search.py
@@ -217,7 +217,7 @@ def search_fn(client, cImgs, cTxts):
     with image:
         if st.session_state["timgs"] and len(st.session_state["timgs"]) > 1:
             index = image_select(
-                label= "**Cognatus Images**",
+                label= "Cognatus Images",
                 images=st.session_state["timgs"],
                 use_container_width=True,
                 # captions=st.session_state["meta"],
