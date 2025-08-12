65309ca70bf7c3bbda6a60fc99509c0f395e9f6b
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-08-12 15:12:02 -0700

    cleanup

---
+++ research/code/test/install/run_mapp.py
@@ -0,0 +1,22 @@
+# run_app.py
+'''
+https://github.com/nativefier/nativefier
+'''
+import streamlit.web.cli as stcli
+import sys
+import os
+'''
+sudo find /usr -type f -size +10M -printf '%s %p\n' | sort -nrk 1 | numfmt --to=iec --field=1 | head -n 40
+'''
+def resolve_path(path):
+    resolved_path = os.path.abspath(os.path.join(os.getcwd(), path))
+    return resolved_path
+
+if __name__ == "__main__":
+    sys.argv = [
+        "streamlit",
+        "run",
+        resolve_path("mapp.py"),
+        "--global.developmentMode=false", # Optional: disable development mode
+    ]
+    sys.exit(stcli.main())
