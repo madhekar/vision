bb3ce8f70f188ff55ff90f6ad2a0ea29490258f3
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-08-22 10:09:54 -0700

    poc pool of exif

---
+++ research/code/test/latlong/exif/exit_pool.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+import json
+
+class ExifTool(object):
+
+    sentinel = "{ready}\n"
+
+    def __init__(self, executable="/usr/bin/exiftool"):
+        self.executable = executable
+        self.process = None
+
+    def __enter__(self):
+        self.process = subprocess.Popen(
+            [self.executable, "-stay_open", "True",  "-@", "-"],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        return self
+
+    def  __exit__(self, exc_type, exc_value, traceback):
+        self.process.stdin.write("-stay_open\nFalse\n")
+        self.process.stdin.flush()
+
+    def execute(self, *args):
+        args = args + ("-execute\n",)
+        self.process.stdin.write(str.join("\n", args))
+        self.process.stdin.flush()
+        output = ""
+        fd = self.process.stdout.fileno()
+        while not output.endswith(self.sentinel):
+            output += os.read(fd, 4096)
+        return output[:-len(self.sentinel)]
+
+    def get_metadata(self, *filenames):
+        return json.loads(self.execute("-G", "-j", "-n", *filenames))
+
+
+if __name__=='__main__':
+    et = ExifTool()
+    root = '/Users/emadhekar/Documents/images'
+    l_images =[os.path.join(root, img) for img in os.listdir(root)]
+    print(l_images)
+    mdata = et.get_metadata(l_images)
+
+    print(mdata)
