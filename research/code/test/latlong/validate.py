fea33232a648d0019f97c175516f2e44caa43d76
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-08-25 15:19:03 -0700

    validation for data static

---
+++ research/code/test/latlong/validate.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def validate_static_metadata_location(loc_file):
+
+   df = pd.read_csv(loc_file) 
+
+   df.columns=['name', 'state', 'country','latitude', 'longitude']
+
+   print(df.head(80))
+
+#    print(f'find null values per column: {df.isnull()}')
+
+#    print(df['name'].isnull().sum())
+
+#    print(df.dtypes)
+
+   df['latitude'] = df['latitude'].astype(float)
+   df["longitude"] = df["longitude"].astype(float)
+
+   print(df.isnull().sum(axis=1))
+
+   print(df[df.isnull().any(axis=1)])
+
+
+if __name__=='__main__':
+   #validate_static_metadata_location('/home/madhekar/work/vision/research/code/test/zm/schema/base_data/locations/in-cities.csv')   
+   validate_static_metadata_location('/home/madhekar/work/vision/research/code/test/zm/schema/base_data/locations/us-nationalparks.csv') 
+   #validate_static_metadata_location('/home/madhekar/work/home-media-app/data/app-data/static-metadata/locations/user-specific/madhekar/user-specific.csv')
