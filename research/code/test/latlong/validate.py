43d9c06ecbcf743c2e4da5f6017bed68456e24f9
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-08-25 16:32:30 -0700

    static metadata validation

--- research/code/test/latlong/validate.py
+++ research/code/test/latlong/validate.py
@@ -1,28 +1,30 @@
 import pandas as pd
 
 def validate_static_metadata_location(loc_file):
+    df = pd.read_csv(loc_file)
 
-   df = pd.read_csv(loc_file) 
+    df.columns = ["name", "state", "country", "latitude", "longitude"]
 
-   df.columns=['name', 'state', 'country','latitude', 'longitude']
+    # print(df.head(80))
 
-   print(df.head(80))
+    #    print(f'find null values per column: {df.isnull()}')
 
-#    print(f'find null values per column: {df.isnull()}')
+    #    print(df['name'].isnull().sum())
 
-#    print(df['name'].isnull().sum())
+    #    print(df.dtypes)
 
-#    print(df.dtypes)
+    #df["latitude"] = df["latitude"].astype(float)
+    df["longitude"] = df["longitude"].astype(float)
 
-   df['latitude'] = df['latitude'].astype(float)
-   df["longitude"] = df["longitude"].astype(float)
+    df["latitude"] = pd.to_numeric(df["latitude"], errors='coerce')
+    df['latitude'].to_csv('out1.csv')
+    # print(df.isnull().sum(axis=1))
 
-   print(df.isnull().sum(axis=1))
+    print(df[df.isnull().any(axis=1)])
 
-   print(df[df.isnull().any(axis=1)])
-
+    #print(df[df["latitude"]])
 
 if __name__=='__main__':
    #validate_static_metadata_location('/home/madhekar/work/vision/research/code/test/zm/schema/base_data/locations/in-cities.csv')   
-   validate_static_metadata_location('/home/madhekar/work/vision/research/code/test/zm/schema/base_data/locations/us-nationalparks.csv') 
-   #validate_static_metadata_location('/home/madhekar/work/home-media-app/data/app-data/static-metadata/locations/user-specific/madhekar/user-specific.csv')
+   #validate_static_metadata_location('/home/madhekar/work/vision/research/code/test/zm/schema/base_data/locations/us-nationalparks.csv') 
+   validate_static_metadata_location('/home/madhekar/work/home-media-app/data/app-data/static-metadata/locations/user-specific/madhekar/user-specific.csv')
