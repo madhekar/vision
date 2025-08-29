4fde4fe31ba12e516951b0f976d6fbb58b5c8ed2
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-08-29 12:11:26 -0700

    image dedup poc

--- research/code/test/duplicates/dup.py
+++ research/code/test/duplicates/dup.py
@@ -1,5 +1,13 @@
 import hashlib
+from PIL import Image
+import imagehash
 import os
+
+def calculate_pHash(filepath):
+    image_path = filepath
+    image = Image.open(image_path)
+    ph = imagehash.phash(image)
+    return ph
 
 def calculate_md5(filepath):
     with open(filepath, "rb") as f:
@@ -11,4 +19,5 @@ def calculate_md5(filepath):
 # Example usage:
 image_path = "/Users/emadhekar/Downloads/1909ab0d-8449-47a4-848c-4a5d7c4832ad.JPG"
 md5_hash = calculate_md5(image_path)
-print(f"MD5 hash of {image_path}: {md5_hash}")
+p_hash = calculate_pHash(image_path)
+print(f"MD5 and pHash hash of {image_path}: {md5_hash} {p_hash}")
