854be9885113202c3bc6a4c67781bd1e97988f8b
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-02 17:31:16 -0700

    POC

--- research/code/test/duplicates/KDdup.py
+++ research/code/test/duplicates/KDdup.py
@@ -3,20 +3,7 @@ import ImgToHash as ah
 import find
 import random
 from helper import build_tree, save_results
-"""
-$ deduplication search \
---images-path datasets/potatoes \
---output-path outputs \
---tree-type KDTree \
---threshold 40 \
---parallel f \
---nearest-neighbors 5 \
---hash-algorithm phash \
---hash-size 8 \
---distance-metric manhattan \
---query datasets/potatoes/2018-12-11-15-031193.png
 
-"""
 # images_path = '/home/madhekar/work/home-media-app/data/input-data/img/madhekar'#'/home/madhekar/work/home-media-app/data/input-data/img/madhekar/cc2af672-0277-5d32-ad7d-2dfac3662e7b'#'/home/madhekar/work/home-media-app/data/input-data/img/madhekar/2596441a-e02f-588c-8df4-dc66a133fc99'#'/home/madhekar/temp/faces'
 # output_path = './result'
 # hash_algo = 'phash'
@@ -40,9 +27,6 @@ $ deduplication search \
 
 # print(x)
 
-
-'''----'''
-
 """ 
 img_file_list='/home/madhekar/work/home-media-app/data/input-data/img/madhekar'
 output_path='./result'
@@ -61,11 +45,8 @@ safe_deletion='n'
 image_h = 512
 image_w = 512 
 """
-
-
 def remove_duplicates(img_file_list, output_path, hash_size=8, tree_type='KDTree', distance_metric='manhattan', nearest_neighbors=5,
-           leaf_size=15, hash_algo='phash', parallel='y', batch_size=64, threshold=5, backup_keep='y', backup_duplicate='y', safe_deletion='n', image_w=512,
-           image_h=512):
+           leaf_size=15, hash_algo='phash', parallel='y', batch_size=64, threshold=5, backup_keep='y', backup_duplicate='y', safe_deletion='n', image_w=512, image_h=512):
     
     # Build the tree
     df_dataset, _ = ah.ImageToHash(img_file_list, hash_size=hash_size, hash_algo=hash_algo).build_dataset(parallel=parallel, batch_size=batch_size)
@@ -81,16 +62,18 @@ def remove_duplicates(img_file_list, output_path, hash_size=8, tree_type='KDTree
     # Show a duplicate
     if len(dict_image_to_duplicates) > 0:
         random_img = random.choice(list(dict_image_to_duplicates.keys()))
-        near_duplicate_image_finder.show_an_image_duplicates(dict_image_to_duplicates, random_img, output_path,
-                                                                image_w=image_w,
-                                                                image_h=image_h)
+        near_duplicate_image_finder.show_an_image_duplicates(dict_image_to_duplicates, random_img, output_path, image_w=image_w, image_h=image_h)
+
     # Save results
     save_results(to_keep, to_remove, hash_size, threshold, output_path, backup_keep, backup_duplicate, safe_deletion)
 
+    return to_keep, to_remove
     print(to_keep, to_remove)
 
 
 if __name__=='__main__':
     img_file_list='/home/madhekar/work/home-media-app/data/input-data/img/madhekar'
     output_path='./result'
-    remove_duplicates(img_file_list, output_path)    
+    to_keep, to_remove = remove_duplicates(img_file_list, output_path)    
+
+    print(to_remove)
