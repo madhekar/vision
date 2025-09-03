6ceaaeec8c07ff8a9f896b66438546e703330a92
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-02 17:13:49 -0700

    poc fix

--- research/code/test/duplicates/KDdup.py
+++ research/code/test/duplicates/KDdup.py
@@ -43,6 +43,7 @@ $ deduplication search \
 
 '''----'''
 
+""" 
 img_file_list='/home/madhekar/work/home-media-app/data/input-data/img/madhekar'
 output_path='./result'
 hash_size=8
@@ -58,29 +59,38 @@ backup_keep='y'
 backup_duplicate='y' 
 safe_deletion='n'
 image_h = 512
-image_w = 512
+image_w = 512 
+"""
 
 
-# def delete(df_dataset, img_file_list, output_path, hash_size, tree_type, distance_metric, nearest_neighbors,
-#            leaf_size, parallel, batch_size, threshold, backup_keep, backup_duplicate, safe_deletion, image_w=128,
-#            image_h=128):
+def remove_duplicates(img_file_list, output_path, hash_size=8, tree_type='KDTree', distance_metric='manhattan', nearest_neighbors=5,
+           leaf_size=15, hash_algo='phash', parallel='y', batch_size=64, threshold=5, backup_keep='y', backup_duplicate='y', safe_deletion='n', image_w=512,
+           image_h=512):
+    
     # Build the tree
-df_dataset, _ = ah.ImageToHash(img_file_list, hash_size=hash_size, hash_algo=hash_algo).build_dataset(parallel=parallel, batch_size=batch_size)
-near_duplicate_image_finder = build_tree(df_dataset, tree_type, distance_metric, leaf_size, parallel, batch_size)
+    df_dataset, _ = ah.ImageToHash(img_file_list, hash_size=hash_size, hash_algo=hash_algo).build_dataset(parallel=parallel, batch_size=batch_size)
+    near_duplicate_image_finder = build_tree(df_dataset, tree_type, distance_metric, leaf_size, parallel, batch_size)
 
-# Find duplicates
-to_keep, to_remove, dict_image_to_duplicates = near_duplicate_image_finder.find_all_near_duplicates(
-    nearest_neighbors,
-    threshold)
+    # Find duplicates
+    to_keep, to_remove, dict_image_to_duplicates = near_duplicate_image_finder.find_all_near_duplicates(
+        nearest_neighbors,
+        threshold)
 
-print('We have found {0}/{1} duplicates in folder'.format(len(to_remove), len(img_file_list)))
+    print('We have found {0}/{1} duplicates in folder'.format(len(to_remove), len(img_file_list)))
 
-# Show a duplicate
-if len(dict_image_to_duplicates) > 0:
-    random_img = random.choice(list(dict_image_to_duplicates.keys()))
-    near_duplicate_image_finder.show_an_image_duplicates(dict_image_to_duplicates, random_img, output_path,
-                                                            image_w=image_w,
-                                                            image_h=image_h)
-# Save results
-save_results(to_keep, to_remove, hash_size, threshold, output_path, backup_keep, backup_duplicate, safe_deletion)
-print(to_keep, to_remove)
+    # Show a duplicate
+    if len(dict_image_to_duplicates) > 0:
+        random_img = random.choice(list(dict_image_to_duplicates.keys()))
+        near_duplicate_image_finder.show_an_image_duplicates(dict_image_to_duplicates, random_img, output_path,
+                                                                image_w=image_w,
+                                                                image_h=image_h)
+    # Save results
+    save_results(to_keep, to_remove, hash_size, threshold, output_path, backup_keep, backup_duplicate, safe_deletion)
+
+    print(to_keep, to_remove)
+
+
+if __name__=='__main__':
+    img_file_list='/home/madhekar/work/home-media-app/data/input-data/img/madhekar'
+    output_path='./result'
+    remove_duplicates(img_file_list, output_path)    
