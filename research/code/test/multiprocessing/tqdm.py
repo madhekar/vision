9d8a0a78fc9b0e1a05e42345afbc30e0a015ca38
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-04 16:25:22 -0700

    tqdm ux

---
+++ research/code/test/multiprocessing/tqdm.py
@@ -0,0 +1,20 @@
+import time
+from multiprocessing import Pool
+from random import randint
+
+from tqdm import tqdm
+
+
+def _foo(my_number):
+    square = my_number * my_number
+    time.sleep(randint(1, 2) / 2)
+    return square
+
+
+if __name__ == "__main__":
+    max_ = 30
+    with Pool(processes=2) as p, tqdm(total=max_) as pbar:
+        for result in p.imap(_foo, range(0, max_)):
+            pbar.update()
+            pbar.refresh()
+            # do something with `result`
