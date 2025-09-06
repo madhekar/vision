8976f7be6b38b4f9a5369b098cc51e6ca36b315e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-05 17:02:35 -0700

    geohashing

--- research/code/test/latlong/geohash_sample.py
+++ research/code/test/latlong/geohash_sample.py
@@ -1,11 +1,11 @@
 
-import geohash
+from geohash import encode, decode
 
 lat =51.502278
 lon = -0.141947
 
-geohash_cd = geohash.encode(lat, lon, precision=12)
+geohash_cd = encode(lat, lon, precision=12)
 
 print(f'geohash code for lat =51.502278, lon = -0.141947 {geohash_cd}')
 
-print(f' decode: {geohash.decode(geohash_cd)}')
+print(f' decode: {decode(geohash_cd)}')
