3f4a5f8d4c66a38aa7945f68bfd19af2f412583e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-09 12:11:31 -0700

    Pool tqdm POC

--- research/code/test/multiprocessing/ex_tqdm.py
+++ research/code/test/multiprocessing/ex_tqdm.py
@@ -7,13 +7,14 @@ from tqdm import tqdm
 
 def _foo(my_number):
     square = my_number * my_number
+    #print('-->', square)
     time.sleep(randint(1, 2) / 2)
     return square
 
 
 if __name__ == "__main__":
     max_ = 30
-    with Pool(processes=2) as p, tqdm(total=max_) as pbar:
+    with Pool(processes=2) as p, tqdm(total=max_, desc='example using tqdm in pool') as pbar:
         for result in p.imap(_foo, range(0, max_)):
             pbar.update()
             pbar.refresh()
