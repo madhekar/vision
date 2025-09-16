7c84d5aebedf20eca1c75b2a7373cb3ea9ffa4d8
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-16 11:31:38 -0700

    ux colors

--- research/code/test/zm/utils/overview_util/overview.py
+++ research/code/test/zm/utils/overview_util/overview.py
@@ -5,7 +5,8 @@ from utils.config_util import config
 from utils.util import model_util as mu
 
 # https://www.color-hex.com/color-palette/164
-colors = ["#ae5a41", "#1b85b8"]#["#636B2F","#BAC095"] #["#9EB8A0", "#58855c"]#['#58855c','#0D3311']#["#BAC095", "#636B2F"]
+colors = ['#847577','#cfd2cd']#['#f07162','#0081a7']#['#f97171','#8ad6cc']
+#["#ae5a41", "#1b85b8"]#["#636B2F","#BAC095"] #["#9EB8A0", "#58855c"]#['#58855c','#0D3311']#["#BAC095", "#636B2F"]
 
 def extract_folder_paths():
     raw_data_path, input_data_path, app_data_path, final_data_path = (
