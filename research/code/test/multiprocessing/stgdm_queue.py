59a501acd4c2b9a055f9239e60de174b10ba19f5
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-16 10:49:39 -0700

    stqdm with queue

---
+++ research/code/test/multiprocessing/stgdm_queue.py
@@ -0,0 +1,33 @@
+def worker(file_path, session_target_path, queue_obj):
+    result = func(target=file_path)
+    queue_obj.put(result, timeout=60)
+
+def listener(queue_obj, n_files):
+    model = load_model() # tf model
+    with stqdm(desc='text', total=n_files) as progress:
+        while True:
+            try:
+                message = queue_obj.get(timeout=20)
+
+            except Empty:
+                break
+
+            score = make_inference()
+            progress.update()
+
+            print(score)
+
+def multi_process_directory(file_paths):
+    manager = mp.Manager()
+    queue = manager.Queue()    
+    pool = mp.Pool(mp.cpu_count())
+    
+    # Now using a single listener
+    pool.apply_async(listener, (queue, len(file_paths)))
+
+    func = partial(worker, session_target_path, queue)
+    for _ in stqdm(pool.imap(func, file_paths), total=len(file_paths)):
+        pass
+
+    pool.close()
+    pool.join()
