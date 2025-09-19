c7a91914cbcb3a49d8d59cd4b5d7e12bcd702319
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-19 14:57:51 -0700

    reformat

--- research/code/test/general/build_json.py
+++ research/code/test/general/build_json.py
@@ -7,7 +7,6 @@ qa_metadata_file = "face-detection-qa.json"
 
 def build_json(img_root):
    files = os.listdir(img_root)
-   print(files)
    test_data = [{"img": os.path.join(img_root, img), "text": ""} for img in files]
    with open(os.path.join(qa_metadata_path, qa_metadata_file), "w") as f:
       json.dump(test_data, f, indent=2)
