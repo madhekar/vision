e0bc44677ced5924d99e4d460634ec0875639213
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-29 15:27:36 -0700

    study output structure

---
+++ research/code/test/InsightFace/structure.py
@@ -0,0 +1,142 @@
+[
+    [
+        [
+            'Face at coordinates (125, 327) is of "Kumar", a "man" expressing "neutral" emotion. And  one  man  in the image.'
+        ]
+    ],
+    [
+        [
+            "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/IMG_3141.JPG",
+            "abfb3968-b7fe-4da0-8c64-b481929c20dd",
+        ]
+    ],
+    [["1572728241.0"]],
+    [["(18.325447, 72.9561)", "Usar"]],
+    [
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/IMG_3141.JPG"
+    ],
+]
+
+
+####
+
+[
+  [
+    ['Face at coordinates (125, 327) is of "Kumar", a "man" expressing "neutral" emotion. And  one  man  in the image.'],
+    ['Face at coordinates (178, 84) is of "Kumar", a "man" expressing "neutral" emotion. Face at coordinates (336, 505) is of "Asha", a "woman" expressing "neutral" emotion. And in the image.'],
+    ['Face at coordinates (252, 140) is of "Esha", a "woman" expressing "happy" emotion. Face at coordinates (804, 243) is of "Kumar", a "man" expressing "sad" emotion. And  one girl  in the image.'],
+    ['Face at coordinates (795, 245) is of "Kumar", a "man" expressing "happy" emotion. And  one  man  in the image.']
+  ], 
+  [
+    ['/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/IMG_3141.JPG', '2b2eec45-9089-4102-866c-bcbccb486853'],
+    ['/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/6c58a231-3369-4b46-867d-39e31985d313-1.jpg', '001056ae-5e0b-49d0-8854-1b09cf910af4'], 
+    ['/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/74eb3d57-326b-43d3-9e46-0e3205d6564e.jpg', 'b4f2e256-e457-4de4-9e8d-6e44c9323be1'], 
+    ['/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/7b0b6775-e5a4-4cad-b0bc-ab1af3bebe72.jpg', '5cfe7c82-a998-4667-b9eb-3d5a2988b70a']
+  ], 
+  [
+    ['1572728241.0'], 
+    ['1758227006.0'], 
+    ['1758226925.0'], 
+    ['1758227046.0']
+  ],
+  [
+    ['(18.325447, 72.9561)', 'Usar'], 
+    ['(19.101053, 74.740677)', 'Ahmednagar'], 
+    ['(18.495983, 73.81435)', 'Sadaphuli Society in Pune'], 
+    ['(18.50989, 73.807182)', 'Kothrud Pune']
+  ], 
+      
+    [
+        '/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/IMG_3141.JPG', 
+        '/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/6c58a231-3369-4b46-867d-39e31985d313-1.jpg',
+        '/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/74eb3d57-326b-43d3-9e46-0e3205d6564e.jpg',
+        '/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/7b0b6775-e5a4-4cad-b0bc-ab1af3bebe72.jpg'
+    ]
+]
+
+####
+
+[
+    [
+        "07d2d553-8ccb-45d7-a386-96740281c14a",
+        "cca7ee0c-f5ab-46bf-b5b1-189ffdfb1c36",
+        "9225bd8e-a950-4638-816a-d4c20beaeb8e",
+        "824be3ad-5503-43a6-bce9-0549f14b9b8e",
+    ],
+    ["1572728241.0", "1758227006.0", "1758226925.0", "1758227046.0"],
+    [
+        'Face at coordinates (125, 327) is of "Kumar", a "man" expressing "neutral" emotion. And  one  man  in the image.',
+        'Face at coordinates (178, 84) is of "Kumar", a "man" expressing "neutral" emotion. Face at coordinates (336, 505) is of "Asha", a "woman" expressing "neutral" emotion. And in the image.',
+        'Face at coordinates (252, 140) is of "Esha", a "woman" expressing "happy" emotion. Face at coordinates (804, 243) is of "Kumar", a "man" expressing "sad" emotion. And  one girl  in the image.',
+        'Face at coordinates (795, 245) is of "Kumar", a "man" expressing "happy" emotion. And  one  man  in the image.',
+    ],
+    [
+        ("(18.325447, 72.9561)", "Usar"),
+        ("(19.101053, 74.740677)", "Ahmednagar"),
+        ("(18.495983, 73.81435)", "Sadaphuli Society in Pune"),
+        ("(18.50989, 73.807182)", "Kothrud Pune"),
+    ],
+    [
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/IMG_3141.JPG",
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/6c58a231-3369-4b46-867d-39e31985d313-1.jpg",
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/74eb3d57-326b-43d3-9e46-0e3205d6564e.jpg",
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/7b0b6775-e5a4-4cad-b0bc-ab1af3bebe72.jpg",
+    ],
+]
+
+#+-+-#
+
+[
+    [
+        "6d73edce-3b49-4041-9e8e-d3f62029c0ea",
+        "1572728241.0",
+        'Face at coordinates (125, 327) is of "Kumar", a "man" expressing "neutral" emotion. And  one  man  in the image.',
+        ("(18.325447, 72.9561)", "Usar"),
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/IMG_3141.JPG",
+    ],
+    [
+        "9a77c387-f376-43c3-b4ac-04c69c51e401",
+        "1758227006.0",
+        'Face at coordinates (178, 84) is of "Kumar", a "man" expressing "neutral" emotion. Face at coordinates (336, 505) is of "Asha", a "woman" expressing "neutral" emotion. And in the image.',
+        ("(19.101053, 74.740677)", "Ahmednagar"),
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/6c58a231-3369-4b46-867d-39e31985d313-1.jpg",
+    ],
+    [
+        "07fa38d0-bddc-478d-b06d-2f33e7b28396",
+        "1758226925.0",
+        'Face at coordinates (252, 140) is of "Esha", a "woman" expressing "happy" emotion. Face at coordinates (804, 243) is of "Kumar", a "man" expressing "sad" emotion. And  one girl  in the image.',
+        ("(18.495983, 73.81435)", "Sadaphuli Society in Pune"),
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/74eb3d57-326b-43d3-9e46-0e3205d6564e.jpg",
+    ],
+    [
+        "f023c1f6-add5-4da3-a719-b9a4b1f6cd03",
+        "1758227046.0",
+        'Face at coordinates (795, 245) is of "Kumar", a "man" expressing "happy" emotion. And  one  man  in the image.',
+        ("(18.50989, 73.807182)", "Kothrud Pune"),
+        "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/7b0b6775-e5a4-4cad-b0bc-ab1af3bebe72.jpg",
+    ],
+]
+
+
+#$$#
+
+{
+    "uri": "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/7e9a4cc3-b380-40ff-a391-8bf596f8cd27.jpg",
+    "id": "4d88ce3e-4d01-4aea-bee9-c760ef2a774d",
+    "ts": "1758226884.0",
+    "latlon": "(18.495983, 73.81435)",
+    "loc": "Sadaphuli Society in Pune",
+    "names": "Kumar, Asha",
+    "attrib": "neutral",
+    "text": "\n    \nThe image features two individuals, Kumar and Asha, standing side by side in front of a curtain. Kumar, positioned on the right, is dressed in a gray shirt and sports a mustache. On the left, Asha is adorned in a blue sari and a gold necklace. Both individuals are captured in a neutral emotional state. The backdrop of the image is a beige curtain, providing a contrasting background to the subjects. The image does not contain any discernible text. The relative positions of the individuals and the curtain suggest a well-composed photograph, with the subjects in focus against the blurred background.",
+}
+
+#UPDATED
+{
+    "uri": "/home/madhekar/work/home-media-app/data/input-data/img/Samsung_USB/b6f657c7-7b7f-5415-82b7-e005846a6ef5/7e9a4cc3-b380-40ff-a391-8bf596f8cd27.jpg",
+    "id": "4d88ce3e-4d01-4aea-bee9-c760ef2a774d",
+    "ts": "1758226884.0",
+    "latlon": "(18.495983, 73.81435)",
+    "loc": "Sadaphuli Society in Pune",
+    "ppt": 'Face at coordinates (252, 140) is of "Esha", a "woman" expressing "happy" emotion. Face at coordinates (804, 243) is of "Kumar", a "man" expressing "sad" emotion. And  one girl  in the image.',
+}
