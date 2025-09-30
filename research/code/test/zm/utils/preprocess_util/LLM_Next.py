1e14e5e95095407d7888001fb4ec0d55810ab908
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-09-30 13:15:02 -0700

    refining LLM response

--- research/code/test/zm/utils/preprocess_util/LLM_Next.py
+++ research/code/test/zm/utils/preprocess_util/LLM_Next.py
@@ -5,7 +5,7 @@ import streamlit as st
 @st.cache_resource(ttl=36000, show_spinner=True)
 def setLLM():
 
-    model_id = "xtuner/llava-phi-3-mini-hf"
+    model_id = "xtuner/llava-phi-3-mini-hf" #"xtuner/llava-llama-3-8b-hf"
     pipe = pipeline("image-to-text", model=model_id, device="cpu")
     return pipe
 
@@ -16,16 +16,15 @@ def fetch_llm_text(imUrl, pipe, question, partial_prompt, location):
     image = Image.open(imUrl).convert("RGB")
     
     prompt = """<|im_start|>system
-    A chat between a curious human and an artificial intelligence assistant. The assistant is an expert in people, emotions and locations, and gives thoughtful, helpful, detailed, and polite answers to the human questions. 
-    The assistant does not hallucinate and gives extreamly very close attention to the details and take time to process information if necessary, please produce plain text response " strictly avoid emojis or lists" in response.
+    Do not hallucinate and gives very close attention to the details and takes time to process information provided, produce plain text response " strictly avoid emojis or lists" in response.
     <|im_end|>
     <|im_start|>user
-    <image>"{question}" It is extremely important that, response MUST include the people name and emotion details "{partial_prompt}" and the location details "{location}" in the response.
+    <image>"{question}" It is extremely important that, response MUST include the people name and emotion details provided "{partial_prompt}" and the location details "{location}" in the response.
     <|im_end|> 
     <|im_start|>assistant
     """.format(question=question, partial_prompt=partial_prompt, location=location)  # , article=st.session_state["document"])
  
-    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 300})
 
     result = outputs[0]["generated_text"].partition("<|im_start|>assistant")[2]
 
