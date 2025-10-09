aae6eb80efbb380fec15cf91fa6518e95de1fc4e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-09 15:17:21 -0700

    failed POC attempts

--- research/code/test/zm/utils/preprocess_util/LLM_Next.py
+++ research/code/test/zm/utils/preprocess_util/LLM_Next.py
@@ -117,17 +117,16 @@ def fetch_llm_text(imUrl, pipe, question, partial_prompt, location):
             question=question, location=location
         )  # , article=st.session_state["document"])
 
-    # outputs = pipe(
-    #     image,
-    #     prompt=prompt,
-    #     repetition_penalty=2.0,
-    #     generate_kwargs={"max_new_tokens": 200},
-    # )
+    outputs = pipe(
+        image,
+        prompt=prompt,
+        generate_kwargs={"max_new_tokens": 200},
+    )
 
     #
-    inputs = processor(prompt, image, return_tensors="pt").to(0, torch.float16)
+    #inputs = processor(prompt, image, return_tensors="pt").to(0, torch.float16)
     #
-    outputs = model.generate(**inputs, max_new_tokens=200 , do_sample=False, repetition_penalty=1.5)
+    #outputs = model.generate(**inputs, max_new_tokens=200 , do_sample=False, repetition_penalty=1.5)
 
     result = outputs[0]["generated_text"].partition("<|im_start|>assistant")[2]
     #rr = repr(result)
