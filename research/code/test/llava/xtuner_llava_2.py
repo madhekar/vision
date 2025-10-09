a2aad614ef55454a630f50fb4daaf45cf6fb8749
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-09 10:13:32 -0700

    cuda issues with torch

--- research/code/test/llava/xtuner_llava_2.py
+++ research/code/test/llava/xtuner_llava_2.py
@@ -6,15 +6,22 @@ from transformers import AutoProcessor, LlavaForConditionalGeneration
 
 model_id = "xtuner/llava-llama-3-8b-transformers"
 
-prompt = ("<|start_header_id|>user<|end_header_id|>\n\n<image>\nWhat are these?<|eot_id|>"
-          "<|start_header_id|>assistant<|end_header_id|>\n\n")
+# torch.cuda.current_device()
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+prompt = (
+    "<|start_header_id|>user<|end_header_id|>\n\n<image>\nDescribe the image with thoughtful insights using additional information provided<|eot_id|>"
+    "<|start_header_id|>assistant<|end_header_id|>\n\n"
+)
 image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 
 model = LlavaForConditionalGeneration.from_pretrained(
     model_id, 
     torch_dtype=torch.float16, 
-    low_cpu_mem_usage=True, 
-).to(0)
+    low_cpu_mem_usage=False
+).to('cpu')
+
+#model.to('cpu')
 
 processor = AutoProcessor.from_pretrained(model_id)
 
@@ -23,7 +30,7 @@ img = "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/2596441a-
 
 raw_image = Image.open(img)
 #raw_image = Image.open(requests.get(image_file, stream=True).raw)
-inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
+inputs = processor(prompt, raw_image, return_tensors='pt').to('cpu', torch.float16)
 
-output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+output = model.generate(**inputs, max_new_tokens=200, repetition_penalty=1.5, temperature=0, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
