a916d849e3291d9eba21ba340b818eb2126a68ab
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-13 12:55:36 -0700

    cleanup

--- research/code/test/zm/tools/classify/img_classsify.py
+++ research/code/test/zm/tools/classify/img_classsify.py
@@ -13,93 +13,99 @@ import os
 try if needed ViT : https://github.com/sanjay-dutta/Computer-Vision-Practice/tree/main/Vit_flower
 '''
 
-# Define directories
-base_path = '/home/madhekar/work/home-media-app/models'
-train_dir = os.path.join(base_path, 'img_classify/training')
-validation_dir = os.path.join(base_path, 'img_classify/validation')
-image_size = (224, 224)
-batch_size = 32
+def load_init_params():
+    # Define directories
+    base_path = '/home/madhekar/work/home-media-app/models'
+    train_dir = os.path.join(base_path, 'img_classify/training')
+    validation_dir = os.path.join(base_path, 'img_classify/validation')
+    image_size = (224, 224)
+    batch_size = 32
 
-# Use ImageDataGenerator to load and augment images
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
+def transfer_learn_train(train_dir, validation_dir, image_size, batch_size):
+    # Use ImageDataGenerator to load and augment images
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
+    validation_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=image_size,
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
 
-validation_generator = validation_datagen.flow_from_directory(
-    validation_dir,
-    target_size=image_size,
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+    validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
 
-# Load the pre-trained MobileNetV2 model (without the top classification layer)
-base_model = MobileNetV2(
-    input_shape=(224, 224, 3),
-    include_top=False,
-    weights='imagenet'
-)
+    # Load the pre-trained MobileNetV2 model (without the top classification layer)
+    base_model = MobileNetV2(
+        input_shape=(224, 224, 3),
+        include_top=False,
+        weights='imagenet'
+    )
 
-# Freeze the base model layers to prevent re-training
-base_model.trainable = False
+    # Freeze the base model layers to prevent re-training
+    base_model.trainable = False
 
-# Add a new classification head
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(128, activation='relu')(x)
-predictions = Dense(3, activation='softmax')(x) # 3 classes: document, scenic, people
+    # Add a new classification head
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(128, activation='relu')(x)
+    predictions = Dense(3, activation='softmax')(x) # 3 classes: document, scenic, people
 
-# Create the final model
-model = Model(inputs=base_model.input, outputs=predictions)
+    # Create the final model
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.0001),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    # Compile the model
+    model.compile(optimizer=Adam(learning_rate=0.0001),
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
 
-# Monitor 'val_loss' and wait for 5 epochs without improvement before stopping
-early_stopping_callback = EarlyStopping(
-    monitor='val_loss',        # Metric to monitor
-    patience=5,                # Number of epochs with no improvement after which training will be stopped
-    restore_best_weights=True, # Restore model weights from the epoch with the best value of the monitored metric
-    verbose=1                  # Print messages when early stopping is triggered
-)
+    # Monitor 'val_loss' and wait for 5 epochs without improvement before stopping
+    early_stopping_callback = EarlyStopping(
+        monitor='val_loss',        # Metric to monitor
+        patience=5,                # Number of epochs with no improvement after which training will be stopped
+        restore_best_weights=True, # Restore model weights from the epoch with the best value of the monitored metric
+        verbose=1                  # Print messages when early stopping is triggered
+    )
 
-# Train the model
-history = model.fit(
-    train_generator,
-    epochs=100,
-    validation_data=validation_generator,
-    callbacks =[early_stopping_callback]
-)
+    # Train the model
+    history = model.fit(
+        train_generator,
+        epochs=100,
+        validation_data=validation_generator,
+        callbacks =[early_stopping_callback]
+    )
 
-model.save('img_cl.keras')
+    model.save('img_cl.keras')
+
+    return list(train_generator.class_indices.keys())
+
+
 # Optional: Unfreeze and fine-tune the model for better accuracy
 # base_model.trainable = True
 # model.compile(...)
 # model.fit(...)
 
 # Plot training history
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs_range = range(len(acc))
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs_range = range(len(acc))
 
 # plt.figure(figsize=(10, 5))
 # plt.subplot(1, 2, 1)
@@ -133,43 +139,52 @@ def predict_image(image_path, model, class_names):
     return predicted_class
 
 # Example usage (assuming you have a test image named 'test_image.jpg')
-class_names = list(train_generator.class_indices.keys())
-img_list = [
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/76b281d8-f830-4f24-9b45-ec02e88b52ac.jpg", "label": "people"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/8109e957-ce86-4283-96aa-11c55d2fba62-1.jpg", "label": "people"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/129c39e4-e7bf-4cfd-9bdd-0eb541bf9a60.jpg", "label": "scenic"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/679e1665-6c43-4018-8a8f-ffb2f33739b6-1.jpg", "label": "scenic"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/7f5a9544-f625-4bbf-ad25-e0b874a4e5fd-1.jpg", "label": "document"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/107e99ff-289f-4d92-b2e6-c4943dccbccd.jpg", "label": "document"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/91f2dc11-cffd-4d08-8575-7d437ef31774.jpg", "label": "scenic"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/121b15b7-81b3-4792-b654-c95aa2ee4b49-1.jpg", "label": "scenic"},
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2835-2.JPG", "label": "document"}, 
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2838.JPG", "label": "document"}, 
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2842-2.JPG", "label": "document"}, 
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2860.JPG", "label": "document"}, 
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2861.JPG", "label": "document"}, 
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2864.JPG", "label": "document"}, 
-    {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2874-2.JPG", "label": "document"}
+#class_names = list(train_generator.class_indices.keys())
+def static_test_model():
+    
+    img_list = [
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/76b281d8-f830-4f24-9b45-ec02e88b52ac.jpg", "label": "people"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/8109e957-ce86-4283-96aa-11c55d2fba62-1.jpg", "label": "people"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/129c39e4-e7bf-4cfd-9bdd-0eb541bf9a60.jpg", "label": "scenic"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/679e1665-6c43-4018-8a8f-ffb2f33739b6-1.jpg", "label": "scenic"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/7f5a9544-f625-4bbf-ad25-e0b874a4e5fd-1.jpg", "label": "document"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/107e99ff-289f-4d92-b2e6-c4943dccbccd.jpg", "label": "document"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/91f2dc11-cffd-4d08-8575-7d437ef31774.jpg", "label": "scenic"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/121b15b7-81b3-4792-b654-c95aa2ee4b49-1.jpg", "label": "scenic"},
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2835-2.JPG", "label": "document"}, 
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2838.JPG", "label": "document"}, 
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2842-2.JPG", "label": "document"}, 
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2860.JPG", "label": "document"}, 
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2861.JPG", "label": "document"}, 
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2864.JPG", "label": "document"}, 
+        {"img": "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/IMG_2874-2.JPG", "label": "document"}
 
-]
-for d in img_list:
-  p_class = predict_image(d["img"], model, class_names)
-  print(f'predicted: {p_class} actual: {d["label"]}' )
+    ]
+    for d in img_list:
+        p_class = predict_image(d["img"], model, class_names)
+        print(f'predicted: {p_class} actual: {d["label"]}' )
+
+
+def execute():
+    
 
 
 
-# Assume you have a new image named 'new_image.jpg' in the project directory
-new_image_path = "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/6465dc91-6216-4b0e-81ee-3ad87eabe623-4.jpg"
+# # Assume you have a new image named 'new_image.jpg' in the project directory
+# new_image_path = "/home/madhekar/work/home-media-app/data/input-data/img/madhekar/767bfd11-78fa-573e-ac47-cb74883dc6c9/6465dc91-6216-4b0e-81ee-3ad87eabe623-4.jpg"
 
-img = load_img(new_image_path, target_size=image_size)
-img_array = img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
+# img = load_img(new_image_path, target_size=image_size)
+# img_array = img_to_array(img)
+# img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+# predictions = model.predict(img_array)
+# score = tf.nn.softmax(predictions[0])
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
-        class_names[np.argmax(score)], 100 * np.max(score)
-    )
-)
+# print(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
+#         class_names[np.argmax(score)], 100 * np.max(score)
+#     )
+# )
+
+
+
