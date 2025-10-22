bbca1e57402f8659de6cb1f17c3e0fc33d0e6034
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-22 10:28:38 -0700

    with fix

--- research/code/test/latlong/exif_write_custom/exif_write_sample.py
+++ research/code/test/latlong/exif_write_custom/exif_write_sample.py
@@ -38,8 +38,8 @@ filenames = ['/Users/emadhekar/Pictures/00e39dd1-e166-49ae-9f9e-e83b2546b056.JPG
              '/Users/emadhekar/Pictures/1a5e9da6-462d-4f2f-a289-5c45e0db1176.JPG',
              '/Users/emadhekar/Pictures/5c726d80-e2db-4600-8483-f6c1b88fcec2.JPG']
 
-# with ExifTool() as e:
-#             metadata = e.get_metadata(*filenames)
+with ExifTool() as e:
+            metadata = e.get_metadata(*filenames)
 
-e = ExifTool()
-metadata = e.get_metadata(*filenames)
+# e = ExifTool()
+# metadata = e.get_metadata(*filenames)
