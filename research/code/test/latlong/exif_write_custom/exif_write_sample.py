3850e3d8b8e7e1fa2ceb9a2f86b1b2a0a1121711
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-22 09:55:45 -0700

    poc custom batch write

--- research/code/test/latlong/exif_write_custom/exif_write_sample.py
+++ research/code/test/latlong/exif_write_custom/exif_write_sample.py
@@ -11,9 +11,9 @@ class ExifTool(object):
 
     def __enter__(self):
         self.process = subprocess.Popen(
-            [self.executable, "-stay_open", "True",  "-@", "-"],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        return self
+         [self.executable, "-stay_open", "True",  "-@", "-"],
+         universal_newlines=True,
+         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     def  __exit__(self, exc_type, exc_value, traceback):
         self.process.stdin.write("-stay_open\nFalse\n")
@@ -26,8 +26,14 @@ class ExifTool(object):
         output = ""
         fd = self.process.stdout.fileno()
         while not output.endswith(self.sentinel):
-            output += os.read(fd, 4096)
+            output += os.read(fd, 4096).decode('utf-8')
         return output[:-len(self.sentinel)]
 
     def get_metadata(self, *filenames):
         return json.loads(self.execute("-G", "-j", "-n", *filenames))
+    
+filenames = ['/Users/emadhekar/Pictures/00e39dd1-e166-49ae-9f9e-e83b2546b056.JPG',
+             '/Users/emadhekar/Pictures/1a5e9da6-462d-4f2f-a289-5c45e0db1176.JPG',
+             '/Users/emadhekar/Pictures/5c726d80-e2db-4600-8483-f6c1b88fcec2.JPG']
+with ExifTool() as e:
+            metadata = e.get_metadata(*filenames)
