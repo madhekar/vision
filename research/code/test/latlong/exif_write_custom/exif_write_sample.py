c161c9611f61da186b61da80ba7194952d5e496c
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-22 10:09:29 -0700

    bug fix

--- research/code/test/latlong/exif_write_custom/exif_write_sample.py
+++ research/code/test/latlong/exif_write_custom/exif_write_sample.py
@@ -8,6 +8,7 @@ class ExifTool(object):
 
     def __init__(self, executable="/usr/bin/exiftool"):
         self.executable = executable
+        self.process = None
 
     def __enter__(self):
         self.process = subprocess.Popen(
@@ -35,5 +36,9 @@ class ExifTool(object):
 filenames = ['/Users/emadhekar/Pictures/00e39dd1-e166-49ae-9f9e-e83b2546b056.JPG',
              '/Users/emadhekar/Pictures/1a5e9da6-462d-4f2f-a289-5c45e0db1176.JPG',
              '/Users/emadhekar/Pictures/5c726d80-e2db-4600-8483-f6c1b88fcec2.JPG']
-with ExifTool() as e:
-            metadata = e.get_metadata(*filenames)
+
+# with ExifTool() as e:
+#             metadata = e.get_metadata(*filenames)
+
+e = ExifTool()
+metadata = e.get_metadata(*filenames)
