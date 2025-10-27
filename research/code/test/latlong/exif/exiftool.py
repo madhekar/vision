73938a1af7b133afb99544e750d60dfde14fdb80
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-10-27 09:53:17 -0700

    poc3

---
+++ research/code/test/latlong/exif/exiftool.py
@@ -0,0 +1,8 @@
+import exiftool
+
+files = ["a.jpg", "b.png", "c.tif"]
+with exiftool.ExifTool() as et:
+    metadata = et.get_metadata_batch(files)
+for d in metadata:
+    print("{:20.20} {:20.20}".format(d["SourceFile"],
+                                     d["EXIF:DateTimeOriginal"]))
