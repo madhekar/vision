adff12dfbb5d17b9b943367db3a0bd9a88088f73
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-11-07 13:08:42 -0800

    bug

--- research/code/test/nvidia_test/pytorch.py
+++ research/code/test/nvidia_test/pytorch.py
@@ -1,6 +1,6 @@
-    import torch
-    print(torch.__version__)
-    print(torch.cuda.is_available())
-    print(torch.cuda.get_device_name(0))
-    print(torch.cuda.get_device_properties(0).major)
-    print(torch.cuda.get_device_properties(0).minor)
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+print(torch.cuda.get_device_properties(0).major)
+print(torch.cuda.get_device_properties(0).minor)
