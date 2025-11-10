07436b2214b11998926837acacdcc69384f9805f
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-11-10 12:40:01 -0800

    verify image

--- research/code/test/zm/utils/dedup_util/phash/ImgToHash.py
+++ research/code/test/zm/utils/dedup_util/phash/ImgToHash.py
@@ -48,9 +48,12 @@ class ImageToHash(object):
         :param hash_algo: The hash algorithm.
         :return: an ImageHash.
         """
-        img =cv2.imread(image_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return hash_algo_dict[hash_algo](img, hash_size=hash_size)
+        #img =cv2.imread(image_path)
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        img = Image.open(image_path)
+        if img.verify():
+           return hash_algo_dict[hash_algo](Image.open(image_path), hash_size=hash_size)
 
     @staticmethod
     def get_images_list(path, natural_order=True):
