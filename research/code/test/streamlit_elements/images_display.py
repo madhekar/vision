d7e8d4d12a9d6f60b4a2e024a44e1e4caca795bc
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-11-26 11:56:55 -0800

    image display poc

---
+++ research/code/test/streamlit_elements/images_display.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import time
+import os
+
+
+def get_image_files(folder_path):
+    """
+    Returns a list of image filenames from a specified folder.
+    """
+    image_files = []
+    valid_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
+
+    for filename in os.listdir(folder_path):
+        # Get the file extension
+        _, ext = os.path.splitext(filename)
+        # Check if the extension is in the list of valid image extensions
+        if ext.lower() in valid_extensions:
+            filename = os.path.join(folder_path, filename)
+            image_files.append(filename)
+    return image_files
+
+
+image_holder = st.empty()
+images = get_image_files("/Users/emadhekar/Pictures")
+print(images)
+
+for item in images:
+    image_holder.image(item)
+    time.sleep(5000)
