85f4f1e79e190a941f2fd48af5c4015e4fa52670
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-12-18 09:58:27 -0800

    bug fix 45

--- research/code/test/streamlit_elements/folder_stat_ux.py
+++ research/code/test/streamlit_elements/folder_stat_ux.py
@@ -72,9 +72,9 @@ for src in src_list:
             # print(ftrim)
             npath = os.path.normpath(ftrim)
             path_list = npath.split(os.sep)
-            if len(path_list) <=2:
-                path_list.append("data")
-            # print(path_list)
+            if len(path_list) ==3:
+                path_list[2] = "data"
+            print(path_list)
             # print(f"{ftrim:<30} | {count:<15} | {size_gb:<15.4f}")
             rlist.append({"source": src, "data_stage": path_list[0], "data_type": path_list[1], "data_attrib": path_list[2],  "count": count, "size": size_gb})
 
@@ -84,13 +84,16 @@ for src in src_list:
             # print(ftrim)
             npath = os.path.normpath(ftrim)
             path_list = npath.split(os.sep)
-            if len(path_list) <=2:
-                path_list.append("data")
-            # print(path_list)
+            if len(path_list) == 3:
+                path_list[2] = "data"
+            print(path_list)
             # print(f"{ftrim:<30} | {0:<15} | {0:<15.4f} ")
             rlist.append({"source": src, "data_stage": path_list[0], "data_type": path_list[1], "data_attrib": path_list[2],  "count": 0, "size": 0.0})
             pass
 
 df = pd.DataFrame(rlist, columns=["source", "data_stage", "data_type", "data_attrib", "count", "size"])
-
 print(df)
+
+out = df.pivot_table(index=["source", "data_stage", "data_type"], columns=["data_attrib"], values=["count", "size"])
+
+print(out)
