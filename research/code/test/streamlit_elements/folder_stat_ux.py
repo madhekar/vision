e06e9019f98e8373c8c34611f7a4bf0b40348c73
Bhalchandra Madhekar <bmadhekar@gmail.com>
2025-12-22 10:09:13 -0800

    ux fixes

--- research/code/test/streamlit_elements/folder_stat_ux.py
+++ research/code/test/streamlit_elements/folder_stat_ux.py
@@ -208,11 +208,12 @@ def filter_selection(df):
     # Create the chart
     chart = (
         alt.Chart(df)
-        .mark_bar()
+        .mark_circle()
         .encode(
-            x="count:Q",
-            y="size:Q",
-            color="data_attrib:N",
+            x=alt.X("count:Q", axis=alt.Axis(grid=True, gridColor='grey')),
+            y=alt.Y("size:Q", axis=alt.Axis(grid=True, gridColor="grey")),
+            size="source:N",
+            color="data_stage:N",
             tooltip=["source", "data_stage", "data_type", "data_attrib"],
         )
         .add_params(source_selection, data_stage_selection, data_type_selection)
