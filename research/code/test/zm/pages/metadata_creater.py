b5b6c68cada5bcfadfa5fa0a7311350f17230187
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-09 13:27:44 -0800

    fix 24

--- research/code/test/zm/pages/metadata_creater.py
+++ research/code/test/zm/pages/metadata_creater.py
@@ -4,7 +4,7 @@ from utils.preprocess_util import preprocess as pp
 from utils.util import storage_stat as ss
 from utils.config_util import config
 
-(raw_data_path, input_data_path, app_data_path, final_data_path) = config.overview_config_load()
+(raw_data_path, input_data_path, app_data_path, final_data_path,*_) = config.overview_config_load()
 
 user_source_selected = st.sidebar.selectbox("data source folder", options=ss.extract_user_raw_data_folders(raw_data_path), label_visibility="collapsed")
 
