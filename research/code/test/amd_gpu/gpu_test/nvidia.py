7ce71cdc4778bc59649141cc39c4a87ec6562f6e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-15 11:48:45 -0800

    oom error cuda

--- research/code/test/amd_gpu/gpu_test/nvidia.py
+++ research/code/test/amd_gpu/gpu_test/nvidia.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 import torch
-
+import os
 
 # Initialize pipeline on CPU
 # pipe = pipeline('text-generation', model='gpt2', device=-1)
@@ -9,6 +9,13 @@ import torch
 # if torch.cuda.is_available():
 #     pipe.model.to("cuda:0")
 
+# Source - https://stackoverflow.com/a/74952995
+# Posted by chakrr, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-15, License - CC BY-SA 4.0
+
+torch.cuda_empyt_cache()
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
 model_id = "xtuner/llava-llama-3-8b-v1_1-transformers" #"xtuner/llava-phi-3-mini-hf" #"xtuner/llava-llama-3-8b-hf"
 pipe = pipeline("image-to-text", model=model_id, device=-1)
