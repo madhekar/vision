ab56b93c7262178587389d75b348b6d22188d0e1
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-15 15:32:37 -0800

    4bit qualtized

---
+++ research/code/test/amd_gpu/gpu_test/nvidia_3.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
+
+# 4-bit quantization config
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "xtuner/llava-llama-3-8b-v1_1-transformers",
+    quantization_config=bnb_config,
+    device_map="auto" # Automatically map to available devices
+)
