b642d9aed80539eb12db5bded6641ed8112c017f
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-19 14:23:32 -0800

    ollama poc

---
+++ research/code/test/zm/utils/preprocess_util/ollama_llava_next.py
@@ -0,0 +1,50 @@
+import ollama
+import base64
+
+
+'''
+Now we need to enable vulkan, allow the Ollama server to talk to your local network, and allow flash attention. Flash attention is optional, but recommended. In a terminal enter the following:
+
+sudo systemctl edit ollama
+You’ll get a nano editor with the service’s settings. Add the following lines after the “### Anything between here and the comment below<…>” to set environmental variables.
+
+The OLLAMA_HOST line will allow anything on your local network (the 0.0.0.0 part) to find and talk to the server. If you are working purely local to your machine, don’t add this.
+The OLLAMA_VULKAN line requests Ollama to use vulkan rather than rocm which has been problematic.
+The OLLAMA_FLASH_ATTENTION line will enable flash attention which is ideal for larger contexts. The last line is optional and will depend on your needs.
+The OLLAMA_CONTEXT_LENGTH line allows you to set the context window size.
+Note that not all models will support large contexts. Change this value up or down to suit your needs. Be aware that large contexts may also degrade performance.
+The default if you don’t add this line is 4096 tokens which is not many. 32k is a good number for general coding. I typically set mine to 50k or more if the model can handle it.
+### Editing /etc/systemd/system/ollama.service.d/override.conf
+
+### Anything between here and the comment below will become the contents of the drop-in file
+
+[Service]
+Environment="OLLAMA_HOST=0.0.0.0:11434"
+Environment="OLLAMA_VULKAN=1"
+Environment="OLLAMA_FLASH_ATTENTION=1"
+Environment="OLLAMA_CONTEXT_LENGTH=32768"
+'''
+# Function to encode the image to base64
+def encode_image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def describe_image(img_path, sup_content):
+
+# Path to your image
+#image_path = '/home/madhekar/temp/filter/training/people/IMG_5379.PNG'
+    encoded_image = encode_image_to_base64(img_path)
+
+    # Perform inference
+    response = ollama.chat(
+        model='llava:13b',
+        messages=[
+            {
+                'role': 'user',
+                'content': [sup_content],
+                'images': [encoded_image]
+            }
+        ],
+    )
+
+    print(response['message']['content'])
