af9e9d79b5fe8896cf5aa7b0b7e5032d6a62fd34
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-20 12:59:17 -0800

    GUP debug

--- research/code/test/zm/utils/preprocess_util/ollama_llava_next.py
+++ research/code/test/zm/utils/preprocess_util/ollama_llava_next.py
@@ -35,7 +35,7 @@ def describe_image(img_path, sup_content):
 
     # Perform inference
     response = ollama.chat(
-        model='llava:13b',
+        model='llava',
         messages=[
             {
                 'role': 'system',
