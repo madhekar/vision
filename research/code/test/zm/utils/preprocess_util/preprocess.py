bb3641d27ec1a90d7d14fd1693d351d85d1b564c
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-22 13:21:53 -0800

    await

--- research/code/test/zm/utils/preprocess_util/preprocess.py
+++ research/code/test/zm/utils/preprocess_util/preprocess.py
@@ -262,7 +262,7 @@ async def run_workflow(
                     rflist, oflist = new_xform(res)
 
                     #res1 = await asyncio.gather(pool.map(describeImage,  rflist))
-                    res1 = pool.map(describeImage,  rflist)
+                    res1 = await pool.map(describeImage,  rflist)
                     print('****', res1)
 
                     zlist = [oflist[i] + [res1[0][i]]  for i in range(len(oflist))]
