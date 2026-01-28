3baeeba13e7e15eb10b98743178c30055f14fa34
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-28 13:49:40 -0800

    relative path

--- research/code/test/zip_archive/parallel_archive.py
+++ research/code/test/zip_archive/parallel_archive.py
@@ -2,16 +2,16 @@ import subprocess
 import os
 from concurrent.futures import ProcessPoolExecutor
 
-def zip_folder(folder_path):
+def zip_folder(folder_path, relative_path):
     """Zips a single folder using system subprocess (zip command)."""
-    zip_name = f"{os.path.basename(os.path.normpath(folder_path))}.zip"
+    zip_name = "archive/" + f"{os.path.basename(os.path.normpath(folder_path))}.zip"
     print(f"Starting compression: {folder_path} -> {zip_name}")
     
     # -r: recursive, -q: quiet
     cmd = ["zip", "-r", "-q", zip_name, folder_path]
     
     try:
-        subprocess.run(cmd, cwd="/home/madhekar/work", check=True, capture_output=True)
+        subprocess.run(cmd, cwd=relative_path, check=True, capture_output=True)
         print(f"Finished: {zip_name}")
     except subprocess.CalledProcessError as e:
         print(f"Error zipping {folder_path}: {e}")
@@ -24,10 +24,15 @@ def main():
                   'home-media-app/data/final-data/video',
                   'home-media-app/data/final-data/audio',
                   'home-media-app/data/app-data']
-    
+    relative_path =['/home/madhekar/work',
+                    '/home/madhekar/work',
+                    '/home/madhekar/work',
+                    '/home/madhekar/work',
+                    '/home/madhekar/work',
+                    '/home/madhekar/work']
     # Use ProcessPoolExecutor for true parallelism
     with ProcessPoolExecutor() as executor:
-        executor.map(zip_folder, folders_to_zip)
+        executor.map(zip_folder, folders_to_zip, relative_path)
 
 if __name__ == "__main__":
     main()
