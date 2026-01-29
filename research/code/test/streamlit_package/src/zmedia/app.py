ea1724aef9b1cd65449a59af70b956b5e421b201
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-01-29 13:29:52 -0800

    package poc

---
+++ research/code/test/streamlit_package/src/zmedia/app.py
@@ -0,0 +1,9 @@
+# src/your_app_package_name/app.py
+import streamlit as st
+
+def main():
+    st.title("My Streamlit App Package")
+    st.write("This app is running as an installed package.")
+
+if __name__ == "__main__":
+    main()
