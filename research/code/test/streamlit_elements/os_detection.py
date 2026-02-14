895d48ebebc4598b71024a6481afe85d1076f75e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-02-14 15:51:36 -0800

    os specific ds

--- research/code/test/streamlit_elements/os_detection.py
+++ research/code/test/streamlit_elements/os_detection.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import platform
+from pathlib import Path, WindowsPath
 import os
 
+img_path = "/home/madhekar/work/home-media-app/data/final-data/img/madhekar/2596441a-e02f-588c-8df4-dc66a133fc99/IMG_6683.PNG"
+linux_prefix = "/mnt/zmdata/"
+mac_prefix = "/Users/Share/zmdata/"
+win_prefix = "c:/Users/Public/zmdata/"
+token = "home-media-app"
 st.title("Server OS Detector")
 
 # Get OS information
@@ -18,8 +24,23 @@ st.write(f"**Platform Version:** {platform_version}")
 
 # Example of conditional logic based on OS
 if platform_system == "Windows":
-    st.success("Running on Windows")
+    w_img_path = WindowsPath(img_path)
+    parts = w_img_path.split(token,1)
+    if len(parts) > 1:
+        n_pth = win_prefix + token + parts[1]
+    st.success(f"Running on Windows: {n_pth} ")
+
 elif platform_system == "Linux":
-    st.success("Running on Linux (likely Streamlit Cloud/Docker)")
-else:
+    parts = img_path.split(token,1)
+    if len(parts) > 1:
+        n_pth = linux_prefix + token + parts[1]
+    st.success(f"Running on Linux: {n_pth}")
+
+elif platform_system == "Darwin":
+    parts = img_path.split(token,1)
+    print(parts)
+    if len(parts) > 1:
+        n_pth = mac_prefix + token + parts[1]
+    st.success(f"Running on MacOS : {n_pth}")
+else:    
     st.info(f"Running on: {platform_system}")
