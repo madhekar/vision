56883c92417b9b5ca8aea46e198444f4627de3c0
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-02-19 10:55:43 -0800

    notes

--- research/code/test/zm/utils/dedup_util/phash/KDduplicates.py
+++ research/code/test/zm/utils/dedup_util/phash/KDduplicates.py
@@ -9,7 +9,7 @@ from utils.util import model_util  as mu
 import random
 from utils.dedup_util.phash import helper_functions as hf #import build_tree, save_results
 
-def remove_duplicates(img_file_list, output_path, hash_size=128, tree_type='KDTree', distance_metric='manhattan', nearest_neighbors=20,
+def remove_duplicates(img_file_list, output_path, hash_size=64, tree_type='KDTree', distance_metric='manhattan', nearest_neighbors=10,
            leaf_size=16, hash_algo='phash', parallel='y', batch_size=64, threshold=20, backup_keep=False, backup_duplicate=True, safe_deletion=False, image_w=32, image_h=32):
     
     # Build the tree
