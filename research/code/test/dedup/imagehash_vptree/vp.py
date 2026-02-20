e29d8e72f4952e693d1654498937eeddcd40f5d2
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-02-19 17:51:44 -0800

    decision pt

--- research/code/test/dedup/imagehash_vptree/vp.py
+++ research/code/test/dedup/imagehash_vptree/vp.py
@@ -1,7 +1,49 @@
 import random
 from PIL import Image
+from collections import defaultdict
 import imagehash
 import os
+
+'''
+[
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8133.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8132.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8134.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8132.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8132.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8133.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/52d3b8f6-b993-50bf-b2a0-0debd314f98a/IMG_8134.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/8d9e4356-64fa-5ae4-bb63-3614752f08fa/IMG_8106.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/8d9e4356-64fa-5ae4-bb63-3614752f08fa/IMG_8109.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/8d9e4356-64fa-5ae4-bb63-3614752f08fa/IMG_8109.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/8d9e4356-64fa-5ae4-bb63-3614752f08fa/IMG_8106.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/c8bf737f-6bcb-5a84-b13c-d588f324788e/IMG_5403.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/c8bf737f-6bcb-5a84-b13c-d588f324788e/IMG_5404.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/c8bf737f-6bcb-5a84-b13c-d588f324788e/IMG_5404.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/c8bf737f-6bcb-5a84-b13c-d588f324788e/IMG_5403.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/bb617fd9-0341-5ce0-aaaa-32b99451d302/image1-23.jpg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/bb617fd9-0341-5ce0-aaaa-32b99451d302/image1-small-24.jpg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/bb617fd9-0341-5ce0-aaaa-32b99451d302/image1-small-24.jpg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/bb617fd9-0341-5ce0-aaaa-32b99451d302/image1-23.jpg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/baa04b52-1d78-55c5-b79d-71905e7e40aa/IMG_6156.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/baa04b52-1d78-55c5-b79d-71905e7e40aa/IMG_6157.jpeg'], 
+
+['/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/baa04b52-1d78-55c5-b79d-71905e7e40aa/IMG_6157.jpeg', 
+'/mnt/zmdata/home-media-app/data/input-data/img/Berkeley/baa04b52-1d78-55c5-b79d-71905e7e40aa/IMG_6156.jpeg']
+]
+
+'''
+
+
 
 def generate_hashes(image_dir):
     hashes = {}
@@ -41,37 +83,58 @@ def hamming_distance(hash1, hash2):
 
 # Conceptual class - actual implementation will be in a specific library
 class VPTree:
-    def __init__(self, items, distance_function):
+    def __init__(self, items, idict, distance_function):
         # ... implementation details ...
         self.items = items.values()
+        self.idict = idict
         self.distance_function = distance_function
 
     def find_duplicates(self, threshold):
-        duplicates = set()
+        #duplicates = defaultdict(list)
+        res = []
         for i, item in enumerate(self.items):
             #print(i, item)
             # Query the tree for neighbors within the threshold
             # The query_tree method is the core VP-tree functionality
-            neighbors = self.query_tree(item, threshold) 
-            #print(f'neighbors: {neighbors}')
-            for distance, neighbor_item in neighbors:
-                print(f"---> {distance} - {neighbor_item}")
-                # Add to duplicates list, ensuring we don't count an item as its own duplicate
-                if neighbor_item != item:
-                    duplicates.add(tuple((item, neighbor_item))) # Use tuple for unique pairs
-        return duplicates
+            neighbors = self.query_tree_dict(i, item, threshold) 
+            
+            if len(neighbors) > 1:
+                res.append(neighbors)
+            #   print(f'neighbors: {neighbors}')
+            #   for neighbor_item in neighbors:
+            #     #print(f"---> {distance} - {neighbor_item}")
+            #     # Add to duplicates list, ensuring we don't count an item as its own duplicate
+            #     if neighbor_item != self.idict[item]:
+            #         duplicates[item].append(neighbor_item) # Use tuple for unique pairs
+        return res
     
     def query_tree(self, query_item, threshold):
         # ... conceptual nearest neighbor search implementation ...
         # In a real library, this would efficiently prune the search space
         #print(f"->>>>{query_item}")
+        print(f"H {query_item}")
         found = []
         for item in self.items:
             if item != query_item:
                 dist = self.distance_function(query_item, item)
                 if dist <= threshold:
                     found.append((dist, item))
+                    print(f"C {item} -- {found}")
         return found
+    
+    def query_tree_dict(self, i, query_item, threshold):
+        found = []#defaultdict(list)
+        found.append(self.idict[query_item])
+        for item in self.items:
+            if item != query_item:
+                #print(f"===={query_item}")
+                dist = self.distance_function(query_item, item)
+                if dist <= threshold:
+                    #print(f"{i}:{self.idict[query_item]}=={self.idict[item]}")
+                    #found[self.idict[query_item]].append(self.idict[item])
+                    found.append(self.idict[item])
+        #print(found)            
+        return found 
 
 # Example Usage:
 # Assume these are 64-bit integer hashes for images
@@ -84,10 +147,11 @@ class VPTree:
 # ]
 image_dir = "/mnt/zmdata/home-media-app/data/input-data/img/Berkeley"
 image_hashes = generate_hashes(image_dir=image_dir)
+idict = {v:k for k, v in image_hashes.items()}
 # Build the VP Tree (using the conceptual class)
 # A real library would have a 'build' method, e.g., `tree = vptree.VPTree(image_hashes, hamming_distance)`
-vp_tree = VPTree(image_hashes, hamming_distance)
-print('----', vp_tree)
+vp_tree = VPTree(image_hashes, idict, hamming_distance)
+#print('----', vp_tree)
 # Find duplicates with a threshold of 1 (exact or 1-bit difference)
 # This will return pairs of items that are considered duplicates
 duplicate_pairs = vp_tree.find_duplicates(threshold=2)
