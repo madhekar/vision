9867dfe5af1447b4ca8d717c508ea3ec5dcee7d3
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-02-20 14:39:57 -0800

    bug fix dup

--- research/code/test/dedup/imagededup/cnn.py
+++ research/code/test/dedup/imagededup/cnn.py
@@ -1,7 +1,7 @@
 from imagededup.methods import CNN
 
 # Define the directory containing your images
-image_directory = "/mnt/zmdata/home-media-app/data/input-data/img/madhekar"
+image_directory = "/mnt/zmdata/home-media-app/data/input-data/img/Berkeley"
 
 # 1. Initialize the CNN encoder
 cnn_encoder = CNN()
@@ -10,7 +10,7 @@ cnn_encoder = CNN()
 # min_similarity_threshold can be adjusted (e.g., 0.9 for high similarity)
 duplicates = cnn_encoder.find_duplicates(
     image_dir=image_directory,
-    min_similarity_threshold=0.95, # Adjust as needed
+    min_similarity_threshold=0.98, # Adjust as needed
     scores=False, # Set to True to get similarity scores
     recursive=True
 )
@@ -22,7 +22,7 @@ for key, value in duplicates.items():
     if len(value) > 0:
         print(f"{key}: {value}")
         res = [(key, v) for v in value]
-        dup_images_to_remove.append(res)
+        dup_images_to_remove.extend(res)
 
 
 print(dup_images_to_remove)
