0c7843b04e54624e763e6db3edb26fe7a2a1f10c
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-02-27 10:38:27 -0800

    local smb

--- research/code/test/smb/mac_smb_cmd.py
+++ research/code/test/smb/mac_smb_cmd.py
@@ -1,4 +1,8 @@
 import os
+'''
+//[server-ip]/[share-path] /mnt/myshare cifs credentials=/etc/samba/myshare.cred,iocharset=utf8,nounix,file_mode=0777,dir_mode=0777 0 0
+'''
+
 mount_point = "/Users/Shared/zmdata" # Use an existing or newly created local path
 # The command might require root/sudo privileges
 os.system(f"mount_smbfs //madhekar:Manya1@us@192.168.68.121/home-media-app {mount_point}")
