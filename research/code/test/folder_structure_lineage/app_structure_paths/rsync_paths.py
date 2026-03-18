9cdd4700b55a89ee28bbb81628c6613201482629
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-03-18 10:22:52 -0700

    rsync

---
+++ research/code/test/folder_structure_lineage/app_structure_paths/rsync_paths.py
@@ -0,0 +1,2 @@
+rsync -avzh --progress /mnt/zmdata/home-media-app/data/app-data/vectordb madhekar@192.168.68.121:/mnt/zmdata/home-media-app/data/app-data/vectordb
+rsync -avzh --progress /mnt/zmdata/home-media-app/data/final-data madhekar@192.168.68.121:/mnt/zmdata/home-media-app/data/final-data
