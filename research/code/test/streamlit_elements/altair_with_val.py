96f7a78253dd4517f61d0dcf471c40e465a783fb
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-03-20 13:23:48 -0700

    c alt

--- research/code/test/streamlit_elements/altair_with_val.py
+++ research/code/test/streamlit_elements/altair_with_val.py
@@ -1,12 +1,27 @@
 import altair as alt
 from vega_datasets import data
+import pandas as pd
+import streamlit as st
 
 
 source = data.wheat()
-
-base = alt.Chart(source).encode(
-    x='wheat',
-    y="year:O",
-    text='wheat'
+images = [{"source": "Berkeley", "count": 580},{"source": "madhekar", "count": 1020},{"source": "samsung", "count": 200}]
+df = pd.DataFrame(images)
+print(df)
+base = alt.Chart(df).encode(
+    x='source:N',
+    y="count:Q",
+    text='count',
+    color='source:N'
 )
-base.mark_bar() + base.mark_text(align='left', dx=2)
+ch = base.mark_bar() + base.mark_text(align='center', dy=-10)
+
+
+
+st.altair_chart(ch, use_container_width=True)
+
+data = alt.graticule(step=[15,15])
+gc = alt.Chart(data).mark_geoshape(stroke="blue").project('orthographic', rotate=(0, -45,0))
+
+st.altair_chart(gc)
+
