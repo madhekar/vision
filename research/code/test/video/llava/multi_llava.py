8bc454f6fe63534c0eca9e67a2f2b2a714ff3348
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-03-30 10:23:48 -0700

    dj

--- research/code/test/video/llava/multi_llava.py
+++ research/code/test/video/llava/multi_llava.py
@@ -87,7 +87,7 @@ def describe_image( frames_path, ppt, location):
                     'images': eimg_list,
                 }
             ],
-            options={'num_predict': 300,  'temperature': 0.9},
+            options={'num_predict': 400,  'temperature': 0.8},
             stream=False # Get full response at once
         )
 
