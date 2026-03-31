fd4f4f6bd0c3dd0588fe544cf94baf0497e61fb2
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-03-31 12:19:04 -0700

    isample ex

--- research/code/test/InsightFace/agumentation/pil_agumentation.py
+++ research/code/test/InsightFace/agumentation/pil_agumentation.py
@@ -1,7 +1,7 @@
-from PIL import Augmentor
+import Augmentor
 
 # 1. Instantiate a Pipeline object pointing to your image directory
-p = Augmentor.Pipeline(source_directory="path/to/your/images")
+p = Augmentor.Pipeline(source_directory="/home/madhekar/tmp")
 
 # 2. Add operations to the Pipeline
 # Rotate 90 degrees left or right with 70% probability to simulate yaw
@@ -12,4 +12,4 @@ p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
 p.flip_left_right(probability=0.5)
 
 # 3. Sample from the pipeline to generate new images
-p.sample(10) # Generates 100 augmented images
+p.sample(5) # Generates 100 augmented images
