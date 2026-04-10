166fa7a9b927e694afbd14c386afecb21c5d42b3
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-04-10 12:06:39 -0700

    typo

--- research/code/test/zm/pages/metadata_creater.py
+++ research/code/test/zm/pages/metadata_creater.py
@@ -14,13 +14,13 @@ st.header("METADATA: GENERATE", divider="gray")
 c1, c2, c3 = st.columns([.1, .1, .8])
 
 with c1:
-        btn_metatdata = st.button(label='Image Metadata Generate', type='primary')
+        btn_metatdata = st.button(label='Image Metadata Generater', type='primary')
         if btn_metatdata:
                 st.info('step 2: create metadata for search such as annotations location, text, person names etc...')
                 pp.execute(user_source_selected)
 
 with c2:
-        btn_metatdata = st.button(label='Video Metadata Generate', type='primary')
+        btn_metatdata = st.button(label='Video Metadata Generater', type='primary')
         if btn_metatdata:
                 st.info('step 2: create metadata for search such as annotations location, text, person names etc...')
                 ppv.execute(user_source_selected)
