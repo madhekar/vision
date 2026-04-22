abe7c52e8337e1fed5be2d8ffeb74151c4355630
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-04-22 10:47:50 -0700

    new poc

---
+++ research/code/test/chromadb/text/xls_extract.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+'''
+To fix the ImportError: Pandas requires version '2.0.1' or newer of 'xlrd', upgrade xlrd using pip install --upgrade xlrd in your terminal or command prompt. If you are working with .xlsx files, it is highly recommended to install openpyxl instead using pip install openpyxl, as newer xlrd versions only support .xls files. 
+Recommended Solutions
+
+    Upgrade xlrd (For .xls files):
+    Run this command in your terminal/command prompt to update xlrd to the required version:
+    bash
+
+    pip install --upgrade xlrd
+
+    Use code with caution.
+    Install/Use openpyxl (For .xlsx files - Best Practice):
+    Newer versions of xlrd (2.0+) dropped support for .xlsx files. If you are reading .xlsx, install openpyxl:
+    bash
+
+    pip install openpyxl
+
+    Use code with caution.
+    Then, specify the engine in your pandas code:
+    python
+
+    import pandas as pd
+    df = pd.read_excel("file.xlsx", engine="openpyxl")
+
+    Use code with caution.
+    For Jupyter/Colab Notebooks:
+    Run this in a cell:
+    python
+
+    !pip install --upgrade xlrd
+
+    Use code with caution.
+     
+
+Other Potential Solutions
+
+    Virtual Environments: Ensure you are installing these packages in the same virtual environment where you are running your script.
+    Condo Environment: If using Anaconda, run conda install xlrd. 
+
+Are you working with .xls or .xlsx files?
+If you can share a snippet of your read_excel code, I can tell you exactly which command to run.
+
+'''
+
+df = pd.read_excel("/mnt/zmdata/home-media-app/data/input-data/txt/Berkeley/21d6c524-fe73-5118-b20e-1297ae057db6/Data Analysis Examples.xls")
+
+print(df.to_csv())
+
