4922c7e498c0fed3227806ab655283ba58b11a4c
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-05-21 14:27:03 -0700

    package module fix

--- research/code/test/image_quality/fix_image/fix_png.py
+++ research/code/test/image_quality/fix_image/fix_png.py
@@ -1,7 +1,16 @@
-from PIL import Image
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+# try:
+#     img = Image.open("IMG_0241.PNG")
+#     img.save("fixed.png")
+# except Exception as e:
+#     print(f"Error: {e}")
+
 
 try:
-    img = Image.open("IMG_0241.PNG")
-    img.save("fixed.png")
+    with Image.open("IMG_0241.PNG") as img:
+        img.verify()
+    print("Image is valid.")
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"Image is corrupted: {e}")
