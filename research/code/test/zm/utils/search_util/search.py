ce485c8cb32d0855e831e9a8e7634f5ed051929b
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-05-26 11:36:40 -0700

    cleanup ux

--- research/code/test/zm/utils/search_util/search.py
+++ research/code/test/zm/utils/search_util/search.py
@@ -369,7 +369,7 @@ def search_fn(client, cImgs, cTxts, cVideos):
             #     )
 
 
-            colt, cole = c2.columns([0.2, 0.8])
+            colt, cole = c2.columns([0.1, 0.9])
             with colt:
                 st.markdown("<p class='big-font-subh'>Caption: </p>", unsafe_allow_html=True)
             with cole:
@@ -377,21 +377,21 @@ def search_fn(client, cImgs, cTxts, cVideos):
                 st.markdown(o_caption, unsafe_allow_html=True)
 
 
-            colt, cole = c2.columns([0.2, 0.8])
+            colt, cole = c2.columns([0.1, 0.9])
             with colt:
-                st.markdown("<p class='big-font-subh'>Gleeful Desc: </p>", unsafe_allow_html=True)
+                st.markdown("<p class='big-font-subh'>Desc: </p>", unsafe_allow_html=True)
             with cole:
                 o_desc = f'<p class="input">{st.session_state["imgs"][index][1]["text"]}</p>'
                 st.markdown(o_desc, unsafe_allow_html=True)
 
-            colt, cole = c2.columns([0.2, 0.8])
+            colt, cole = c2.columns([0.1, 0.9])
             with colt:
                st.write("<p class='big-font-subh'>People: </p>", unsafe_allow_html=True)
             with cole:
                o_names = f'<p class="input">{st.session_state["imgs"][index][1]["ppt"]} </p>'
                st.markdown(o_names, unsafe_allow_html=True)
 
-            colt, cole = c2.columns([0.2, 0.8])
+            colt, cole = c2.columns([0.1, 0.9])
             with colt:
                st.write("<p class='input-subh'>Date Time: </p>", unsafe_allow_html=True)
             with cole:
@@ -399,7 +399,7 @@ def search_fn(client, cImgs, cTxts, cVideos):
                 o_datetime = f'<p class="input">{str(datetime.datetime.fromtimestamp(float(tts)))}</p>'
                 st.markdown(o_datetime, unsafe_allow_html=True)
 
-            colt, cole = c2.columns([0.2, 0.8])
+            colt, cole = c2.columns([0.1, 0.9])
             with colt:
                 st.write("<p class='big-font-subh'>Location: </p>", unsafe_allow_html=True)
             with cole:
@@ -445,7 +445,7 @@ def search_fn(client, cImgs, cTxts, cVideos):
         with c2:
             #c2.write(st.session_state["videos"]["metadatas"][0][1:][index]["text"])
 
-            colt, cole = st.columns([0.2, 0.8])
+            colt, cole = st.columns([0.1, 0.9])
             with colt:
                     st.markdown("<p class='big-font-subh'>Caption: </p>", unsafe_allow_html=True)
             with cole:
@@ -457,7 +457,7 @@ def search_fn(client, cImgs, cTxts, cVideos):
                         st.markdown("None", unsafe_allow_html=True)
 
 
-            colt, cole = st.columns([0.2, 0.8])
+            colt, cole = st.columns([0.1, 0.9])
 
             with colt:
                     st.markdown("<p class='big-font-subh'>Gleeful Desc: </p>", unsafe_allow_html=True)
@@ -472,7 +472,7 @@ def search_fn(client, cImgs, cTxts, cVideos):
             # #        o_names = f'<p class="input">{st.session_state["videos"]["metadatas"][0][1:][index]["ppt"]} </p>' #- {st.session_state["imgs"]["metadatas"][0][1:][index]["names"]}</p>'
             # #        st.markdown(o_names, unsafe_allow_html=True)
 
-            colt, cole = st.columns([0.2, 0.8])
+            colt, cole = st.columns([0.1, 0.9])
             with colt:
                 st.write("<p class='input-subh'>Date Time: </p>", unsafe_allow_html=True)
             with cole:
@@ -480,7 +480,7 @@ def search_fn(client, cImgs, cTxts, cVideos):
                     o_datetime = f'<p class="input">{str(tts)}</p>' # datetime.datetime.fromtimestamp(float(tts))
                     st.markdown(o_datetime, unsafe_allow_html=True)
 
-            colt, cole = st.columns([0.2, 0.8])
+            colt, cole = st.columns([0.1, 0.9])
             with colt:
                     st.write("<p class='big-font-subh'>Location: </p>", unsafe_allow_html=True)
             with cole:
