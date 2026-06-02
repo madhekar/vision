7331aa0ef622a43254e3a239a65500f3d96d9c7d
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-06-02 12:49:20 -0700

    test case 3

--- research/code/test/json_gen/batch_load_json.py
+++ research/code/test/json_gen/batch_load_json.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Define the batch/chunk size (number of rows per batch)
-chunk_size = 10000
+chunk_size = 1000
 chunks = []
 
 # Use chunksize to return an iterator
-with pd.read_json("large_file.json", lines=True, chunksize=chunk_size) as reader:
+with pd.read_json("/mnt/zmdata/home-media-app/data/app-data/metadata/ASSORT_K30/metadata.json", lines=True, chunksize=chunk_size) as reader:
     for chunk in reader:
         # Process your batch chunk here if needed
         chunks.append(chunk)
