fb558f12b58d9dbabdeca1d24fc010af50978e03
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-06-05 12:01:33 -0700

    poc container css

--- research/code/test/video/video_aspect/streamlit_aspect.py
+++ research/code/test/video/video_aspect/streamlit_aspect.py
@@ -5,7 +5,7 @@ def display(vid_input,vid_modified):
     co, cm = st.columns([1,1])
 
     with co:
-        with st.container(height=500, border=False):
+        with st.container(key="my_custom_container"):#(height=500, border=False):
             #st.markdown('<div class="responsive-img-container">', unsafe_allow_html=True)
             video_file = open(vid_input, "rb")
             video_bytes = video_file.read()
@@ -24,6 +24,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     layout="wide"
 )
+
 st.html("""
     <style>
     .responsive-img-container img {
@@ -35,6 +36,17 @@ st.html("""
     </style>
 """)
 
+st.markdown(
+    """
+    <style>
+    div[class*="st-key-my_custom_container"] {
+        height: 50vh;
+        overflow-y: auto; /* Adds a scrollbar if content overflows */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 o_vidio = "/home/madhekar/tmp/VID_20181205_171018.mp4"
 s_video = "/home/madhekar/tmp/output_1080p_video.mp4"
