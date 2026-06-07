611786c7b5a0791426bc6f00581a3f819107d138
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-06-07 14:46:06 -0700

    bug fix 7

--- research/code/test/zmedia_package/src/zmedia/utils/search_util/search.py
+++ research/code/test/zmedia_package/src/zmedia/utils/search_util/search.py
@@ -257,7 +257,7 @@ def search_fn(rr_model, cImgs, cTxts, cVideos):
             # )
 
             # execute image query with search criteria
-            st.session_state["imgs"] = cu.rerank_image_search(os.path.join(rr_model, './', similar_image.name), cImgs, rmax=20, top=9)
+            st.session_state["imgs"] = cu.rerank_image_search(rr_model, os.path.join(rr_model, './', similar_image.name), cImgs, rmax=20, top=9)
 
             #execute video query with search criteria
             st.session_state["videos"] = cu.rerank_video_search(rr_model, os.path.join('./', similar_image.name), cVideos, rerank=False, rmax=20, top=9)
