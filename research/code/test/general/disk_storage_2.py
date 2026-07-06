981f6a057d9f2e621e82849c73b1330f9f6eb694
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-07-06 10:23:26 -0700

    ss

--- research/code/test/general/disk_storage_2.py
+++ research/code/test/general/disk_storage_2.py
@@ -1,17 +1,18 @@
 import shutil
 
-# Use current directory
-path = '/'
+def storage_stats(path):
+    # Get disk usage information
+    usage = shutil.disk_usage(path)
 
-# Get disk usage information
-usage = shutil.disk_usage(path)
+    # Print disk usage information
+    print("Disk Usage Information:")
+    print("Total Space:", usage.total / (1024**3), " GB")
+    print("Used Space:", usage.used / (1024**3), " GB")
+    print("Free Space:", usage.free / (1024**3), " GB")
 
-# Print disk usage information
-print("Disk Usage Information:")
-print("Total Space:", usage.total // (1024**3), " GB")
-print("Used Space:", usage.used // (1024**3), " GB")
-print("Free Space:", usage.free // (1024**3), " GB")
+    # Convert to GB for readability
+    print("\nIn Gigabytes:")
+    print("Total Space: {:.2f} GB".format(usage.total / (1024**3)))
 
-# Convert to GB for readability
-print("\nIn Gigabytes:")
-print("Total Space: {:.2f} GB".format(usage.total / (1024**3)))
+
+storage_stats('/')    
