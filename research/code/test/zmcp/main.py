8d0738072ae178673446c2a66e110e64b4e3e712
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-07-20 10:51:13 -0700

    ipoc

--- research/code/test/zmcp/main.py
+++ research/code/test/zmcp/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from zmcp!")
+from fastmcp import FastMCP
+
+zm = FastMCP("Zmedia MCP")
+
+@zm.tool()
+def add_num(a: int, b: int) -> int:
+    return a + b 
+
+@zm.tool()
+def greet_user(name: str) -> str:
+    print(f"Hello from zmcp {name}!")
 
 
 if __name__ == "__main__":
-    main()
+    zm.run()
