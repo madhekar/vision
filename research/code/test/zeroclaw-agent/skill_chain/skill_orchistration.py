776b1079ed169d008e87235a194d24b135a2a6fb
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-08-06 16:25:28 -0700

    orchistration poc

--- research/code/test/zeroclaw-agent/skill_chain/skill_orchistration.py
+++ research/code/test/zeroclaw-agent/skill_chain/skill_orchistration.py
@@ -1,6 +1,15 @@
+
+import sys
 import json
 import subprocess as sub
 
-uri_list = sub.check_output(["python3", "skills/skills-query-executor/scripts/chroma_query_methods.py", "query_video_collections_uri","math"])
+# Check if arguments were passed
+if len(sys.argv) > 1:
+    print("First argument:", sys.argv[1])
+    uri_list = sub.check_output(["python3", "skills/skills-query-executor/scripts/chroma_query_methods.py", "query_video_collections_uri",sys.argv[1]])
+    print("after call", uri_list)
+    sub.call(["scripts/send_email_w_attach.sh","bmadhekar@gmail.com",uri_list[0][0], uri_list[0][1], uri_list[0][2]], shell=True)
+    
 
-sub.call(["scripts/send_email_w_attach.sh","bmadhekar@gmail.com",uri_list[0][0], uri_list[0][1], uri_list[0][2]], shell=True)
+else:
+    print("No arguments provided.")
