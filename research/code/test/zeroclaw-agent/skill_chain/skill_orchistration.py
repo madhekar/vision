60db7dbde49f5731328a9aaafca11a71fc421973
bhalchandra madhekar <bmadhekar@gmail.com>
2026-08-07 16:30:54 -0700

    updated poc

--- research/code/test/zeroclaw-agent/skill_chain/skill_orchistration.py
+++ research/code/test/zeroclaw-agent/skill_chain/skill_orchistration.py
@@ -6,11 +6,20 @@ import subprocess as sub
 # Check if arguments were passed
 if len(sys.argv) > 1:
     print("First argument:", sys.argv[1])
-    processes = sub.Popen(["python3", "./chroma_query_methods.py", "query_video_collections_uri",sys.argv[1]])
-    processes.wait()
-    #print("after call", uri_list, uri_list.stdout)
-    #sub.call(["scripts/send_email_w_attach.sh","bmadhekar@gmail.com",uri_list[0][0], uri_list[0][1], uri_list[0][2]], shell=True)
+    cmd1 = ["python3",
+            "./chroma_query_methods.py", 
+            "query_video_collections_uri", 
+            sys.argv[1]]
+    uri_list = sub.run(cmd1, stdout=sub.PIPE, stderr=sub.PIPE, text=True)
     
+    print("after call", uri_list, uri_list.stdout)
+    cmd2 = ["python3", 
+            "./send_email_w_attach.sh",
+            "bmadhekar@gmail.com",
+            uri_list[0][0], 
+            uri_list[0][1], 
+            uri_list[0][2]]
 
+    sub.run(cmd2, shell=True, stdout=sub.PIPE, stderr=sub.PIPE, text=True)
 else:
     print("No arguments provided.")
