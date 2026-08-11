91fdf71f464d4a2a501db83056beb86dc868f2ce
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-08-11 13:36:46 -0700

    fix

--- research/code/test/zeroclaw-agent/skill_chain/skill_orchistration.py
+++ research/code/test/zeroclaw-agent/skill_chain/skill_orchistration.py
@@ -1,25 +1,38 @@
 
 import sys
 import json
-import subprocess as sub
+import subprocess
 
+module = "chroma_query_methods"
+method = "query_video_collection_uri"
+arg = "'Esha'" #sys.argv[1]
 # Check if arguments were passed
 if len(sys.argv) > 1:
     print("First argument:", sys.argv[1])
-    cmd1 = ["python3", "./chroma_query_methods.py", 
-            "query_video_collections_uri", 
-            sys.argv[1]]
-    uri_list = sub.call(cmd1)
+
+    cmd_1 = f"import {module}; {module}.{method}({arg})"
+
+    print(cmd_1)
+#     cmd1 = ["python3", "./chroma_query_methods.py", 
+#             "query_video_collections_uri", 
+#             sys.argv[1]]
+    with subprocess.run([sys.executable, "-c", cmd_1], 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.STDOUT, 
+                            #capture_output=True, 
+                            text=True) as process:
+        for line in process.stdout:
+            print(line, end="")
     
-    print("after call", uri_list)
+    #print("after call", result.stdout)
 
-    cmd2 = ["python3", 
-            "./send_email_w_attach.sh",
-            "bmadhekar@gmail.com",
-            uri_list[0], 
-            uri_list[1], 
-            uri_list[2]]
+#     cmd2 = ["python3", 
+#             "./send_email_w_attach.sh",
+#             "bmadhekar@gmail.com",
+#             uri_list[0], 
+#             uri_list[1], 
+#             uri_list[2]]
 
-    sub.run(cmd2, shell=True, stdout=sub.PIPE, stderr=sub.PIPE, text=True)
+#     sub.run(cmd2, shell=True, stdout=sub.PIPE, stderr=sub.PIPE, text=True)
 else:
     print("No arguments provided.")
