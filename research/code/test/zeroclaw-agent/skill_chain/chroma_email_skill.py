a36ae6e3856fd5483484e644d55c549ee20a517e
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-08-13 14:04:22 -0700

    new chroma skills

---
+++ research/code/test/zeroclaw-agent/skill_chain/chroma_email_skill.py
@@ -0,0 +1,23 @@
+import ast
+import sys
+import subprocess
+
+module = "chroma_query_methods"
+method = "query_video_collection_uri"
+arg_query, arg_email_id =  "'San Diego'", "email-id"
+# Check if arguments were passed
+if len(sys.argv) > 2:
+    print(f"arguments: {sys.argv[1]} : {sys.argv[2]}")
+    arg_email_id = f"{sys.argv[1]}"
+    arg_query = f'"{sys.argv[2]}"'
+    cmd_1 = f"import {module}; {module}.{method}({arg_query})"
+
+    cp = subprocess.run([sys.executable, "-c", cmd_1], capture_output=True, text=True, check=True)
+    valid_arr = ast.literal_eval(cp.stdout)
+
+    cmd_2 = ["./send_email_w_attach.sh", arg_email_id, valid_arr[0]['url'], valid_arr[0]['caption'], valid_arr[0]['text'], "--debug"]
+
+    print(cmd_2)
+    cp2 = subprocess.run(cmd_2,  capture_output=True, text=True, check=True)
+else:
+    print("No arguments provided. e.g skill_orchestration.py email-id query-string")
