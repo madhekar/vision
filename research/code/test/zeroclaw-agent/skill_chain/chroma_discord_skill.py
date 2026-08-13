a3ead634c534c720199913f0a53b3a323e7c9f87
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-08-13 14:27:36 -0700

    chroma discord skill

--- research/code/test/zeroclaw-agent/skill_chain/chroma_discord_skill.py
+++ research/code/test/zeroclaw-agent/skill_chain/chroma_discord_skill.py
@@ -19,15 +19,25 @@ if len(sys.argv) > 2:
 
     #cmd_2 = ["./send_email_w_attach.sh", arg_email_id, valid_arr[0]['url'], valid_arr[0]['caption'], valid_arr[0]['text'], "--debug"]
 
-    msg = valid_arr[0]['caption'] + "/n/n" +  valid_arr[0]['text']
+    msg = valid_arr[0]['caption'] + ":\n" +  valid_arr[0]['text']
     cmd_2 =     command = [
         "openclaw", 
+        "message",
         "send", 
-        arg_discord_id,
-        "--image", valid_arr[0]['url'], 
-        "--message", msg 
+        "--channel", "discord",
+        "--media", valid_arr[0]['url'], 
+        "--message", msg, 
+        "--target", arg_discord_id
     ]
-    print(cmd_2)
-    cp2 = subprocess.run(cmd_2,  capture_output=True, text=True, check=True)
+    #print(cmd_2)
+    try:
+        result = subprocess.run(cmd_2,  capture_output=True, text=True, check=True)
+        print("Message sent successfully!")
+        print("Output:", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Failed to send message.")
+        print("Error code:", e.returncode)
+        print("Error output:", e.stderr)
+
 else:
     print("No arguments provided. e.g chroma_discord_skill.py email-id query-string")
