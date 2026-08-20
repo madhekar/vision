42be6a31c814d82eb7c827c980001e7f7d53f30f
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-08-19 17:28:03 -0700

    skill update bug fix

--- research/code/test/zeroclaw-agent/skill_chain/chroma_email_skill.py
+++ research/code/test/zeroclaw-agent/skill_chain/chroma_email_skill.py
@@ -16,7 +16,7 @@ if len(sys.argv) > 2:
     valid_arr = ast.literal_eval(cp.stdout)
 
     try:
-       cmd_2 = ["./send_email_w_attach.sh", arg_email_id, valid_arr[0]['url'], valid_arr[0]['caption'], valid_arr[0]['text'], "--debug"]
+       cmd_2 = ["./send_msmtp_email_w_attach.sh", arg_email_id, valid_arr[0]['url'], valid_arr[0]['caption'], valid_arr[0]['text'], "--debug"]
        print(cmd_2)
        cp2 = subprocess.run(cmd_2,  capture_output=True, text=True, check=True)
        print("Message sent successfully!")
