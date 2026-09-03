959f2e0fa2b8b598a34b9fb6b8a3695c782273e6
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-09-03 08:10:57 -0700

    skill test

---
+++ research/code/test/zeroclaw-agent/skill_chain/skill_exec_test.py
@@ -0,0 +1,6 @@
+import chroma_email_metadata_skill as cqmk
+
+#arguments: {sys.argv[1]} : {sys.argv[2]} : {sys.argv[3]} : {sys.argv[4]}: {sys.argv[5]} : {sys.argv[6]}
+my= cqmk("image", "bhalba@yahoo.com", "trees","GRANDCANYON", "Wed Dec 26 12:37:41 2018", "Wed Dec 26 12:37:41 2019")
+
+print(my)
