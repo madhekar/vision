dde16bff43e493b02631765b0ebfffa0ae394464
Bhalchandra Madhekar <bmadhekar@gmail.com>
2026-09-24 10:59:17 -0700

    process bug

--- research/code/test/zeroclaw-agent/skill_chain/chroma_query_methods.py
+++ research/code/test/zeroclaw-agent/skill_chain/chroma_query_methods.py
@@ -25,9 +25,10 @@ def chroma_query_init():
     img_collection = client.get_or_create_collection(name="multimodal_collection_images", embedding_function=embedding_function)
     vid_collection = client.get_or_create_collection(name="multimodal_collection_videos", embedding_function=embedding_function)
     txt_collection = client.get_or_create_collection(name="multimodal_collection_texts", embedding_function=embedding_function)
+    reranker_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
     n_results = 2
     
-    return img_collection, vid_collection, txt_collection, n_results
+    return img_collection, vid_collection, txt_collection, reranker_model, n_results
 
 def get_collection_count() -> list[int]:
     img_collection, vid_collection, txt_collection, _ = chroma_query_init()
@@ -37,12 +38,14 @@ def get_collection_count() -> list[int]:
 
 def query_image_collection( query_texts: list) -> dict:
     """Return semantic similarity search results for given query texts for image collection."""
-    img_collection, _, _, n_results = chroma_query_init()
-    """     img_res =  img_collection.query(
+    img_collection, _, _, rr_model, n_results = chroma_query_init()
+    print("cc", n_results)
+    img_res =  img_collection.query(
         query_texts=query_texts,
         n_results=n_results
-    ) """
-    img_res = cu.rerank_image_text_search(init_rerank_model(), query_texts, img_collection, 100, n_results)
+    ) 
+    #img_res = cu.rerank_image_text_search(rr_model, query_texts, img_collection, 100, n_results)
+    print("--->", img_res)
     result_list = []
     for ir in img_res["metadatas"][0]:
         result_list.append({"caption": ir["caption"] , "text": ir["text"], "ts": ir["ts"], "url": ir["uri"] })
